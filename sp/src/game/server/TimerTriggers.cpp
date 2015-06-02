60bd9106a767ac0e6c51fe2d614b78a81bbf60bd
Fatalis <fatalis.twitch@gmail.com>
2015-06-02 08:15:09 -0500

    Example code for creating non-bsp triggers

--- sp/src/game/server/TimerTriggers.cpp
+++ sp/src/game/server/TimerTriggers.cpp
@@ -3,6 +3,7 @@
 #include "tier0/memdbgon.h"
 
 void CTriggerStart::EndTouch(CBaseEntity* other) {
+	Msg("dbg: CTriggerStart::EndTouch");
 	BaseClass::EndTouch(other);
 	CTimer::timer()->Start(gpGlobals->tickcount);
 	TriggerCommands::SetStartTrigger(this);
@@ -45,3 +46,18 @@ void TriggerCommands::SetCheckpointTrigger(CTriggerCheckpoint* trigger)
 {
 	lastCheckpointTrigger = trigger;
 }
+
+static void TestCreateTriggerStart(void)
+{
+	CTriggerStart *pTrigger = (CTriggerStart *)CreateEntityByName("trigger_start");
+	if (pTrigger)
+	{
+		pTrigger->Spawn();
+		pTrigger->SetAbsOrigin( UTIL_GetLocalPlayer()->GetAbsOrigin() );
+		pTrigger->SetSize( Vector(-256, -256, -256), Vector(256, 256, 256) );
+		pTrigger->SetSolid( SOLID_BBOX );
+		// now use mom_reset_to_start
+	}
+}
+
+static ConCommand mom_createstart("mom_createstart", TestCreateTriggerStart, "test");
