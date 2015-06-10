1190ed395282a20244419f546483e33f2a4ac9a3
RabsRincon <rubrinbla@gmail.com>
2015-06-10 06:52:31 +0200

    Added concept to allow players to change the speedometer's units of measure with a ConVar (gh_speedmeter_units), choosing from USP (engine system), metric system, and imperial system. (Former one might need revision, as I'm not used to the imperial system and maybe chosen units are not the best ones for this)
    As it can be changed in-game, a callback function would be needed to change the Label text if the ConVar changes its value in-game.

--- sp/src/game/client/momentum/speedometer.cpp
+++ sp/src/game/client/momentum/speedometer.cpp
@@ -13,6 +13,9 @@ using namespace vgui;
 
 static ConVar gh_speedmeter_hvel("gh_speedmeter_hvel", "0", (FCVAR_CLIENTDLL | FCVAR_ARCHIVE), "If set to 1, doesn't take the vertical velocity component into account.");
 
+//Would be better if a callback function was added (To change the Label text if it's changed in-game), maybe reusing Reset()?
+static ConVar gh_speedmeter_units("gh_speedmeter_units", "1",(FCVAR_DONTRECORD | FCVAR_ARCHIVE | FCVAR_CLIENTDLL),"Changes the units of measure of the speedmeter. \n 1: Units per second. \n 2: Meters per second. \n 3: Inches per second.",true, 1, true, 3);
+
 class CHudSpeedMeter : public CHudElement, public CHudNumericDisplay
 {
 	DECLARE_CLASS_SIMPLE(CHudSpeedMeter, CHudNumericDisplay);
@@ -27,15 +30,34 @@ public:
 	{
 		Reset();
 	}
+
 	virtual void Reset()
 	{
-		SetLabelText(L"UPS");
+		//We set the proper LabelText based on gh_speedmeter_units value
+		switch ((int)gh_speedmeter_units.GetFloat()) 
+		{
+		case 1:
+			SetLabelText(L"UPS");
+			break;
+		case 2:
+			SetLabelText(L"m/s");
+			break;
+		case 3:
+			SetLabelText(L"in/s");
+			break;
+		default:
+			//If its value is not supported, USP is assumed (Even though this shouln't happen as Max and Min values are set)
+			SetLabelText(L"UPS");
+			break;
+		}	
 		SetDisplayValue(0);
 	}
 	virtual void OnThink();
 };
 
 DECLARE_HUDELEMENT(CHudSpeedMeter);
+
+
 
 CHudSpeedMeter::CHudSpeedMeter(const char *pElementName) :
 CHudElement(pElementName), CHudNumericDisplay(NULL, "HudSpeedMeter")
@@ -56,6 +78,27 @@ void CHudSpeedMeter::OnThink()
 			velocity.z = 0;
 		}
 
-		SetDisplayValue((int)velocity.Length());
+		//Conversions based on https://developer.valvesoftware.com/wiki/Dimensions#Map_Grid_Units:_quick_reference
+		float vel = (float)velocity.Length();
+		switch ((int)gh_speedmeter_units.GetFloat())
+		{
+		case 1:
+			//We do nothing but break out of the switch, as default vel is already in UPS
+			break;
+		case 2:
+			//1 unit = 19.05mm -> 0.01905m --Small enough to approximate to 0.02--
+			vel = vel * 0.02;
+			break;
+		case 3:
+			//1 unit = 0.75"
+			//I'm not very used to imperial system.Maybe other scale but inches would be better?
+			vel = vel * 0.75;
+			break;
+		default:
+			//We do nothing but break out of the switch, as default vel is already in UPS
+			break;
+		}
+		SetDisplayValue((int)vel);
 	}
 }
+
