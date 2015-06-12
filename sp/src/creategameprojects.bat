1e8bbce5f116312baf7f09f9dff036027b3fff0e
Fatalis <fatalis.twitch@gmail.com>
2015-06-12 13:30:36 -0500

    Detect SDK hl2.exe location and configure vcxproj automatically

--- sp/src/creategameprojects.bat
+++ sp/src/creategameprojects.bat
@@ -1,2 +1,3 @@
 devtools\bin\vpc.exe /hl2 +game /mksln momentum.sln
-copy momentum.sln+sln_hack.txt momentum.sln
+copy momentum.sln+sln_fix.txt momentum.sln
+Powershell.exe -ExecutionPolicy Bypass -File creategameprojects.ps1
