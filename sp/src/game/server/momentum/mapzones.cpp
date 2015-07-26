dc53266adeaaed7ece993075168e96e3fcbc979b
TheAsuro <theasuro@gmail.com>
2015-07-26 08:36:31 +0200

    Added basic parsing of zone files. The code looks for a file with the map name + ".zon" in the maps directory. Example file: "zones" {     "start"     {         "xPos" "10"         "yPos" "10"         "zPos" "10"     }
        "checkpoint"
        {
            "index" "1"
            "xPos" "-40"
            "yPos" "-50"
            "zPos" "60"
        }
    
        "end"
        {
            "xPos" "100"
            "yPos" "100"
            "zPos" "100"
        }
    }

--- sp/src/game/server/momentum/mapzones.cpp
+++ sp/src/game/server/momentum/mapzones.cpp
@@ -1,7 +1,47 @@
 #include "cbase.h"
 #include "mapzones.h"
+#include "filesystem.h"
+#include "KeyValues.h"
+#include "TimerTriggers.h"
 
 #include "tier0/memdbgon.h"
+
+CMapzone::CMapzone()
+{
+	type = -1;
+}
+
+CMapzone::~CMapzone()
+{
+
+}
+
+CMapzone::CMapzone(const int pType, const Vector pPos, const Vector pRot, const Vector pScale)
+{
+	type = pType;
+	pos = pPos;
+	rot = pRot;
+	scale = pScale;
+}
+
+CMapzone::CMapzone(const int pType, const Vector pPos, const Vector pRot, const Vector pScale, int pIndex)
+{
+	type = pType;
+	pos = pPos;
+	rot = pRot;
+	scale = pScale;
+	index = pIndex;
+}
+
+void CMapzone::SpawnZone()
+{
+	// TODO: Create trigger
+}
+
+void CMapzone::RemoveZone()
+{
+	// TODO: Remove trigger
+}
 
 CMapzoneData::CMapzoneData(const char *pMapName)
 {
@@ -11,22 +51,76 @@ CMapzoneData::CMapzoneData(const char *pMapName)
 	Q_strcat(zoneFilePath, pMapName, MAX_PATH);
 	Q_strncat(zoneFilePath, zoneFileEnding, MAX_PATH);
 
-	Log("Looking for zone file: ");
-	Log(zoneFilePath);
-	Log("\n");
+	int counter = 0;
 
-	if (g_pFullFileSystem->FileExists(pMapName, "GAME"))
-		Log("Found file!\n");
+	KeyValues* zoneKV = new KeyValues(pMapName);
+	if (zoneKV->LoadFromFile(filesystem, zoneFilePath, "GAME"))
+	{
+		// Count map zones in file
+		CMapzoneData::zoneCount = 0;
+		for (KeyValues *cp = zoneKV->GetFirstSubKey(); cp; cp = cp->GetNextKey())
+		{
+			CMapzoneData::zoneCount++;
+		}
+		CMapzoneData::zones = new CMapzone[CMapzoneData::zoneCount];
 
-	// TODO: Load zone file and store info in memory
+		// Go through checkpoints
+		for (KeyValues *cp = zoneKV->GetFirstSubKey(); cp; cp = cp->GetNextKey())
+		{
+			// Load position information (will default to 0 if the keys don't exist)
+			int xPos = cp->GetInt("xPos");
+			int yPos = cp->GetInt("yPos");
+			int zPos = cp->GetInt("zPos");
+
+			int xRot = cp->GetInt("xRot");
+			int yRot = cp->GetInt("yRot");
+			int zRot = cp->GetInt("zRot");
+
+			int xScale = cp->GetInt("xScale");
+			int yScale = cp->GetInt("yScale");
+			int zScale = cp->GetInt("zScale");
+
+			// Do specific things for different types of checkpoints
+			if (Q_strcmp(cp->GetName(), "start") == 0)
+			{
+				CMapzoneData::zones[counter] = CMapzone(0, Vector(xPos, yPos, zPos), Vector(xRot, yRot, zRot), Vector(xScale, yScale, zScale));
+			}
+			else if (Q_strcmp(cp->GetName(), "checkpoint") == 0)
+			{
+				CMapzoneData::zones[counter] = CMapzone(1, Vector(xPos, yPos, zPos), Vector(xRot, yRot, zRot), Vector(xScale, yScale, zScale), cp->GetInt("index"));
+			}
+			else if (Q_strcmp(cp->GetName(), "end") == 0)
+			{
+				CMapzoneData::zones[counter] = CMapzone(2, Vector(xPos, yPos, zPos), Vector(xRot, yRot, zRot), Vector(xScale, yScale, zScale));
+			}
+			else
+			{
+				Log("Error while reading zone file! Unknown checkpoint type.");
+			}
+
+			counter++;
+		}
+	}
+	zoneKV->deleteThis();
 }
 
 CMapzoneData::~CMapzoneData()
 {
-	// TODO: Remove all previously created triggers
+	RemoveMapZones();
 }
 
 void CMapzoneData::SpawnMapZones()
 {
-	// TODO: Create zone triggers from loaded file info
+	for (int i = 0; i < CMapzoneData::zoneCount; i++)
+	{
+		zones[i].SpawnZone();
+	}
+}
+
+void CMapzoneData::RemoveMapZones()
+{
+	for (int i = 0; i < CMapzoneData::zoneCount; i++)
+	{
+		zones[i].RemoveZone();
+	}
 }
--- sp/src/game/server/momentum/mapzones.h
+++ sp/src/game/server/momentum/mapzones.h
@@ -1,5 +1,24 @@
 #include "filesystem.h"
 
+class CMapzone
+{
+public:
+	CMapzone::CMapzone();
+	CMapzone::CMapzone(const int type, const Vector pos, const Vector rot, const Vector scale);
+	CMapzone::CMapzone(const int type, const Vector pos, const Vector rot, const Vector scale, int index);
+	~CMapzone();
+
+	void SpawnZone();
+	void RemoveZone();
+
+private:
+	int type; // 0 = start, 1 = checkpoint, 2 = end
+	int index;
+	Vector pos;
+	Vector rot;
+	Vector scale;
+};
+
 class CMapzoneData
 {
 public:
@@ -7,8 +26,12 @@ public:
 	~CMapzoneData();
 
 	void SpawnMapZones();
+	void RemoveMapZones();
 
 private:
 	const char* mapPath = "maps/";
 	const char* zoneFileEnding = ".zon";
+
+	int zoneCount = 4;
+	CMapzone* zones;
 };
