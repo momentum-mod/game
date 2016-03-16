0bc9d57dc1cf9d1bc05ed07f485012bd45807ce7
tuxxi <aidan.sojourner@gmail.com>
2016-03-15 18:05:11 -0700

    back to raytrace; needs fixing

--- mp/src/game/server/momentum/mom_doorfix.cpp
+++ mp/src/game/server/momentum/mom_doorfix.cpp
@@ -3,7 +3,7 @@
 
 #include "tier0/memdbgon.h"
 
-void CMOMBhopBlockFixSystem::FindBhopBlocks()
+       void CMOMBhopBlockFixSystem::FindBhopBlocks()
 {
     SetDefLessFunc(m_mapBlocks);
     //  ---- func_door ----
@@ -17,9 +17,7 @@ void CMOMBhopBlockFixSystem::FindBhopBlocks()
 
         if (startpos.z > endpos.z)
         {
-            CBaseEntity *pEntTeleport;
-            if ((pEntTeleport = FindTeleport(pEntDoor)) != NULL)
-                AddBhopBlock(pEntDoor, pEntTeleport, true);
+            FindTeleport(pEntDoor, true);
 
             if (m_mapBlocks.Count() == MAX_BHOPBLOCKS)
                 break;
@@ -36,9 +34,7 @@ void CMOMBhopBlockFixSystem::FindBhopBlocks()
 
         if (startpos.z > endpos.z && (pEntButton->HasSpawnFlags(SF_BUTTON_TOUCH_ACTIVATES)))
         {
-            CBaseEntity *pEntTeleport;
-            if ((pEntTeleport = FindTeleport(pEntButton)) != NULL)
-                AddBhopBlock(pEntButton, pEntTeleport, true);
+            FindTeleport(pEntButton, false);
 
             if (m_mapBlocks.Count() == MAX_BHOPBLOCKS)
                 break;
@@ -50,20 +46,20 @@ void CMOMBhopBlockFixSystem::AlterBhopBlock(bhop_block_t block)
     if (block.m_bIsDoor)
     {
         // And now the settings begin
-        CBaseDoor *pEntDoor = static_cast<CBaseDoor *>(block.m_pBlockEntity); //(block.m_hBlockEntity.Get());
+        CBaseDoor *pDoorEnt = static_cast<CBaseDoor *>(block.m_pBlockEntity); //(block.m_hBlockEntity.Get());
 
-        pEntDoor->m_vecPosition2 = pEntDoor->m_vecPosition1; // Set the end position to start (not allowed to move)
+        pDoorEnt->m_vecPosition2 = pDoorEnt->m_vecPosition1; // Set the end position to start (not allowed to move)
 
-        pEntDoor->m_flSpeed = 0.0; // set speed to 0 (further not allowed to move)
+        pDoorEnt->m_flSpeed = 0.0; // set speed to 0 (further not allowed to move)
 
-        pEntDoor->ClearSpawnFlags();
-        pEntDoor->AddSpawnFlags(SF_DOOR_PTOUCH); // Player touch affects this
+        pDoorEnt->ClearSpawnFlags();
+        pDoorEnt->AddSpawnFlags(SF_DOOR_PTOUCH); // Player touch affects this
 
         variant_t emptyvarient;
-        pEntDoor->AcceptInput("Lock", NULL, NULL, emptyvarient, 0); // Lock the door bhop block
+        pDoorEnt->AcceptInput("Lock", NULL, NULL, emptyvarient, 0); // Lock the door bhop block
 
-        pEntDoor->m_ls.sLockedSound =
-            pEntDoor->m_NoiseMoving; // Plays the sound like normal (makes the player aware they jumped it)
+        pDoorEnt->m_ls.sLockedSound =
+            pDoorEnt->m_NoiseMoving; // Plays the sound like normal (makes the player aware they jumped it)
     }
     else
     { // func_button block
@@ -77,6 +73,7 @@ void CMOMBhopBlockFixSystem::AlterBhopBlock(bhop_block_t block)
         pEntButton->AddSpawnFlags(SF_BUTTON_DONTMOVE | SF_BUTTON_TOUCH_ACTIVATES);
     }
 }
+
 void CMOMBhopBlockFixSystem::PlayerTouch(CBaseEntity *pPlayerEnt, CBaseEntity *pBlock)
 {
     CMomentumPlayer *pPlayer = static_cast<CMomentumPlayer *>(pPlayerEnt);
@@ -100,54 +97,48 @@ void CMOMBhopBlockFixSystem::PlayerTouch(CBaseEntity *pPlayerEnt, CBaseEntity *p
         }
     }
 }
-CBaseEntity* CMOMBhopBlockFixSystem::FindTeleport(CBaseEntity *pOther, float step)
+
+void CMOMBhopBlockFixSystem::FindTeleport(CBaseEntity *pBlockEnt, bool isDoor)
 {
-    CBaseEntity* ent = NULL;
-    while ((ent = gEntList.FindEntityByClassname(ent, "trigger_teleport")) != NULL)
+    // Create Vectors for the start, stop, and direction
+    Vector vecAbsStart, vecAbsEnd, vecDir;
+
+    vecDir = Vector(0, 0, -1); // Straight down
+
+    // Get the Start/End
+    vecAbsStart = pBlockEnt->GetAbsOrigin();
+    //move vector to top of door
+    vecAbsStart.z += pBlockEnt->WorldAlignMaxs().z;
+
+    vecAbsEnd = vecAbsStart + (vecDir * MAX_TRACE_LENGTH);
+
+    // Do the TraceLine, and write our results to our trace_t class, tr.
+    Ray_t ray;
+    ray.Init(vecAbsStart, vecAbsEnd);
+    CTeleportTriggerTraceEnum triggerTraceEnum(&ray, pBlockEnt, isDoor);
+
+    enginetrace->EnumerateEntities(ray, true, &triggerTraceEnum);
+}
+
+bool CTeleportTriggerTraceEnum::EnumEntity(IHandleEntity *pHandleEntity)
+{
+    trace_t tr;
+    CBaseEntity *pEnt = gEntList.GetBaseEntity(pHandleEntity->GetRefEHandle());
+
+    // Done to avoid hitting an entity that's both solid & a trigger.
+    if (pEnt->IsSolid())
+        return true;
+
+    enginetrace->ClipRayToEntity(*m_pRay, MASK_ALL, pHandleEntity, &tr);
+
+    if (tr.fraction < 1.0f)
     {
-        //DevLog("FOUND THE ACTUAL TRIGGER!\n");
-        float origin[3];
-
-        pOther->GetLocalOrigin().CopyToArray(origin);
-        Vector mins = ent->WorldAlignMins();
-        Vector maxs = ent->WorldAlignMaxs();
-
-        //double each member of mins and maxs since origin should be in the entitiy's world space center
-        GetAbsBoundingBox(ent, mins, maxs);
-
-        do
-        {
-            bool x = origin[0] >= mins.x && origin[0] <= maxs.x;
-            bool y = origin[1] >= mins.y && origin[1] <= maxs.y;
-            bool z = origin[2] >= mins.z && origin[2] <= maxs.z;
-
-            if (x && y && z)
-                return ent;
-
-            origin[2] -= step;
-        } 
-        while (origin[2] >= ent->WorldAlignMins().z);
+        DevLog("Entindex of TP: %i", pEnt->entindex());
+        DevLog("Entindex of door: %i", pEntBlock->entindex());
+        g_MOMBlockFixer->AddBhopBlock(pEntBlock, pEnt, bIsDoor);
     }
-
-    return NULL;
+    return true;
 }
-void CMOMBhopBlockFixSystem::GetAbsBoundingBox(CBaseEntity *ent, Vector &mins, Vector &maxs)
-{
-    if (!ent) return;
 
-    Vector origin(ent->GetLocalOrigin());
-
-    mins = ent->WorldAlignMins();
-    maxs = ent->WorldAlignMaxs();
-
-    mins.x += origin.x;
-    mins.y += origin.y;
-    mins.z += origin.z;
-
-    maxs.x += origin.x;
-    maxs.y += origin.y;
-    maxs.z += origin.z;
-
-}
 static CMOMBhopBlockFixSystem s_MOMBlockFixer("CMOMBhopBlockFixSystem");
 CMOMBhopBlockFixSystem *g_MOMBlockFixer = &s_MOMBlockFixer;
--- mp/src/game/server/momentum/mom_doorfix.h
+++ mp/src/game/server/momentum/mom_doorfix.h
@@ -16,20 +16,23 @@
 class CMOMBhopBlockFixSystem : CAutoGameSystem
 {
 
-  public:
+public:
     CMOMBhopBlockFixSystem(const char *pName) : CAutoGameSystem(pName) {}
 
-    void LevelInitPostEntity() { FindBhopBlocks(); }
+    virtual void LevelInitPostEntity()
+    {
+        FindBhopBlocks();
+    }
 
-    void LevelShutdownPostEntity() { m_mapBlocks.RemoveAll(); }
+    virtual void LevelShutdownPostEntity() { m_mapBlocks.RemoveAll(); }
+
+    bool IsBhopBlock(int entIndex) { return (m_mapBlocks.Find(entIndex) != m_mapBlocks.InvalidIndex()); }
 
     void PlayerTouch(CBaseEntity *pPlayerEnt, CBaseEntity *pBlock);
 
     void FindBhopBlocks();
-    bool IsBhopBlock(int entIndex) { return (m_mapBlocks.Find(entIndex) != m_mapBlocks.InvalidIndex()); }
 
-    void GetAbsBoundingBox(CBaseEntity *ent, Vector &mins, Vector &maxs);
-    CBaseEntity* FindTeleport(CBaseEntity *pOther, float step = 1.0f);
+    void FindTeleport(CBaseEntity *, bool);
 
     void AddBhopBlock(CBaseEntity *pBlockEnt, CBaseEntity *pTeleportEnt, bool isDoor)
     {
@@ -41,7 +44,7 @@ class CMOMBhopBlockFixSystem : CAutoGameSystem
         m_mapBlocks.Insert(pBlockEnt->entindex(), block);
     }
 
-  private:
+private:
     struct bhop_block_t
     {
         CBaseEntity* m_pBlockEntity;// func_door or func_button
@@ -50,6 +53,22 @@ class CMOMBhopBlockFixSystem : CAutoGameSystem
     };
     CUtlMap<int, bhop_block_t> m_mapBlocks;
     void AlterBhopBlock(bhop_block_t);
+};
+
+class CTeleportTriggerTraceEnum : public IEntityEnumerator
+{
+public:
+    CTeleportTriggerTraceEnum(Ray_t *pRay, CBaseEntity *block, bool isDoor)
+        : m_pRay(pRay), pEntBlock(block), bIsDoor(isDoor)
+    {
+    }
+
+    virtual bool EnumEntity(IHandleEntity *pHandleEntity);
+
+private:
+    bool bIsDoor;
+    CBaseEntity *pEntBlock;
+    Ray_t *m_pRay;
 };
 
 extern CMOMBhopBlockFixSystem *g_MOMBlockFixer;
