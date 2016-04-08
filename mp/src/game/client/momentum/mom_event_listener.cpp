a4c75987323428c1b550681a17d1fc10a72b0929
RSTFS <nolan.oppegard@gmail.com>
2016-04-08 17:01:36 -0400

    Make event vars static since we apparently can't make one listener class or interface for it.

--- mp/src/game/client/momentum/mom_event_listener.cpp
+++ mp/src/game/client/momentum/mom_event_listener.cpp
@@ -3,6 +3,22 @@
 #include "mom_event_listener.h"
 #include "tier0/memdbgon.h"
 
+bool C_Momentum_EventListener::m_bTimerIsRunning, C_Momentum_EventListener::m_bMapFinished;
+bool C_Momentum_EventListener::m_bTimeDidSave, C_Momentum_EventListener::m_bTimeDidUpload;
+
+bool C_Momentum_EventListener::m_bPlayerInsideStartZone, C_Momentum_EventListener::m_bPlayerInsideEndZone;
+bool C_Momentum_EventListener::m_bPlayerHasPracticeMode;
+
+int C_Momentum_EventListener::m_iTotalStrafes, C_Momentum_EventListener::m_iTotalJumps;
+float C_Momentum_EventListener::m_flStartSpeed, C_Momentum_EventListener::m_flEndSpeed, C_Momentum_EventListener::m_flVelocityMax, 
+    C_Momentum_EventListener::m_flVelocityAvg, C_Momentum_EventListener::m_flStrafeSyncAvg, C_Momentum_EventListener::m_flStrafeSync2Avg;
+
+int C_Momentum_EventListener::m_iCurrentStage, C_Momentum_EventListener::m_iStageTicks[MAX_STAGES], 
+    C_Momentum_EventListener::m_iStageJumps[MAX_STAGES], C_Momentum_EventListener::m_iStageStrafes[MAX_STAGES];
+float C_Momentum_EventListener::m_flStageStartSpeed[MAX_STAGES], C_Momentum_EventListener::m_flStageVelocityMax[MAX_STAGES],
+    C_Momentum_EventListener::m_flStageVelocityAvg[MAX_STAGES], C_Momentum_EventListener::m_flStageStrafeSyncAvg[MAX_STAGES],
+    C_Momentum_EventListener::m_flStageStrafeSync2Avg[MAX_STAGES];
+
 C_Momentum_EventListener::C_Momentum_EventListener()
 {
     //add listeners for all of our custom events
--- mp/src/game/client/momentum/mom_event_listener.h
+++ mp/src/game/client/momentum/mom_event_listener.h
@@ -10,17 +10,17 @@ public:
 
     void FireGameEvent(IGameEvent* pEvent);
 
-    bool m_bTimerIsRunning = false, m_bMapFinished = false;
-    bool m_bTimeDidSave, m_bTimeDidUpload;
+    static bool m_bTimerIsRunning, m_bMapFinished;
+    static bool m_bTimeDidSave, m_bTimeDidUpload;
 
-    bool m_bPlayerInsideStartZone = false, m_bPlayerInsideEndZone = false;
-    bool m_bPlayerHasPracticeMode = false;
+    static bool m_bPlayerInsideStartZone, m_bPlayerInsideEndZone;
+    static bool m_bPlayerHasPracticeMode;
 
-    int m_iTotalStrafes = 0, m_iTotalJumps = 0;
-    float m_flStartSpeed = 0, m_flEndSpeed, m_flVelocityMax, m_flVelocityAvg, m_flStrafeSyncAvg, m_flStrafeSync2Avg;
+    static int m_iTotalStrafes, m_iTotalJumps;
+    static float m_flStartSpeed, m_flEndSpeed, m_flVelocityMax, m_flVelocityAvg, m_flStrafeSyncAvg, m_flStrafeSync2Avg;
 
-    int m_iCurrentStage, m_iStageTicks[MAX_STAGES], m_iStageJumps[MAX_STAGES], m_iStageStrafes[MAX_STAGES];
-    float m_flStageStartSpeed[MAX_STAGES], m_flStageVelocityMax[MAX_STAGES],
+    static int m_iCurrentStage, m_iStageTicks[MAX_STAGES], m_iStageJumps[MAX_STAGES], m_iStageStrafes[MAX_STAGES];
+    static float m_flStageStartSpeed[MAX_STAGES], m_flStageVelocityMax[MAX_STAGES],
         m_flStageVelocityAvg[MAX_STAGES], m_flStageStrafeSyncAvg[MAX_STAGES], 
         m_flStageStrafeSync2Avg[MAX_STAGES];
 
