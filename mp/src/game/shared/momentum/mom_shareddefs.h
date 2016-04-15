7535711509b5eec14181ec35bd626a0c27f05cec
Nick K <nkerns25@yahoo.com>
2016-04-15 00:25:06 -0400

    0.2.7 -> 0.3.0
    New HUD UI elements from vguiruninfo_wip
    New start zones from newstartzone_wip
    Updated clientscheme.res LaF

--- mp/src/game/shared/momentum/mom_shareddefs.h
+++ mp/src/game/shared/momentum/mom_shareddefs.h
@@ -24,7 +24,7 @@ typedef enum MOMGM
 // Main Version (0 is alpha, 1 is beta, 2 is release)​.Main feature push (increment by one for each)​.​Small commits or hotfixes​
 // When editing this, remember to also edit version.txt on the main dir of the repo
 // If you have any doubts, please refer to http://semver.org/
-#define MOM_CURRENT_VERSION "0.2.7"
+#define MOM_CURRENT_VERSION "0.3.0"
 
 #define MOM_COLORIZATION_CHECK_FREQUENCY 0.1f
 
