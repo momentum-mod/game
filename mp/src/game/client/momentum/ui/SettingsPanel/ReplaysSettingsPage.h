9e2c5a637f1f3e4bf5328d589193b6581f7131dc
Nick K <nkerns25@yahoo.com>
2016-07-02 18:17:27 -0400

    Removed defunct variables
    And added localization

--- mp/src/game/client/momentum/ui/SettingsPanel/ReplaysSettingsPage.h
+++ mp/src/game/client/momentum/ui/SettingsPanel/ReplaysSettingsPage.h
@@ -19,13 +19,6 @@ class ReplaysSettingsPage : public SettingsPage
 
         m_pReplayModelAlphaEntry = FindControl<TextEntry>("ReplayModelAlphaEntry");
         m_pReplayModelAlphaEntry->AddActionSignalTarget(this);
-
-        // There is no cvar for this, not that I can find
-        m_pReplayShouldLoop = FindControl<CvarToggleCheckButton<ConVarRef>>("ReplayShouldLoop");
-        m_pReplayShouldLoop->AddActionSignalTarget(this);
-
-        m_pReplayInReverse = FindControl<CvarToggleCheckButton<ConVarRef>>("ReplayPlayInReverse");
-        m_pReplayInReverse->AddActionSignalTarget(this);
     }
 
     ~ReplaysSettingsPage() {}
@@ -69,7 +62,6 @@ private:
         Q_snprintf(buf, sizeof(buf), " %.1f", m_pReplayModelAlphaSlider->GetSliderValue());
         m_pReplayModelAlphaEntry->SetText(buf);
     }
-    CvarToggleCheckButton<ConVarRef> *m_pReplayShouldLoop, *m_pReplayInReverse;
     CCvarSlider *m_pReplayModelAlphaSlider;
     TextEntry *m_pReplayModelAlphaEntry;
 };
