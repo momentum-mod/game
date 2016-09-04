d1c3b4958a5e05bc65b81e75667052006d6be5c3
Kamay <xutaxkamay@gmail.com>
2016-09-05 00:22:56 +0200

    This shit shouldn't have been touched (it was a test but I noticed it was posted too late)

--- mp/src/game/client/c_baseentity.h
+++ mp/src/game/client/c_baseentity.h
@@ -2148,7 +2148,7 @@ inline VarMapping_t* C_BaseEntity::GetVarMapping()
 //-----------------------------------------------------------------------------
 inline bool	C_BaseEntity::IsInterpolationEnabled()
 {
-	return true; //always interpolate 
+	return s_bInterpolate; 
 }
 
 //-----------------------------------------------------------------------------
@@ -2156,7 +2156,7 @@ inline bool	C_BaseEntity::IsInterpolationEnabled()
 //-----------------------------------------------------------------------------
 inline bool C_BaseEntity::IsNoInterpolationFrame()
 {
-	return /*m_ubOldInterpolationFrame != m_ubInterpolationFrame*/ false; //always interpolate
+	return m_ubOldInterpolationFrame != m_ubInterpolationFrame;
 }
 
 //-----------------------------------------------------------------------------
