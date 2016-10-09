76b4a3fea7752abd7c5046e81464961e256989ac
RabsRincon <rubrinbla@gmail.com>
2016-10-09 16:34:47 +0200

    decals_subrect.txt allows us to spawn decals in-game. Measure points are now set where the player is looking at. Visuals added. TODO: Remove declas when pointschange

--- mp/src/game/server/momentum/mom_ruler.cpp
+++ mp/src/game/server/momentum/mom_ruler.cpp
@@ -9,27 +9,44 @@ CMOMRulerTool::CMOMRulerTool(const char* pName) : CAutoGameSystem(pName)
     m_vSecondPoint = vec3_invalid;
 }
 
-CON_COMMAND_F(mom_ruler_first, "Creates the first measure point where the player is looking at.\n", FCVAR_CLIENTCMD_CAN_EXECUTE)
+CON_COMMAND_F(mom_ruler_first, "Creates the first measure point where the player is looking at.\n", FCVAR_CLIENTCMD_CAN_EXECUTE | FCVAR_HIDDEN)
 {
     CBasePlayer* pPlayer = UTIL_GetLocalPlayer();
     if (!pPlayer || !g_MOMRulerTool)
         return;
-    // MOM_TODO: Make it get set on player eyes target, not oprigin, and put some graphical effect
-    g_MOMRulerTool->m_vFirstPoint = pPlayer->GetAbsOrigin();
-    DevMsg("First point set in (%f, %f, %f)\n", g_MOMRulerTool->m_vFirstPoint);
+    trace_t tr;
+    Vector vecFwd;
+
+    AngleVectors(pPlayer->EyeAngles(), &vecFwd);
+
+    UTIL_TraceLine(pPlayer->EyePosition(), pPlayer->EyePosition() + vecFwd * 1000, MASK_PLAYERSOLID, pPlayer, COLLISION_GROUP_NONE, &tr);
+    if (!tr.DidHit())
+        return;
+    g_MOMRulerTool->m_vFirstPoint = tr.endpos;
+    pPlayer->DecalTrace(&tr, "BirdPoop");
+    DevMsg("First point set in (%.4f, %.4f, %.4f)\n", g_MOMRulerTool->m_vFirstPoint.x, g_MOMRulerTool->m_vFirstPoint.y, g_MOMRulerTool->m_vFirstPoint.z);
+    
 }
 
-CON_COMMAND_F(mom_ruler_second, "Creates the second measure point where the player is looking at.\n", FCVAR_CLIENTCMD_CAN_EXECUTE)
+CON_COMMAND_F(mom_ruler_second, "Creates the second measure point where the player is looking at.\n", FCVAR_CLIENTCMD_CAN_EXECUTE | FCVAR_HIDDEN)
 {
     CBasePlayer* pPlayer = UTIL_GetLocalPlayer();
     if (!pPlayer || !g_MOMRulerTool)
         return;
-    // MOM_TODO: Make it get set on player eyes target, not oprigin, and put some graphical effect
-    g_MOMRulerTool->m_vSecondPoint = pPlayer->GetAbsOrigin();
-    DevMsg("Second point set in (%f, %f, %f)\n", g_MOMRulerTool->m_vFirstPoint);
+    trace_t tr;
+    Vector vecFwd;
+
+    AngleVectors(pPlayer->EyeAngles(), &vecFwd);
+
+    UTIL_TraceLine(pPlayer->EyePosition(), pPlayer->EyePosition() + vecFwd * 1000, MASK_PLAYERSOLID, pPlayer, COLLISION_GROUP_NONE, &tr);
+    if (!tr.DidHit())
+        return;
+    g_MOMRulerTool->m_vSecondPoint = tr.endpos;
+    pPlayer->DecalTrace(&tr, "BirdPoop");
+    DevMsg("Second point set in (%.4f, %.4f, %.4f)\n", g_MOMRulerTool->m_vSecondPoint.x, g_MOMRulerTool->m_vSecondPoint.y, g_MOMRulerTool->m_vSecondPoint.z);
 }
 
-CON_COMMAND_F(mom_ruler_measure, "Measures the distance between the first and second point.\n", FCVAR_CLIENTCMD_CAN_EXECUTE)
+CON_COMMAND_F(mom_ruler_measure, "Measures the distance between the first and second point.\n", FCVAR_CLIENTCMD_CAN_EXECUTE | FCVAR_HIDDEN)
 {
     CBasePlayer* pPlayer = UTIL_GetLocalPlayer();
     if (!pPlayer || !g_MOMRulerTool)
@@ -37,7 +54,7 @@ CON_COMMAND_F(mom_ruler_measure, "Measures the distance between the first and se
     if (g_MOMRulerTool->m_vFirstPoint != vec3_invalid && g_MOMRulerTool->m_vSecondPoint != vec3_invalid)
     {
         // MOM_TODO: Put this somewhere better.. Like, in the "chat"?
-        DevMsg("Distance between first point (%f, %f, %f) and second point (%f, %f, %f) is %f units.\n", g_MOMRulerTool->m_vFirstPoint.x, g_MOMRulerTool->m_vFirstPoint.y, g_MOMRulerTool->m_vFirstPoint.z,
+        DevMsg("Distance between first point (%.4f, %.4f, %.4f) and second point (%.4f, %.4f, %.4f) is %.4f units.\n", g_MOMRulerTool->m_vFirstPoint.x, g_MOMRulerTool->m_vFirstPoint.y, g_MOMRulerTool->m_vFirstPoint.z,
             g_MOMRulerTool->m_vSecondPoint.x, g_MOMRulerTool->m_vSecondPoint.y, g_MOMRulerTool->m_vSecondPoint.y, g_MOMRulerTool->m_vFirstPoint.DistTo(g_MOMRulerTool->m_vSecondPoint));
     }
     else
