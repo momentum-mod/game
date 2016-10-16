ee484193a9002f2022437d7a2649037bcc302f5c
Nick K <nkerns25@yahoo.com>
2016-10-16 03:37:10 -0400

    0.5.0 -> 0.6.0
    It's happening \o/

--- mp/src/game/shared/momentum/mom_shareddefs.h
+++ mp/src/game/shared/momentum/mom_shareddefs.h
@@ -7,6 +7,13 @@
 #include "const.h"
 #include "shareddefs.h"
 
+
+// Main Version (0 is prealpha, 1 is alpha, 2 is beta and 3 is release)​.Main feature push (increment by one for each)​.​Small commits or hotfixes​
+// When editing this, remember to also edit version.txt on the main dir of the repo
+// If you have any doubts, please refer to http://semver.org/
+#define MOM_CURRENT_VERSION "0.6.0"
+
+
 // Gamemode for momentum
 typedef enum MOMGM
 {
@@ -35,11 +42,6 @@ typedef enum FLAGS
 #define IN_TIMES (1<<26)
 
 #define PANEL_REPLAY "replaycontrols"
-
-// Main Version (0 is prealpha, 1 is alpha, 2 is beta and 3 is release)​.Main feature push (increment by one for each)​.​Small commits or hotfixes​
-// When editing this, remember to also edit version.txt on the main dir of the repo
-// If you have any doubts, please refer to http://semver.org/
-#define MOM_CURRENT_VERSION "0.5.11"
 
 #define MOM_COLORIZATION_CHECK_FREQUENCY 0.1f
 
