686707d39c643ad265e8c41fc2f6c0ed3b7b2a0c
RabsRincon <rubrinbla@gmail.com>
2017-02-26 06:11:48 +0100

    Pushed to allow goc to help.

--- mp/src/game/client/momentum/ui/MapSelection/OnlineMaps.cpp
+++ mp/src/game/client/momentum/ui/MapSelection/OnlineMaps.cpp
@@ -1,5 +1,8 @@
 #include "pch_mapselection.h"
 #include "util/jsontokv.h"
+#include "view_scene.h"
+
+#include "vgui_bitmapimage.h"
 
 using namespace vgui;
 
@@ -80,10 +83,16 @@ void COnlineMaps::MapsQueryCallback(HTTPRequestCompleted_t* pCallback, bool bIOF
                     m.m_iZoneCount = pRun->GetInt("zones");
                     m.m_iDifficulty = pRun->GetInt("difficulty");
                     m.m_iGameMode = pRun->GetInt("gamemode");
+                    m.m_iMapId = pRun->GetInt("id");
                     Q_strcpy(m.m_szBestTime, pRun->GetString("zones"));
+                    Q_strcpy(m.m_szThumbnailUrl, pRun->GetString("thumbnail"));
                     map.m_mMap = m;
                     KeyValues *kv = new KeyValues("map");
- 
+                    if (Q_strlen(m.m_szThumbnailUrl) > 3)
+                    {
+                        CImageDownloader imageDownloader;
+                        map.m_iMapImageIndex = imageDownloader.Process(m.m_iMapId, m.m_szMapName, m.m_szThumbnailUrl, m_pMapList);
+                    }
                     kv->SetString(KEYNAME_MAP_NAME, m.m_szMapName);
                     kv->SetString(KEYNAME_MAP_LAYOUT, m.m_bHasStages ? "STAGED" : "LINEAR");
                     kv->SetInt(KEYNAME_MAP_ZONE_COUNT, m.m_iZoneCount);
@@ -219,9 +228,9 @@ void COnlineMaps::StartRefresh()
         ClearMapList();
         char szUrl[BUFSIZ];
         Q_snprintf(szUrl, BUFSIZ, "%s/getmaps/2/0/0", MOM_APIDOMAIN);
-        KeyValues *kvFilters = GetFilters();
-        g_pMomentumUtil->CreateAndSendHTTPReq(szUrl, &cbMapsQuery, &COnlineMaps::MapsQueryCallback, this, k_EHTTPMethodPOST, kvFilters);
-        kvFilters->deleteThis();
+        //KeyValues *kvFilters = GetFilters();
+        g_pMomentumUtil->CreateAndSendHTTPReq(szUrl, &cbMapsQuery, &COnlineMaps::MapsQueryCallback, this, k_EHTTPMethodGET);
+        //kvFilters->deleteThis();
     }
 }
 
@@ -237,4 +246,85 @@ void COnlineMaps::OnOpenContextMenu(int itemID)
     // Activate context menu
     CMapContextMenu *menu = MapSelectorDialog().GetContextMenu(m_pMapList);
     menu->ShowMenu(this, true, true);
+}
+
+int CImageDownloader::Process(const int iMapId, const char* szMapName, const char* szUrl, CMapListPanel* pTargetPanel)
+{
+    if (!pTargetPanel || !pTargetPanel->GetImageList())
+    {
+        return 0;
+    }
+    Q_strcpy(m_szImageUrl, szUrl);
+    Q_strcpy(m_szMapName, szMapName);
+    
+    char szPreviewPath[MAX_PATH];
+    Q_snprintf(szPreviewPath, MAX_PATH, "materials/maps/%s.vtf", m_szMapName);
+    
+    FileHandle_t file;
+    file = filesystem->Open(szPreviewPath, "r", "MOD");
+    m_pImageList = pTargetPanel->GetImageList();
+    if (!file)
+    {
+        // fetch the image
+        m_iTargetIndex = m_pImageList->AddImage(nullptr);
+        //g_pMomentumUtil->CreateAndSendHTTPReq(szUrl, &cbDownloadCallback, &CImageDownloader::Callback, this);
+    }
+    else
+    {
+        // If it exists, lets search it on the panel list, in case it's already there
+        const int itemCount = pTargetPanel->GetItemCount();
+        for (int i = 0; i < itemCount; ++i)
+        {
+            const ListPanelItem * itemData = pTargetPanel->GetItemData(i);
+            if (!itemData->kv) continue;
+            // This can't use map id, use image comparising
+            if (itemData->kv->GetInt("id") == iMapId && itemData->m_nImageIndex != 0)
+            {
+                // Image found, return its index (We save it on the target index too just in case we need it later)
+                m_iTargetIndex = itemData->m_nImageIndex;
+                return m_iTargetIndex;
+            }
+        }
+        // We're here because the image was not found, lets add it.
+        // We first need to get the vtf into something that the map lists understands
+        BitmapImage *newImage = new BitmapImage;
+        newImage->SetImageFile(szPreviewPath);
+        m_iTargetIndex = m_pImageList->AddImage(newImage);
+    }
+    return m_iTargetIndex;
+}
+
+void CImageDownloader::Callback(HTTPRequestCompleted_t* pCallback, bool bIOFailure)
+{
+    if (bIOFailure || !pCallback || !pCallback->m_bRequestSuccessful || pCallback->m_eStatusCode != k_EHTTPStatusCode200OK)
+        return;
+
+    FileHandle_t file;
+    char szPreviewPath[MAX_PATH];
+    Q_snprintf(szPreviewPath, MAX_PATH, "materials/maps/%s.vtf");
+    file = filesystem->Open(szPreviewPath, "w+b", "MOD");
+    uint32 size;
+    steamapicontext->SteamHTTP()->GetHTTPResponseBodySize(pCallback->m_hRequest, &size);
+    if (size == 0)
+    {
+        Warning("CImageDownloader::Callback: 0 body size!\n");
+        return;
+    }
+    DevLog("Size of body: %u\n", size);
+    uint8* pData = new uint8[size];
+    steamapicontext->SteamHTTP()->GetHTTPResponseBodyData(pCallback->m_hRequest, pData, size);
+    // write the file
+    filesystem->Write(pData, size, file);
+    // save the file
+    
+    filesystem->Close(file);
+    DevLog("Successfully written file\n");
+    // Free resources
+    m_pImageList->SetImageAtIndex(m_iTargetIndex, nullptr);
+    if (pData)
+    {
+        delete[] pData;
+    }
+    pData = nullptr;
+    steamapicontext->SteamHTTP()->ReleaseHTTPRequest(pCallback->m_hRequest);
 }
