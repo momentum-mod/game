241b91d124b9e37ebc7d54ab134dc3c3c503ab18
RabsRincon <rubrinbla@gmail.com>
2017-02-26 09:07:37 +0100

    TargetIndex does not need to be assigned if we're going to delete ourselves in the next step.

--- mp/src/game/client/momentum/ui/MapSelection/OnlineMaps.cpp
+++ mp/src/game/client/momentum/ui/MapSelection/OnlineMaps.cpp
@@ -284,16 +284,14 @@ bool CImageDownloader::Process(const char* szMapName, const char* szUrl, CMapLis
         if (iterImage->GetID() == newImage->GetID())
         {
             // Image found, return its index (We save it on the target index too just in case we need it later)
-            m_iTargetIndex = i;
-            index = m_iTargetIndex;
+            index = i;
             return true; // Delete me, we already exist
         }
     }
 
     // We're here because the image was not found, lets add it.
-    // We first need to get the vtf into something that the map lists understands    
-    m_iTargetIndex = m_pImageList->AddImage(newImage);
-    index = m_iTargetIndex;
+    // We first need to get the vtf into something that the map lists understands      
+    index = m_pImageList->AddImage(newImage);
     return true; // Delete me, we already exist
 }
 
@@ -348,7 +346,7 @@ void CImageDownloader::Callback(HTTPRequestCompleted_t* pCallback, bool bIOFailu
             pData = nullptr;
         }
     }
-
-    steamapicontext->SteamHTTP()->ReleaseHTTPRequest(pCallback->m_hRequest);
+    if (pCallback)
+        steamapicontext->SteamHTTP()->ReleaseHTTPRequest(pCallback->m_hRequest);
     delete this;
 }
