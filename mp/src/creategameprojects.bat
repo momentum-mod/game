253da4b30ea98c0934d1907931698ca5699ceb17
tuxxi <aidan.sojourner@gmail.com>
2017-08-12 17:34:15 -0700

    Added lakitu64's batch fix for creategameprojects

--- mp/src/creategameprojects.bat
+++ mp/src/creategameprojects.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d %~dp0
 devtools\bin\vpc.exe /momentum +game +shaders /mksln momentum.sln /f
 copy momentum.sln+sln_fix.txt momentum.sln
 Powershell.exe -ExecutionPolicy Bypass -File creategameprojects.ps1
