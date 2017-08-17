80c17c3e0ec2406a80e7a497477d6134a5c3077e
Nolan-O <nolan.oppegard@gmail.com>
2017-08-17 00:37:01 -0400

    Fix stragling case-sensitivity error.

--- mp/src/game/client/vgui_entitypanel.cpp
+++ mp/src/game/client/vgui_entitypanel.cpp
@@ -6,7 +6,7 @@
 // $NoKeywords: $
 //=============================================================================//
 #include "cbase.h"
-#include "vgui_EntityPanel.h"
+#include "vgui_entitypanel.h"
 #include "ienginevgui.h"
 #ifdef TF_CLIENT_DLL
 #include "c_BaseTFPlayer.h"
