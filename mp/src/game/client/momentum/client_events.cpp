4693e3d674b233914c1afdccb83753b5b594dc90
Nick K <nkerns25@yahoo.com>
2017-12-08 23:19:33 -0500

    Simplify CS:S mounting
    CFmtStr is pretty cute
    Fixed interesting sound bug by mounting in Init instead of PostInit
    Removed old SDK wiki mount code (no gameinfo mount stuff anymore)

--- mp/src/game/client/momentum/client_events.cpp
+++ mp/src/game/client/momentum/client_events.cpp
@@ -1,62 +1,47 @@
+#include "cbase.h"
+
 #include "client_events.h"
 
 #include "filesystem.h"
 #include "mom_event_listener.h"
 #include "mom_run_poster.h"
-#include "movevars_shared.h"
 #include "momentum/ui/IMessageboxPanel.h"
+#include "fmtstr.h"
 
 #include "tier0/memdbgon.h"
 
 extern IFileSystem *filesystem;
 
-void CMOMClientEvents::PostInit()
+bool CMOMClientEvents::Init()
 {
-    g_MOMEventListener->Init(); // Hook into game events
-    g_MOMRunPoster->Init();     // Get ready to post runs...
-
-    // enable console by default
-    ConVarRef con_enable("con_enable");
-    con_enable.SetValue(true);
-
-    // Mount CSS content even if it's on a different drive than SDK
+    // Mount CSS content even if it's on a different drive than this game
     if (steamapicontext && steamapicontext->SteamApps())
     {
         char installPath[MAX_PATH];
         uint32 folderLen = steamapicontext->SteamApps()->GetAppInstallDir(240, installPath, MAX_PATH);
         if (folderLen)
         {
-            char pathCStrike[MAX_PATH];
-            V_ComposeFileName(installPath, "cstrike", pathCStrike, sizeof(pathCStrike));
-            filesystem->AddSearchPath(pathCStrike, "GAME");
-
-            char pathPak[MAX_PATH];
-            V_ComposeFileName(pathCStrike, "cstrike_pak.vpk", pathPak, sizeof(pathPak));
-            filesystem->AddSearchPath(pathPak, "GAME");
-
-            char downloadPath[MAX_PATH];
-            V_ComposeFileName(pathCStrike, "download", downloadPath, sizeof(downloadPath));
-            filesystem->AddSearchPath(downloadPath, "GAME");
-            filesystem->AddSearchPath(downloadPath, "download");
+            filesystem->AddSearchPath(CFmtStr("%s/cstrike", installPath), "GAME");
+            filesystem->AddSearchPath(CFmtStr("%s/cstrike/cstrike_pak.vpk", installPath), "GAME");
+            filesystem->AddSearchPath(CFmtStr("%s/cstrike/download", installPath), "GAME");
+            filesystem->AddSearchPath(CFmtStr("%s/cstrike/download", installPath), "download");
 
             if (developer.GetInt())
                 filesystem->PrintSearchPaths();
-
-            // INTERESTING BUG, VOLVO!
-            // Apparently, mounting sounds from any directory that isn't included in gameinfo.txt is impossible
-            // to play without restarting the entire sound system. So we do that here.
-            // If we had engine code...
-            ConCommand *snd_restart = g_pCVar->FindCommand("snd_restart");
-            if (snd_restart)
-            {
-                char const *argv[1] = { "snd_restart" };
-                CCommand cmd(1, argv);
-                snd_restart->Dispatch(cmd);
-            }
         }
     }
 
-    MountAdditionalContent();
+    return true;
+}
+
+void CMOMClientEvents::PostInit()
+{
+    g_MOMEventListener->Init(); // Hook into game events
+    //g_MOMRunPoster->Init();     // Get ready to post runs...
+
+    // enable console by default
+    ConVarRef con_enable("con_enable");
+    con_enable.SetValue(true);
 
     // Version warning
     // MOM_TODO: Change this once we hit Alpha/Beta
@@ -72,38 +57,6 @@ void CMOMClientEvents::PostInit()
 void CMOMClientEvents::LevelInitPreEntity()
 {
     Precache();
-}
-
-void CMOMClientEvents::MountAdditionalContent()
-{
-    // From the Valve SDK wiki
-    KeyValues *pMainFile = new KeyValues("gameinfo.txt");
-    bool bLoad = false;
-#ifndef _WIN32
-    // case sensitivity
-    bLoad = pMainFile->LoadFromFile(filesystem, "GameInfo.txt", "MOD");
-#endif
-    if (!bLoad)
-        bLoad = pMainFile->LoadFromFile(filesystem, "gameinfo.txt", "MOD");
-
-    if (pMainFile && bLoad)
-    {
-        KeyValues *pFileSystemInfo = pMainFile->FindKey("FileSystem");
-        if (pFileSystemInfo)
-        {
-            for (KeyValues *pKey = pFileSystemInfo->GetFirstSubKey(); pKey; pKey = pKey->GetNextKey())
-            {
-                if (Q_strcmp(pKey->GetName(), "AdditionalContentId") == 0)
-                {
-                    int appid = abs(pKey->GetInt());
-                    if (appid)
-                        if (filesystem->MountSteamContent(-appid) != FILESYSTEM_MOUNT_OK)
-                            Warning("Unable to mount extra content with appId: %i\n", appid);
-                }
-            }
-        }
-    }
-    pMainFile->deleteThis();
 }
 
 void CMOMClientEvents::Precache()
