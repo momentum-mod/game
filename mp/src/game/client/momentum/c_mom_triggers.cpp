3d63f18e31b85bf0d375001fd06ff53d6f47827b
Kamay Xutax <xutaxkamay@gmail.com>
2018-06-02 18:37:17 +0200

    Thanks to SCell555 for proper outline brushes.

--- mp/src/game/client/momentum/c_mom_triggers.cpp
+++ mp/src/game/client/momentum/c_mom_triggers.cpp
@@ -1,6 +1,6 @@
 #include "cbase.h"
 #include "c_mom_triggers.h"
-#include "mom_enginedraw.h"
+#include "model_types.h"
 #include "util/mom_util.h"
 
 #include "tier0/memdbgon.h"
@@ -28,17 +28,92 @@ LINK_ENTITY_TO_CLASS(trigger_momentum_timer_stop, C_TriggerTimerStop);
 IMPLEMENT_CLIENTCLASS_DT(C_TriggerTimerStop, DT_TriggerTimerStop, CTriggerTimerStop)
 END_RECV_TABLE();
 
+static class CTriggerOutlineRenderer : public IBrushRenderer, public CAutoGameSystem
+{
+  public:
+    CTriggerOutlineRenderer()
+    {
+        m_pVertices = NULL;
+        m_verticeCount = 0;
+    }
+
+    bool RenderBrushModelSurface(IClientEntity* pBaseEntity, IBrushSurface* pBrushSurface) OVERRIDE
+    {
+        const int vertices = pBrushSurface->GetVertexCount();
+        if (vertices > m_verticeCount)
+        {
+            m_verticeCount = vertices;
+            if (m_pVertices == NULL)
+                m_pVertices =
+                    static_cast<BrushVertex_t *>(MemAlloc_AllocAligned(sizeof(BrushVertex_t) * m_verticeCount, 64));
+            else
+                m_pVertices = static_cast<BrushVertex_t *>(
+                    MemAlloc_ReallocAligned(m_pVertices, sizeof(BrushVertex_t) * m_verticeCount, 64));
+        }
+        pBrushSurface->GetVertexData(m_pVertices);
+        CMatRenderContextPtr pRenderContext(materials);
+
+        CMeshBuilder builder;
+        builder.Begin(pRenderContext->GetDynamicMesh(true, 0, 0, outlineMaterial), MATERIAL_LINE_LOOP, vertices);
+        for ( int i = 0; i < vertices; i++ )
+        {
+            const BrushVertex_t &vertex = m_pVertices[i];
+            builder.Position3fv(vertex.m_Pos.Base());
+            builder.Normal3fv(vertex.m_Normal.Base());
+            builder.Color4ub(outlineColor.r(), outlineColor.g(), outlineColor.b(), outlineColor.a());
+            builder.AdvanceVertex();
+        }
+
+        builder.End(false, true);
+
+        return false;
+    }
+
+    bool Init() OVERRIDE
+    {
+        KeyValues *pVMTKeyValues = new KeyValues("unlitgeneric");
+        pVMTKeyValues->SetString("$vertexcolor", "1");
+        pVMTKeyValues->SetString("$vertexalpha", "1");
+        pVMTKeyValues->SetString("$additive", "1");
+        pVMTKeyValues->SetString("$ignorez", "0"); // Change this to 1 to see it through walls
+        pVMTKeyValues->SetString("$halflambert", "1");
+        pVMTKeyValues->SetString("$selfillum", "1");
+        pVMTKeyValues->SetString("$nofog", "1");
+        pVMTKeyValues->SetString("$nocull", "1");
+        pVMTKeyValues->SetString("$model", "1");
+        outlineMaterial.Init("__utilOutlineColor", pVMTKeyValues);
+        outlineMaterial->Refresh();
+
+        return true;
+    }
+
+    void Shutdown() OVERRIDE
+    {
+        MemAlloc_FreeAligned(m_pVertices);
+        m_pVertices = NULL;
+        outlineMaterial.Shutdown();
+    }
+
+    Color outlineColor;
+
+  private:
+    CMaterialReference outlineMaterial;
+    BrushVertex_t *m_pVertices;
+    int m_verticeCount;
+} outlineRenderer;
+
 bool C_TriggerTimerStart::ShouldDraw() { return true; }
 
 int C_TriggerTimerStart::DrawModel(int flags)
 {
-    if (mom_startzone_outline_enable.GetBool())
+    if (mom_startzone_outline_enable.GetBool() && (flags & STUDIO_RENDER) != 0 &&
+        (flags & (STUDIO_SHADOWDEPTHTEXTURE | STUDIO_SHADOWDEPTHTEXTURE)) == 0)
     {
-        Color color_startzone;
-        if (g_pMomentumUtil->GetColorFromHex(mom_startzone_color.GetString(), color_startzone))
+        if (g_pMomentumUtil->GetColorFromHex(mom_startzone_color.GetString(), outlineRenderer.outlineColor))
         {
-            MOM_DrawBrushOutlineModel(this, color_startzone);
-
+            render->InstallBrushSurfaceRenderer(&outlineRenderer);
+            BaseClass::DrawModel(STUDIO_RENDER);
+            render->InstallBrushSurfaceRenderer(nullptr);
             if (IsEffectActive(EF_NODRAW))
                 return 1;
         }
@@ -50,14 +125,14 @@ int C_TriggerTimerStart::DrawModel(int flags)
 bool C_TriggerTimerStop::ShouldDraw() { return true; }
 
 int C_TriggerTimerStop::DrawModel(int flags)
-{
-    if (mom_endzone_outline_enable.GetBool())
+{    if (mom_startzone_outline_enable.GetBool() && (flags & STUDIO_RENDER) != 0 &&
+        (flags & (STUDIO_SHADOWDEPTHTEXTURE | STUDIO_SHADOWDEPTHTEXTURE)) == 0)
     {
-        Color color_endzone;
-        if (g_pMomentumUtil->GetColorFromHex(mom_endzone_color.GetString(), color_endzone))
+        if (g_pMomentumUtil->GetColorFromHex(mom_endzone_color.GetString(), outlineRenderer.outlineColor))
         {
-            MOM_DrawBrushOutlineModel(this, color_endzone);
-
+            render->InstallBrushSurfaceRenderer(&outlineRenderer);
+            BaseClass::DrawModel(STUDIO_RENDER);
+            render->InstallBrushSurfaceRenderer(nullptr);
             if (IsEffectActive(EF_NODRAW))
                 return 1;
         }
