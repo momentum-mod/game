b2a706a673272de90d5800adea7609d5f095f490
Gocnak <Gocnak@users.noreply.github.com>
2018-08-08 23:11:45 -0400

    Added save/load to campaign progress
    Saves to a progress.dat file

--- mp/src/game/server/momentum/mom_system_progress.cpp
+++ mp/src/game/server/momentum/mom_system_progress.cpp
@@ -1,20 +1,25 @@
 #include "cbase.h"
-
+#include "filesystem.h"
 #include "mom_system_progress.h"
+#include "fmtstr.h"
 
 #include "tier0/memdbgon.h"
 
+#define PROGRESS_FILENAME "progress.dat"
 
-CMomentumProgress::CMomentumProgress()
-    : CAutoGameSystem("CMomentumProgress"), m_CurrentWorld(0)
+CMomentumProgress::CMomentumProgress() : CAutoGameSystem("CMomentumProgress"), m_CurrentWorld(0)
 {
 }
 
-void CMomentumProgress::BeatStage(int stage)
+void CMomentumProgress::BeatStage(int stage, int world /*= -1*/)
 {
-    m_progresses[m_CurrentWorld] |= (1 << stage);
+    // Default to current world if out of bounds
+    if (world < 0 || world > 6)
+        world = m_CurrentWorld;
 
-    // MOM_TODO: Save the progress here
+    m_progresses[world] |= (1 << stage);
+
+    Save();
 }
 
 void CMomentumProgress::Reset()
@@ -32,19 +37,43 @@ bool CMomentumProgress::IsStageBeat(int stage, int world /*= -1*/)
     return (m_progresses[world] & (1 << stage)) == (1 << stage);
 }
 
+void CMomentumProgress::Save()
+{
+    KeyValues *pKv = new KeyValues(PROGRESS_FILENAME);
+    pKv->SetInt("CurrentWorld", m_CurrentWorld);
+    for (int i = 0; i < 7; i++)
+    {
+        pKv->SetInt(CFmtStr("World%i", i).Get(), m_progresses[i]);
+    }
+    pKv->SaveToFile(filesystem, PROGRESS_FILENAME, "MOD");
+    pKv->deleteThis();
+}
+
+void CMomentumProgress::Load()
+{
+    KeyValues *pKv = new KeyValues(PROGRESS_FILENAME);
+    if (pKv->LoadFromFile(filesystem, PROGRESS_FILENAME, "MOD"))
+    {
+        m_CurrentWorld = pKv->GetInt("CurrentWorld");
+        for (int i = 0; i < 7; i++)
+            m_progresses[i] = pKv->GetInt(CFmtStr("World%i", i).Get());
+    }
+    pKv->deleteThis();
+}
+
 void CMomentumProgress::PostInit()
 {
-    // MOM_TODO: Load the progress here
+    Load();
 }
 
 void CMomentumProgress::Shutdown()
 {
-    // MOM_TODO: Save the progress here
+    Save();
 }
 
 void CMomentumProgress::LevelShutdownPostEntity()
 {
-
+    Save();
 }
 
 CON_COMMAND(mom_prog_beat_stage, "Tests the Beat Stage code.")
--- mp/src/game/server/momentum/mom_system_progress.h
+++ mp/src/game/server/momentum/mom_system_progress.h
@@ -7,10 +7,15 @@ class CMomentumProgress : public CAutoGameSystem
   public:
     CMomentumProgress();
 
-    void BeatStage(int stage);
+    void BeatStage(int stage, int world = -1);
     void Reset();
 
     bool IsStageBeat(int stage, int world = -1);
+
+    int GetCurrentWorld() const { return m_CurrentWorld; }
+
+    void Save();
+    void Load();
 
 protected:
     void PostInit() OVERRIDE; // Load
@@ -23,6 +28,7 @@ private:
 
     // Keeps track of which stages in which worlds have been beat, using simple bit flags
     short m_progresses[7];//tutorial + worlds 1-5 + bonus world = 7
+    // stored in shorts because ~11 stages fits into 16 bits
 };
 
 extern CMomentumProgress *g_pMomentumProgress;
