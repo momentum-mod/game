03471b7f91a487b14dc8c0b8ac97836ea5187b74
Gocnak <Gocnak@users.noreply.github.com>
2018-11-08 05:00:30 -0500

    Remove helper script for VPC file
    Shoutouts to Python

--- mp/src/public/cryptopp/script.py
+++
@@ -1,7 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-mypath = "C:\\Users\\Nick\\Documents\\GitHub\\game\\mp\\src\\public\\cryptopp"
-
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for file in onlyfiles:
-    print("$File \"$SRCDIR\\public\\cryptopp\\" + file + "\"")
