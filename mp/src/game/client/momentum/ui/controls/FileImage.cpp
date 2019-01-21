66375747f9022fd1316b3921fd3997bce547eaed
Gocnak <Gocnak@users.noreply.github.com>
2019-01-21 05:52:48 -0500

    FileImage optimizations and fixes
    Don't use STB's IO when we can use FileSystem, plus no need for HDR images
    Fixed FileImage not rendering properly due to improper width and draw color

--- mp/src/game/client/momentum/ui/controls/FileImage.cpp
+++ mp/src/game/client/momentum/ui/controls/FileImage.cpp
@@ -7,14 +7,18 @@
 #include "vgui/ISurface.h"
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_STDIO
+#define STBI_NO_HDR
 #include "stb/image/stb_image.h"
 
 #include "tier0/memdbgon.h"
 
 using namespace vgui;
 
-FileImage::FileImage(): m_iX(0), m_iY(0), m_iWide(0), m_iTall(0), m_iRotation(0), m_iTextureID(-1)
+FileImage::FileImage(): m_iX(0), m_iY(0), m_iImageWide(0), m_iDesiredWide(0), m_iImageTall(0), m_iDesiredTall(0),
+                        m_iRotation(0), m_iTextureID(-1)
 {
+    m_DrawColor = Color(255, 255, 255, 255);
 }
 
 FileImage::~FileImage()
@@ -25,11 +29,18 @@ FileImage::~FileImage()
 bool FileImage::LoadFromFile(const char* pFileName, const char* pPathID /* = "GAME"*/)
 {
     // Convert our path to the full drive path for loading
-    char fullPath[MAX_PATH];
-    g_pFullFileSystem->RelativePathToFullPath_safe(pFileName, pPathID, fullPath);
+    CUtlBuffer fileBuf;
+    if (!g_pFullFileSystem->ReadFile(pFileName, pPathID, fileBuf))
+        return false;
+
+    return LoadFromUtlBuffer(fileBuf);
+}
+
+bool FileImage::LoadFromUtlBuffer(CUtlBuffer &buf)
+{
     // Read our image data
     int w, h, channels;
-    unsigned char *pImageData = stbi_load(fullPath, &w, &h, &channels, STBI_rgb_alpha);
+    unsigned char *pImageData = stbi_load_from_memory((stbi_uc*) buf.Base(), buf.TellPut(), &w, &h, &channels, STBI_rgb_alpha);
     if (!pImageData)
         return false;
 
@@ -49,7 +60,15 @@ void FileImage::LoadFromRGBA(const uint8* pData, int wide, int tall)
     // Create our new one
     m_iTextureID = surface()->CreateNewTextureID(true);
     // Image data gets memcpy'd over in this function...
-    surface()->DrawSetTextureRGBA(m_iTextureID, pData, wide, tall, 1, true);
+    surface()->DrawSetTextureRGBAEx(m_iTextureID, pData, wide, tall, IMAGE_FORMAT_RGBA8888);
+    
+    m_iImageWide = wide;
+    m_iImageTall = tall;
+
+    if (m_iDesiredWide == 0)
+        m_iDesiredWide = wide;
+    if (m_iDesiredTall == 0)
+        m_iDesiredTall = tall;
 }
 
 void FileImage::Paint()
@@ -61,13 +80,13 @@ void FileImage::Paint()
 
         if (m_iRotation == 0)
         {
-            surface()->DrawTexturedRect(m_iX, m_iY, m_iX + m_iWide, m_iY + m_iTall);
+            surface()->DrawTexturedRect(m_iX, m_iY, m_iX + m_iDesiredWide, m_iY + m_iDesiredTall);
         }
         else
         {
             // Rotate about the center of the image
             Vertex_t verts[4];
-            Vector2D center(m_iX + (m_iWide * 0.5f), m_iY + (m_iTall * 0.5f));
+            Vector2D center(m_iX + (m_iDesiredWide * 0.5f), m_iY + (m_iDesiredTall * 0.5f));
 
             // Choose a basis...
             float yawRadians = -m_iRotation * M_PI / 180.0f;
@@ -78,20 +97,31 @@ void FileImage::Paint()
             axis[1].y = axis[0].x;
 
             verts[0].m_TexCoord.Init(0, 0);
-            Vector2DMA(center, -0.5f * m_iWide, axis[0], verts[0].m_Position);
-            Vector2DMA(verts[0].m_Position, -0.5f * m_iTall, axis[1], verts[0].m_Position);
+            Vector2DMA(center, -0.5f * m_iDesiredWide, axis[0], verts[0].m_Position);
+            Vector2DMA(verts[0].m_Position, -0.5f * m_iDesiredTall, axis[1], verts[0].m_Position);
 
             verts[1].m_TexCoord.Init(1, 0);
-            Vector2DMA(verts[0].m_Position, m_iWide, axis[0], verts[1].m_Position);
+            Vector2DMA(verts[0].m_Position, m_iDesiredWide, axis[0], verts[1].m_Position);
 
             verts[2].m_TexCoord.Init(1, 1);
-            Vector2DMA(verts[1].m_Position, m_iTall, axis[1], verts[2].m_Position);
+            Vector2DMA(verts[1].m_Position, m_iDesiredTall, axis[1], verts[2].m_Position);
 
             verts[3].m_TexCoord.Init(0, 1);
-            Vector2DMA(verts[0].m_Position, m_iTall, axis[1], verts[3].m_Position);
+            Vector2DMA(verts[0].m_Position, m_iDesiredTall, axis[1], verts[3].m_Position);
 
             surface()->DrawTexturedPolygon(4, verts);
         }
+    }
+}
+
+void FileImage::GetSize(int& wide, int& tall)
+{
+    if (m_iDesiredWide == 0 && m_iDesiredTall == 0)
+        GetContentSize(wide, tall);
+    else
+    {
+        wide = m_iDesiredWide; 
+        tall = m_iDesiredTall;
     }
 }
 
