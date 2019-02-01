16a583bb80249bdc1849035ab056ce18a89a2af2
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-02-02 07:17:24 +1100

    Fix space/tab mixing

--- mp/src/game/client/momentum/ui/ZoneMenu/ZoneMenu.h
+++ mp/src/game/client/momentum/ui/ZoneMenu/ZoneMenu.h
@@ -9,18 +9,18 @@ class ZoneMenu : public vgui::Frame, public CGameEventListener
   public: // vgui::Frame
     DECLARE_CLASS_SIMPLE(ZoneMenu, vgui::Frame);
 
-	ZoneMenu(vgui::Panel *pParentPanel);
+    ZoneMenu(vgui::Panel *pParentPanel);
     virtual void OnMousePressed(vgui::MouseCode code) OVERRIDE;
 
-	MESSAGE_FUNC(OnCreateNewZone, "CreateNewZone");
+    MESSAGE_FUNC(OnCreateNewZone, "CreateNewZone");
     MESSAGE_FUNC(OnDeleteZone, "DeleteZone");
     MESSAGE_FUNC(OnEditZone, "DeleteZone");
 
   public: // CGameEventListener
-	void FireGameEvent(IGameEvent *event) OVERRIDE;
+    void FireGameEvent(IGameEvent *event) OVERRIDE;
 
   public:
-	bool ShouldBindKeys() const { return m_bBindKeys; }
+    bool ShouldBindKeys() const { return m_bBindKeys; }
     int HandleKeyInput(int down, ButtonCode_t keynum);
 
   private:
@@ -31,9 +31,9 @@ class ZoneMenu : public vgui::Frame, public CGameEventListener
     vgui::Button *m_pDeleteZoneButton;
     vgui::Button *m_pEditZoneButton;
 
-	// Whether or not menu should bind mouse/keyboard input to zoning commands
-	bool m_bBindKeys;
-	// Ent index of zone that player is currently standing in (or -1 if not in zone)
+    // Whether or not menu should bind mouse/keyboard input to zoning commands
+    bool m_bBindKeys;
+    // Ent index of zone that player is currently standing in (or -1 if not in zone)
     int m_iCurrentZone;
 };
 
