4aed8369d5a229e8e8d5a52ed359786c0624b58f
Gocnak <Gocnak@users.noreply.github.com>
2019-02-17 07:26:08 -0500

    Initial download progress override panel
    Renders when downloading a specific map, shows a progress bar and can be customized later. Just proof of concept right now, polish coming next time

--- mp/src/game/client/momentum/ui/MapSelection/CMapListPanel.cpp
+++ mp/src/game/client/momentum/ui/MapSelection/CMapListPanel.cpp
@@ -3,6 +3,10 @@
 #include "CMapListPanel.h"
 #include "BaseMapsPage.h"
 #include "vgui/IInput.h"
+#include "mom_map_cache.h"
+
+#include "vgui_controls/ProgressBar.h"
+#include "vgui_controls/URLLabel.h"
 
 #include "tier0/memdbgon.h"
 
@@ -15,6 +19,7 @@ CMapListPanel::CMapListPanel(CBaseMapsPage *pOuter, const char *pName) : BaseCla
 {
     m_pOuter = pOuter;
     SetRowHeight(80);
+    SetDefLessFunc(m_mapDownloads);
 }
 
 //-----------------------------------------------------------------------------
@@ -62,6 +67,22 @@ void CMapListPanel::OnMouseReleased(MouseCode code)
     }
 }
 
+Panel* CMapListPanel::GetCellRenderer(int itemID, int column)
+{
+    // Find the itemID
+    auto indx = m_mapDownloads.Find(itemID);
+    if (m_mapDownloads.IsValidIndex(indx))
+    {
+        char name[16];
+        if (GetColumnHeaderName(column, name, 16) && FStrEq(name, KEYNAME_MAP_NAME))
+        {
+            return m_mapDownloads[indx].m_pOverridePanel;
+        }
+    }
+
+    return BaseClass::GetCellRenderer(itemID, column);
+}
+
 void CMapListPanel::ApplySchemeSettings(IScheme* pScheme)
 {
     BaseClass::ApplySchemeSettings(pScheme);
@@ -69,7 +90,79 @@ void CMapListPanel::ApplySchemeSettings(IScheme* pScheme)
     SetRowHeight(80);
 }
 
-void CMapListPanel::OnSliderMoved()
+void CMapListPanel::SetFont(vgui::HFont font)
 {
-    
+    int oldHeight = GetRowHeight();
+    BaseClass::SetFont(font);
+    SetRowHeight(oldHeight);
+}
+
+void CMapListPanel::MapDownloadStart(KeyValues* pKv, MapDisplay_t* pDisplay)
+{
+    int itemID = pDisplay->m_iListID;
+
+    // First check if we're downloading already
+    auto indx = m_mapDownloads.Find(itemID);
+    if (m_mapDownloads.IsValidIndex(indx))
+    {
+        DevLog("Already downloading!\n");
+        return;
+    }
+
+    int dummy, wide, tall;
+    GetCellBounds(itemID, HEADER_MAP_NAME, dummy, dummy, wide, tall);
+
+    uint64 size = pKv->GetUint64("size");
+    MapDownloadComponent comp;
+    comp.pMap = pDisplay;
+    comp.m_pOverridePanel = new Panel();
+    comp.m_pMapLabel = new Label(comp.m_pOverridePanel, "MapName", pDisplay->m_pMap->m_szMapName);
+    comp.m_pMapLabel->SetFgColor(COLOR_YELLOW);
+    comp.m_pMapLabel->SetAutoTall(true);
+    comp.m_pMapLabel->SetAutoWide(true);
+    comp.m_pMapLabel->DisableMouseInputForThisPanel(true);
+    comp.m_pProgress = new ContinuousProgressBar(comp.m_pOverridePanel, "progress_bar");
+    comp.m_pProgress->PinToSibling(comp.m_pMapLabel,  PIN_TOPLEFT, PIN_BOTTOMLEFT);
+    comp.m_pProgress->SetSize(wide, 20);
+    comp.m_pProgress->DisableMouseInputForThisPanel(true);
+    comp.m_pOverridePanel->DisableMouseInputForThisPanel(true);
+    comp.m_pOverridePanel->InvalidateLayout(true, false);
+    comp.m_ulDownloadSize = size;
+
+    m_mapDownloads.Insert(itemID, comp);
+}
+
+void CMapListPanel::MapDownloadProgress(KeyValues* pKv, MapDisplay_t* pDisplay)
+{
+    auto indx = m_mapDownloads.Find(pDisplay->m_iListID);
+    if (m_mapDownloads.IsValidIndex(indx))
+    {
+        MapDownloadComponent *comp = &m_mapDownloads[indx];
+
+        uint32 offset = pKv->GetInt("offset");
+        uint32 chunkSize = pKv->GetInt("size");
+        offset += chunkSize;
+
+        comp->m_pProgress->SetProgress(double(offset) / double(comp->m_ulDownloadSize));
+    }
+    else
+    {
+        Warning("Map download end with invalid index!\n");
+    }
+}
+
+void CMapListPanel::MapDownloadEnd(KeyValues* pKv, MapDisplay_t* pDisplay)
+{
+    auto indx = m_mapDownloads.Find(pDisplay->m_iListID);
+    if (m_mapDownloads.IsValidIndex(indx))
+    {
+        MapDownloadComponent *comp = &m_mapDownloads[indx];
+        comp->m_pMapLabel->SetFgColor(COLOR_GREEN);
+        comp->m_pOverridePanel->DeletePanel();
+        m_mapDownloads.RemoveAt(indx);
+    }
+    else
+    {
+        Warning("Map download end with invalid index!\n");
+    }
 }
