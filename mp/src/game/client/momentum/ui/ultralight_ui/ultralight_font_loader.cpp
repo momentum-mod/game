b5b20abd48d8357e147f698cdfcd0a2a84552090
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-02-27 13:11:36 +1100

    Add bold/italic fonts support

--- mp/src/game/client/momentum/ui/ultralight_ui/ultralight_font_loader.cpp
+++ mp/src/game/client/momentum/ui/ultralight_ui/ultralight_font_loader.cpp
@@ -10,13 +10,38 @@ ultralight::Ref<ultralight::Buffer> UltralightFontLoader::Load(const ultralight:
                                                                bool italic, float size)
 {
     char filename[MAX_PATH];
-    Q_snprintf(filename, sizeof(filename), "resource/font/%s.ttf", ultralight::String(family).utf8().data());
+    Q_snprintf(filename, sizeof(filename), "resource/font/%s", ultralight::String(family).utf8().data());
 
+    bool bold = (weight >= 700);
+    char stylizedFilename[MAX_PATH];
+
+    if (bold && italic)
+    {
+        Q_snprintf(stylizedFilename, sizeof(stylizedFilename), "%s-BoldItalic.ttf", filename);
+    }
+    else if (bold)
+    {
+        Q_snprintf(stylizedFilename, sizeof(stylizedFilename), "%s-Bold.ttf", filename);
+    }
+    else if (italic)
+    {
+        Q_snprintf(stylizedFilename, sizeof(stylizedFilename), "%s-Italic.ttf", filename);
+    }
+    else
+    {
+        Q_snprintf(stylizedFilename, sizeof(stylizedFilename), "%s-Regular.ttf", filename);
+    }
 
     CUtlBuffer buffer;
-    if (!g_pFullFileSystem->ReadFile(filename, "MOD", buffer))
+    if (!g_pFullFileSystem->ReadFile(stylizedFilename, "MOD", buffer))
     {
-        return ultralight::DefaultFontLoader()->Load("Roboto", weight, italic, size);
+        // Fall back to non-stylized name and try that
+        Q_snprintf(stylizedFilename, sizeof(stylizedFilename), "%s.ttf", filename);
+        if (!g_pFullFileSystem->ReadFile(stylizedFilename, "MOD", buffer))
+        {
+            // Seems like the font just doesn't exist, fall back to Roboto
+            return ultralight::DefaultFontLoader()->Load("Roboto", weight, italic, size);
+        }
     }
 
     return ultralight::Buffer::Create(buffer.Base(), buffer.Size());
