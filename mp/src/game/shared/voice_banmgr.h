160ae38c98928c4ffe44cdf1fbc049c33b52f239
Gocnak <Gocnak@users.noreply.github.com>
2019-03-25 23:14:07 -0400

    Refactor voice_banmgr
    This ban manager seems really, really dated... the functionality may as well have been replaced with Block Communications from Steam?
    Oh well, it doesn't create an empty "voice_ban.dt" file anymore!

--- mp/src/game/shared/voice_banmgr.h
+++ mp/src/game/shared/voice_banmgr.h
@@ -1,54 +1,26 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose:
 //
 // $NoKeywords: $
 //=============================================================================//
 
-#ifndef VOICE_BANMGR_H
-#define VOICE_BANMGR_H
-#ifdef _WIN32
 #pragma once
-#endif
-
 
 // This class manages the (persistent) list of squelched players.
 class CVoiceBanMgr
 {
 public:
+    // Init loads the list of squelched players from disk.
+    bool Init();
+    // Saves the state into voice_ban.dt.
+    void Save();
 
-				CVoiceBanMgr();
-				~CVoiceBanMgr();	
+    bool GetPlayerBan(uint32 playerID) const;
+    void SetPlayerBan(uint32 playerID, bool bSquelch);
 
-	// Init loads the list of squelched players from disk.
-	bool		Init(const char *pGameDir);
-	void		Term();
+  protected:
+    void Clear();
 
-	// Saves the state into voice_squelch.dt.
-	void		SaveState(const char *pGameDir);
-
-	bool		GetPlayerBan(char const playerID[SIGNED_GUID_LEN]);
-	void		SetPlayerBan(char const playerID[SIGNED_GUID_LEN], bool bSquelch);
-
-
-protected:
-
-	class BannedPlayer
-	{
-	public:
-		char			m_PlayerID[SIGNED_GUID_LEN];
-		BannedPlayer	*m_pPrev, *m_pNext;
-	};
-
-	void			Clear();
-	BannedPlayer*	InternalFindPlayerSquelch(char const playerID[SIGNED_GUID_LEN]);
-	BannedPlayer*	AddBannedPlayer(char const playerID[SIGNED_GUID_LEN]);
-
-
-protected:
-
-	BannedPlayer	m_PlayerHash[256];
+    CUtlVector<uint32> m_BannedPlayers;
 };
-
-
-#endif // VOICE_BANMGR_H
