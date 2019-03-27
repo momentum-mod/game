20adaf81d4a319a7d3ecb51c37801817e0d1aa93
Gocnak <Gocnak@users.noreply.github.com>
2019-03-27 01:02:27 -0400

    Make pre_level_init fire only to Client DLL
    So that it doesn't print out an error; it's firing only for client currently anyways

--- mp/src/game/server/momentum/mom_gameinterface.cpp
+++ mp/src/game/server/momentum/mom_gameinterface.cpp
@@ -27,5 +27,5 @@ void CServerGameDLL::LevelInit_ParseAllEntities(const char *pMapEntities)
 void CServerGameDLL::PrepareLevelResources( /* in/out */ char *pszMapName, size_t nMapNameSize,
                                            /* in/out */ char *pszMapFile, size_t nMapFileSize)
 {
-    g_pModuleComms->FireEvent(new KeyValues("pre_level_init", "map", pszMapName, "file", pszMapFile));
+    g_pModuleComms->FireEvent(new KeyValues("pre_level_init", "map", pszMapName, "file", pszMapFile), FIRE_FOREIGN_ONLY);
 }
