b3cc5757595e8d5f0f98881c3e03e948a38e421b
Gocnak <Gocnak@users.noreply.github.com>
2019-04-02 00:13:45 -0400

    Reset important zone building convars on level shutdown

--- mp/src/game/server/momentum/mapzones_edit.h
+++ mp/src/game/server/momentum/mapzones_edit.h
@@ -14,6 +14,7 @@ class CMomZoneEdit : public CAutoGameSystemPerFrame
     void StopEditing();
 
     virtual void LevelInitPostEntity() OVERRIDE;
+    virtual void LevelShutdownPostEntity() OVERRIDE;
     virtual void FrameUpdatePostEntityThink() OVERRIDE;
 
     void OnCreate(int zonetype = -1);
