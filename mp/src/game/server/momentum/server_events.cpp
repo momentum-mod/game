33de13f6c3c10276ba23579bd49cabba01031436
Gocnak <Gocnak@users.noreply.github.com>
2019-10-14 22:42:30 -0400

    Unload extra concommands
    Some of these are in engine, and unless we get engine, we can't remove them. Best we can do for now.

--- mp/src/game/server/momentum/server_events.cpp
+++ mp/src/game/server/momentum/server_events.cpp
@@ -35,6 +35,13 @@ bool CMomServerEvents::Init()
         g_pFullFileSystem->FindClose(handle);
 
         UnloadConVarOrCommand("plugin_load");
+        UnloadConVarOrCommand("host_limitlocal");
+        UnloadConVarOrCommand("net_fakelag");
+        UnloadConVarOrCommand("net_fakeloss");
+        UnloadConVarOrCommand("net_droppackets");
+        UnloadConVarOrCommand("net_fakejitter");
+        UnloadConVarOrCommand("singlestep");
+        UnloadConVarOrCommand("host_sleep");
 
         // Unregister FCVAR_MAPPING convars
         auto pCvar = g_pCVar->GetCommands();
