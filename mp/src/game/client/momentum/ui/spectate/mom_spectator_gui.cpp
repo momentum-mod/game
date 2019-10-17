77b9e8b11987408d357ec3c6ed0f856d65b18eda
Flipybitz <flipybitz@gmail.com>
2019-10-16 23:34:43 -0500

    Removed "Player: " string
    Changed localization to remove the string "Player: " from the player display

--- mp/src/game/client/momentum/ui/spectate/mom_spectator_gui.cpp
+++ mp/src/game/client/momentum/ui/spectate/mom_spectator_gui.cpp
@@ -62,7 +62,7 @@ CMOMSpectatorGUI::CMOMSpectatorGUI(IViewPort *pViewPort) : EditablePanel(nullptr
     SetScheme("ClientScheme");
 
     m_pTopBar = new Panel(this, "TopBar");
-    m_pPlayerLabel = new Label(this, "PlayerLabel", "#MOM_ReplayPlayer");
+    m_pPlayerLabel = new Label(this, "PlayerLabel", "");//set below on line 283
     m_pReplayLabel = new Label(this, "ReplayLabel", "#MOM_WatchingReplay");
     m_pTimeLabel = new Label(this, "TimeLabel", "#MOM_MF_RunTime");
     m_pGainControlLabel = new Label(this, "DetachInfo", "#MOM_SpecGUI_GainControl");
@@ -83,7 +83,6 @@ CMOMSpectatorGUI::CMOMSpectatorGUI(IViewPort *pViewPort) : EditablePanel(nullptr
     SetMouseInputEnabled(false);
     InvalidateLayout();
 
-    FIND_LOCALIZATION(m_pwReplayPlayer, "#MOM_ReplayPlayer");
     FIND_LOCALIZATION(m_pwGainControl, "#MOM_SpecGUI_GainControl");
     FIND_LOCALIZATION(m_pwRunTime, "#MOM_MF_RunTime");
     FIND_LOCALIZATION(m_pwSpecMap, "#Spec_Map");
@@ -281,7 +280,7 @@ void CMOMSpectatorGUI::Update()
             // Current player name
             wchar_t wPlayerName[BUFSIZELOCL];
             ANSI_TO_UNICODE(pGhost->m_szGhostName, wPlayerName);
-            m_pPlayerLabel->SetText(CConstructLocalizedString(m_pwReplayPlayer, wPlayerName));
+            m_pPlayerLabel->SetText(wPlayerName);
 
             if (pEnt->GetEntType() == RUN_ENT_REPLAY)
             {
