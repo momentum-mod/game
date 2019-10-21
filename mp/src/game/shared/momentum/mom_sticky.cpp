5efb390ec95ba1fb5968d94129e4c3bfdca82130
bonjorno7 <jorijndegraaf@gmail.com>
2019-10-21 22:02:23 +0200

    Add sticky laucher and sticky bomb files

---
+++ mp/src/game/shared/momentum/mom_sticky.cpp
@@ -0,0 +1,306 @@
+#include "cbase.h"
+#include "mom_rocket.h"
+
+#ifndef CLIENT_DLL
+#include "Sprite.h"
+#include "explode.h"
+#include "momentum/mom_triggers.h"
+#endif
+
+#include "tier0/memdbgon.h"
+
+#define MOM_ROCKET_RADIUS 146.0f
+#define MOM_ROCKET_SPEED 1100.0f
+
+#ifndef CLIENT_DLL
+
+BEGIN_DATADESC(CMomRocket)
+    // Fields
+    DEFINE_FIELD(m_hOwner, FIELD_EHANDLE),
+    DEFINE_FIELD(m_hRocketTrail, FIELD_EHANDLE),
+    DEFINE_FIELD(m_flDamage, FIELD_FLOAT),
+
+    // Functions
+    DEFINE_ENTITYFUNC(Touch),
+END_DATADESC();
+#endif
+
+IMPLEMENT_NETWORKCLASS_ALIASED(MomRocket, DT_MomRocket)
+
+BEGIN_NETWORK_TABLE(CMomRocket, DT_MomRocket)
+#ifdef CLIENT_DLL
+RecvPropVector(RECVINFO(m_vInitialVelocity))
+#else
+SendPropVector(SENDINFO(m_vInitialVelocity),
+               20,    // nbits
+               0,     // flags
+               -3000, // low value
+               3000   // high value
+               )
+#endif
+END_NETWORK_TABLE();
+
+LINK_ENTITY_TO_CLASS(momentum_rocket, CMomRocket);
+PRECACHE_WEAPON_REGISTER(momentum_rocket);
+
+CMomRocket::CMomRocket()
+{
+    m_vInitialVelocity.Init();
+
+#ifdef CLIENT_DLL
+    m_flSpawnTime = 0.0f;
+#else
+    m_flDamage = 0.0f;
+    m_hOwner = nullptr;
+    m_hRocketTrail = nullptr;
+#endif
+}
+
+#ifdef CLIENT_DLL
+
+void CMomRocket::PostDataUpdate(DataUpdateType_t type)
+{
+    BaseClass::PostDataUpdate(type);
+
+    if (type == DATA_UPDATE_CREATED)
+    {
+        // Now stick our initial velocity into the interpolation history
+        CInterpolatedVar<Vector> &interpolator = GetOriginInterpolator();
+        interpolator.ClearHistory();
+
+        CInterpolatedVar<QAngle> &rotInterpolator = GetRotationInterpolator();
+        rotInterpolator.ClearHistory();
+
+        float changeTime = GetLastChangeTime(LATCH_SIMULATION_VAR);
+
+        // Add a sample 1 second back.
+        Vector vCurOrigin = GetLocalOrigin() - m_vInitialVelocity;
+        interpolator.AddToHead(changeTime - 1.0f, &vCurOrigin, false);
+
+        QAngle vCurAngles = GetLocalAngles();
+        rotInterpolator.AddToHead(changeTime - 1.0f, &vCurAngles, false);
+
+        // Add the current sample.
+        vCurOrigin = GetLocalOrigin();
+        interpolator.AddToHead(changeTime, &vCurOrigin, false);
+
+        rotInterpolator.AddToHead(changeTime, &vCurAngles, false);
+    }
+}
+
+int CMomRocket::DrawModel(int flags)
+{
+    // Don't draw rocket during the first 0.2 seconds.
+    if (gpGlobals->curtime - m_flSpawnTime < 0.2f)
+    {
+        return 0;
+    }
+
+    return BaseClass::DrawModel(flags);
+}
+
+void CMomRocket::Spawn()
+{
+    m_flSpawnTime = gpGlobals->curtime;
+    BaseClass::Spawn();
+}
+
+#else
+
+void CMomRocket::Spawn()
+{
+    BaseClass::Spawn();
+
+    UseClientSideAnimation();
+    SetCollisionGroup(COLLISION_GROUP_PROJECTILE);
+    SetSolidFlags(FSOLID_NOT_STANDABLE);
+    SetMoveType(MOVETYPE_FLY, MOVECOLLIDE_FLY_CUSTOM);
+    SetSolid(SOLID_BBOX);
+    AddEFlags(EFL_NO_WATER_VELOCITY_CHANGE);
+    AddEffects(EF_NOSHADOW);
+    SetSize(Vector(0, 0, 0), Vector(0, 0, 0));
+    AddFlag(FL_GRENADE);
+
+    m_takedamage = DAMAGE_NO;
+    SetGravity(0.0f);
+
+    SetTouch(&CMomRocket::RocketTouch);
+    SetNextThink(gpGlobals->curtime);
+}
+
+void CMomRocket::Precache()
+{
+    BaseClass::Precache();
+    // MOM_TODO:
+    // Replace HL2 missile model
+    PrecacheModel("models/weapons/w_missile.mdl");
+    PrecacheScriptSound("Missile.Ignite");
+}
+
+void CMomRocket::SetupInitialTransmittedGrenadeVelocity(const Vector &velocity) { m_vInitialVelocity = velocity; }
+
+void CMomRocket::Destroy(bool bNoGrenadeZone)
+{
+    SetThink(&BaseClass::SUB_Remove);
+    SetNextThink(gpGlobals->curtime);
+    SetTouch(NULL);
+    AddEffects(EF_NODRAW);
+    StopSound("Missile.Ignite");
+    DestroyTrail();
+
+    if (bNoGrenadeZone)
+    {
+        CSprite *pGlowSprite = CSprite::SpriteCreate(NOGRENADE_SPRITE, GetAbsOrigin(), false);
+        if (pGlowSprite)
+        {
+            pGlowSprite->SetTransparency(kRenderGlow, 255, 255, 255, 255, kRenderFxFadeFast);
+            pGlowSprite->SetThink(&CSprite::SUB_Remove);
+            pGlowSprite->SetNextThink(gpGlobals->curtime + 1.0);
+        }
+    }
+}
+
+void CMomRocket::DestroyTrail()
+{
+    if (m_hRocketTrail)
+    {
+        m_hRocketTrail->SetLifetime(0.1f);
+        m_hRocketTrail->SetParent(nullptr);
+        m_hRocketTrail = nullptr;
+    }
+}
+
+void CMomRocket::Explode(trace_t *pTrace, CBaseEntity *pOther)
+{
+    if (CNoGrenadesZone::IsInsideNoGrenadesZone(this))
+    {
+        Destroy(true);
+        return;
+    }
+
+    // Make invisible
+    SetModelName(NULL_STRING);
+    SetSolid(SOLID_NONE);
+    m_takedamage = DAMAGE_NO;
+
+    // Pull out a bit
+    if (!CloseEnough(pTrace->fraction, 1.0f))
+    {
+        SetAbsOrigin(pTrace->endpos + (pTrace->plane.normal * 1.0f));
+    }
+
+    Vector vecOrigin = GetAbsOrigin();
+    CBaseEntity *pOwner = GetOwnerEntity();
+
+    float flDamage = GetDamage();
+    float flRadius = GetRadius();
+
+    // Create explosion effect with no damage
+    ExplosionCreate(vecOrigin, GetAbsAngles(), pOwner, flDamage, flRadius, false);
+
+    // Damage
+    CTakeDamageInfo info(this, pOwner, vec3_origin, vecOrigin, flDamage, GetDamageType());
+    RadiusDamage(info, vecOrigin, flRadius, CLASS_NONE, nullptr);
+
+    DestroyTrail();
+
+    m_hOwner = nullptr;
+
+    StopSound("Missile.Ignite");
+
+    if (!pOther->IsPlayer())
+    {
+        UTIL_DecalTrace(pTrace, "Scorch");
+    }
+
+    // Remove the rocket
+    UTIL_Remove(this);
+}
+
+void CMomRocket::RocketTouch(CBaseEntity *pOther)
+{
+    Assert(pOther);
+
+    // Don't touch triggers
+    if (pOther->IsSolidFlagSet(FSOLID_TRIGGER | FSOLID_VOLUME_CONTENTS))
+        return;
+
+    // Handle hitting skybox (disappear).
+    const trace_t *pTrace = &GetTouchTrace();
+    if (pTrace->surface.flags & SURF_SKY)
+    {
+        DestroyTrail();
+
+        m_hOwner = nullptr;
+        StopSound("Missile.Ignite");
+        UTIL_Remove(this);
+        return;
+    }
+
+    // Explode
+    trace_t trace;
+    memcpy(&trace, pTrace, sizeof(trace_t));
+    Explode(&trace, pOther);
+}
+
+void CMomRocket::CreateSmokeTrail()
+{
+    if (m_hRocketTrail)
+        return;
+
+    // Smoke trail.
+    if ((m_hRocketTrail = RocketTrail::CreateRocketTrail()) != NULL)
+    {
+        m_hRocketTrail->m_Opacity = 0.2f;
+        m_hRocketTrail->m_SpawnRate = 100;
+        m_hRocketTrail->m_ParticleLifetime = 0.5f;
+        m_hRocketTrail->m_StartColor.Init(0.65f, 0.65f, 0.65f);
+        m_hRocketTrail->m_EndColor.Init(0.0, 0.0, 0.0);
+        m_hRocketTrail->m_StartSize = 8;
+        m_hRocketTrail->m_EndSize = 32;
+        m_hRocketTrail->m_SpawnRadius = 4;
+        m_hRocketTrail->m_MinSpeed = 2;
+        m_hRocketTrail->m_MaxSpeed = 16;
+
+        m_hRocketTrail->SetLifetime(999);
+        m_hRocketTrail->FollowEntity(this, "0");
+    }
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Spawn a new rocket
+//
+// Input  : &vecOrigin -
+//          &vecAngles -
+//          *pentOwner -
+//
+// Output : CMomRocket
+//-----------------------------------------------------------------------------
+CMomRocket *CMomRocket::EmitRocket(const Vector &vecOrigin, const QAngle &vecAngles, CBaseEntity *pentOwner /*= nullptr*/)
+{
+    CMomRocket *pRocket = static_cast<CMomRocket *>(CreateNoSpawn("momentum_rocket", vecOrigin, vecAngles, pentOwner));
+    pRocket->SetModel("models/weapons/w_missile.mdl");
+    DispatchSpawn(pRocket);
+
+    Vector vecForward;
+    AngleVectors(vecAngles, &vecForward);
+
+    const Vector velocity = vecForward * MOM_ROCKET_SPEED;
+    pRocket->SetAbsVelocity(velocity);
+    pRocket->SetupInitialTransmittedGrenadeVelocity(velocity);
+    pRocket->SetThrower(pentOwner);
+
+    QAngle angles;
+    VectorAngles(velocity, angles);
+    pRocket->SetAbsAngles(angles);
+
+    pRocket->SetDamage(90.0f);
+    // NOTE: Rocket explosion radius is 146.0f in TF2, but 121.0f is used for self damage (see RadiusDamage in mom_gamerules)
+    pRocket->SetRadius(146.0f);
+
+    pRocket->CreateSmokeTrail();
+    pRocket->EmitSound("Missile.Ignite");
+
+    return pRocket;
+}
+#endif
---
+++ mp/src/game/shared/momentum/weapon/weapon_mom_stickylauncher.cpp
@@ -0,0 +1,144 @@
+#include "cbase.h"
+
+#include "mom_player_shared.h"
+#include "mom_system_gamemode.h"
+#include "weapon_mom_rocketlauncher.h"
+
+#include "tier0/memdbgon.h"
+
+IMPLEMENT_NETWORKCLASS_ALIASED(MomentumRocketLauncher, DT_MomentumRocketLauncher)
+
+BEGIN_NETWORK_TABLE(CMomentumRocketLauncher, DT_MomentumRocketLauncher)
+END_NETWORK_TABLE()
+
+BEGIN_PREDICTION_DATA(CMomentumRocketLauncher)
+END_PREDICTION_DATA()
+
+LINK_ENTITY_TO_CLASS(weapon_momentum_rocketlauncher, CMomentumRocketLauncher);
+PRECACHE_WEAPON_REGISTER(weapon_momentum_rocketlauncher);
+
+#ifdef GAME_DLL
+static MAKE_TOGGLE_CONVAR(mom_rj_center_fire, "0", FCVAR_ARCHIVE,
+                          "If enabled, all rockets will be fired from the center of the screen. 0 = OFF, 1 = ON\n");
+#endif
+
+CMomentumRocketLauncher::CMomentumRocketLauncher()
+{
+    m_flTimeToIdleAfterFire = 0.8f;
+    m_flIdleInterval = 20.0f;
+}
+
+void CMomentumRocketLauncher::Precache()
+{
+    BaseClass::Precache();
+
+#ifndef CLIENT_DLL
+    UTIL_PrecacheOther("momentum_rocket");
+#endif
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Return the origin & angles for a projectile fired from the player's gun
+//-----------------------------------------------------------------------------
+void CMomentumRocketLauncher::GetProjectileFireSetup(CMomentumPlayer *pPlayer, Vector vecOffset, Vector *vecSrc, QAngle *angForward)
+{
+#ifdef GAME_DLL
+    static ConVarRef cl_righthand("cl_righthand");
+#else
+    extern ConVar cl_righthand;
+    static ConVarRef mom_rj_center_fire("mom_rj_center_fire");
+#endif
+
+    if (mom_rj_center_fire.GetBool())
+    {
+        vecOffset.y = 0.0f;
+    }
+    else if (!cl_righthand.GetBool())
+    {
+        vecOffset.y *= -1.0f;
+    }
+
+    Vector vecForward, vecRight, vecUp;
+    AngleVectors(pPlayer->EyeAngles(), &vecForward, &vecRight, &vecUp);
+
+    Vector vecShootPos = pPlayer->Weapon_ShootPosition();
+
+    // Estimate end point
+    Vector endPos = vecShootPos + vecForward * 2000.0f;
+
+    // Trace forward and find what's in front of us, and aim at that
+    trace_t tr;
+
+    CTraceFilterSimple filter(pPlayer, COLLISION_GROUP_NONE);
+    UTIL_TraceLine(vecShootPos, endPos, MASK_SOLID, &filter, &tr);
+
+    // Offset actual start point
+    *vecSrc = vecShootPos + (vecForward * vecOffset.x) + (vecRight * vecOffset.y) + (vecUp * vecOffset.z);
+
+    // Find angles that will get us to our desired end point
+    // Only use the trace end if it wasn't too close, which results
+    // in visually bizarre forward angles
+    if (tr.fraction > 0.1f)
+    {
+        VectorAngles(tr.endpos - *vecSrc, *angForward);
+    }
+    else
+    {
+        VectorAngles(endPos - *vecSrc, *angForward);
+    }
+}
+
+void CMomentumRocketLauncher::RocketLauncherFire()
+{
+    CMomentumPlayer *pPlayer = GetPlayerOwner();
+
+    if (!pPlayer)
+        return;
+
+    m_flNextPrimaryAttack = m_flNextSecondaryAttack = gpGlobals->curtime + 0.8f;
+    SetWeaponIdleTime(gpGlobals->curtime + m_flTimeToIdleAfterFire);
+    pPlayer->m_iShotsFired++;
+
+    DoFireEffects();
+    WeaponSound(SINGLE);
+
+    // MOM_FIXME:
+    // Should no longer Assert, unsure about BaseGunFire() though
+    SendWeaponAnim(ACT_VM_PRIMARYATTACK);
+
+    // player "shoot" animation
+    pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+#ifdef GAME_DLL
+    Vector vForward, vRight, vUp;
+
+    pPlayer->EyeVectors(&vForward, &vRight, &vUp);
+
+    // Offset values from
+    // https://github.com/NicknineTheEagle/TF2-Base/blob/master/src/game/shared/tf/tf_weaponbase_gun.cpp#L334
+    Vector vecOffset(23.5f, 12.0f, -3.0f);
+    if (pPlayer->GetFlags() & FL_DUCKING)
+    {
+        vecOffset.z = 8.0f;
+    }
+    Vector vecSrc;
+    QAngle angForward;
+    GetProjectileFireSetup(pPlayer, vecOffset, &vecSrc, &angForward);
+
+    trace_t trace;
+    CTraceFilterSimple traceFilter(this, COLLISION_GROUP_NONE);
+    UTIL_TraceLine(pPlayer->EyePosition(), vecSrc, MASK_SOLID_BRUSHONLY, &traceFilter, &trace);
+
+    CMomRocket::EmitRocket(trace.endpos, angForward, pPlayer);
+#endif
+}
+
+void CMomentumRocketLauncher::PrimaryAttack() { RocketLauncherFire(); }
+
+bool CMomentumRocketLauncher::CanDeploy()
+{
+    if (!g_pGameModeSystem->GameModeIs(GAMEMODE_RJ))
+        return false;
+
+    return BaseClass::CanDeploy();
+}
