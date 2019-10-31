7b4aad7233842a4dd2e86394b21e4dc67d01f3e7
Hat Kid <lozanoski.philip779@googlemail.com>
2019-10-31 02:44:27 +0100

    Removed unused variables, renamed variables to be more descriptive

--- mp/src/game/client/momentum/ui/HUD/hud_keypress.cpp
+++ mp/src/game/client/momentum/ui/HUD/hud_keypress.cpp
@@ -77,8 +77,6 @@ class CHudKeyPressDisplay : public CHudElement, public Panel
     wchar_t m_pwM2[BUFSIZELOCL];
     float m_fJumpColorUntil;
     float m_fDuckColorUntil;
-    float m_fM1ColorUntil;
-    float m_fM2ColorUntil;
 };
 
 DECLARE_HUDELEMENT(CHudKeyPressDisplay);
@@ -232,15 +230,15 @@ void CHudKeyPressDisplay::Paint()
     if (nButtons & IN_ATTACK)
     {
         CHECK_INPUT_P(IN_ATTACK);
-        int text_m1 = GetTextCenter(m_hTextFont, m_pwM1) - (UTIL_ComputeStringWidth(m_hTextFont, m_pwM1)) * 3.5;
-        surface()->DrawSetTextPos(text_m1, top_row_ypos);
+        int xposM1 = GetTextCenter(m_hTextFont, m_pwM1) - (UTIL_ComputeStringWidth(m_hTextFont, m_pwM1)) * 3.5;
+        surface()->DrawSetTextPos(xposM1, top_row_ypos);
         surface()->DrawPrintText(m_pwM1, wcslen(m_pwM1));
     }
     if (nButtons & IN_ATTACK2)
     {
         CHECK_INPUT_P(IN_ATTACK2);
-        int text_m2 = GetTextCenter(m_hTextFont, m_pwM2) + (UTIL_ComputeStringWidth(m_hTextFont, m_pwM2)) * 2.75;
-        surface()->DrawSetTextPos(text_m2, top_row_ypos);
+        int xposM2 = GetTextCenter(m_hTextFont, m_pwM2) + (UTIL_ComputeStringWidth(m_hTextFont, m_pwM2)) * 2.75;
+        surface()->DrawSetTextPos(xposM2, top_row_ypos);
         surface()->DrawPrintText(m_pwM2, wcslen(m_pwM2));
     }
     // ----------
@@ -354,11 +352,11 @@ void CHudKeyPressDisplay::DrawKeyTemplates()
     surface()->DrawSetTextPos(GetTextCenter(m_hWordTextFont, m_pwDuck), duck_row_ypos);
     surface()->DrawPrintText(m_pwDuck, wcslen(m_pwDuck));
     CHECK_INPUT_N(IN_ATTACK);
-    int text_m1 = GetTextCenter(m_hTextFont, m_pwM1) - (UTIL_ComputeStringWidth(m_hTextFont, m_pwM1)) * 3.5;
-    surface()->DrawSetTextPos(text_m1, top_row_ypos);
+    int xposM1 = GetTextCenter(m_hTextFont, m_pwM1) - (UTIL_ComputeStringWidth(m_hTextFont, m_pwM1)) * 3.5;
+    surface()->DrawSetTextPos(xposM1, top_row_ypos);
     surface()->DrawPrintText(m_pwM1, wcslen(m_pwM1));
     CHECK_INPUT_N(IN_ATTACK2);
-    int text_m2 = GetTextCenter(m_hTextFont, m_pwM2) + (UTIL_ComputeStringWidth(m_hTextFont, m_pwM2)) * 2.75;
-    surface()->DrawSetTextPos(text_m2, top_row_ypos);
+    int xposM2 = GetTextCenter(m_hTextFont, m_pwM2) + (UTIL_ComputeStringWidth(m_hTextFont, m_pwM2)) * 2.75;
+    surface()->DrawSetTextPos(xposM2, top_row_ypos);
     surface()->DrawPrintText(m_pwM2, wcslen(m_pwM2));
 }
