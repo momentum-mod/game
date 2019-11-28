54af77b58f66a64dafbbd1175db74b96e2893989
Hat Kid <lozanoski.philip779@googlemail.com>
2019-11-28 10:45:09 +0100

    Add TF2 server fx for explosions and particles to fx_mom_shared

--- mp/src/game/shared/momentum/fx_mom_shared.h
+++ mp/src/game/shared/momentum/fx_mom_shared.h
@@ -1,7 +1,30 @@
 #pragma once
 
+#ifndef CLIENT_DLL
+#include "particle_parse.h"
+#include "weapon/mom_weapon_parse.h"
+#endif
+
 // This runs on both the client and the server.
 // On the server, it only does the damage calculations.
 // On the client, it does all the effects.
 void FX_FireBullets(int iEntIndex, const Vector &vOrigin, const QAngle &vAngles, int iWeaponID, int iMode, int iSeed,
                     float flSpread);
+
+#ifndef CLIENT_DLL
+void TE_TFExplosion(IRecipientFilter &filter, float flDelay, const Vector &vecOrigin, const Vector &vecNormal,
+                    CWeaponID iWeaponID, int nEntIndex);
+
+void TE_TFParticleEffect(IRecipientFilter &filter, float flDelay, const char *pszParticleName,
+                         ParticleAttachment_t iAttachType, CBaseEntity *pEntity, const char *pszAttachmentName,
+                         bool bResetAllParticlesOnEntity = false);
+void TE_TFParticleEffect(IRecipientFilter &filter, float flDelay, const char *pszParticleName,
+                         ParticleAttachment_t iAttachType, CBaseEntity *pEntity = NULL, int iAttachmentPoint = -1,
+                         bool bResetAllParticlesOnEntity = false);
+void TE_TFParticleEffect(IRecipientFilter &filter, float flDelay, const char *pszParticleName, Vector vecOrigin,
+                         QAngle vecAngles, CBaseEntity *pEntity = NULL, int iAttachType = PATTACH_CUSTOMORIGIN);
+void TE_TFParticleEffect(IRecipientFilter &filter, float flDelay, const char *pszParticleName, Vector vecOrigin,
+                         Vector vecStart, QAngle vecAngles, CBaseEntity *pEntity = NULL);
+void TE_TFParticleEffect(IRecipientFilter &filter, float flDelay, int iEffectIndex, Vector vecOrigin, Vector vecStart,
+                         QAngle vecAngles, CBaseEntity *pEntity = NULL);
+#endif
