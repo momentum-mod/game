ccdde288f312987f2fd102ebb039ae527713a7fe
Hat Kid <lozanoski.philip779@googlemail.com>
2019-12-16 05:07:47 +0100

    Add Rocket Jump settings panel source and header files and add to VPC script

---
+++ mp/src/game/client/momentum/ui/SettingsPanel/RocketJumpSettingsPage.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "SettingsPage.h"
+
+class RocketJumpSettingsPage : public SettingsPage
+{
+    DECLARE_CLASS_SIMPLE(RocketJumpSettingsPage, SettingsPage);
+
+    RocketJumpSettingsPage(Panel *pParent);
+    ~RocketJumpSettingsPage();
+
+    void OnApplyChanges() OVERRIDE;
+    void LoadSettings() OVERRIDE;
+
+  private:
+    vgui::ComboBox *m_pParticlesBox, *m_pSoundsBox, *m_pTrailBox;
+
+    vgui::CvarToggleCheckButton *m_pEnableTFRocketModel, *m_pEnableTFViewModel, *m_pEnableCenterFire;
+};
