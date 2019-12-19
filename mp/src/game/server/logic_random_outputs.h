9068c93ea04adc91c3fca442a12fbde3a75be4cc
xen <24222257+xen-000@users.noreply.github.com>
2019-12-19 02:57:42 +0200

    Whitespace

--- mp/src/game/server/logic_random_outputs.h
+++ mp/src/game/server/logic_random_outputs.h
@@ -12,32 +12,32 @@
 class CLogicRandomOutputs : public CLogicalEntity
 {
 public:
-	DECLARE_CLASS( CLogicRandomOutputs, CLogicalEntity );
+    DECLARE_CLASS( CLogicRandomOutputs, CLogicalEntity );
 
-	CLogicRandomOutputs();
+    CLogicRandomOutputs();
 
-	void Activate();
-	void Think();
-	virtual bool KeyValue( const char *szKeyName, const char *szValue );
+    void Activate();
+    void Think();
+    virtual bool KeyValue( const char *szKeyName, const char *szValue );
 
-	// Input handlers
-	void InputEnable( inputdata_t &inputdata );
-	void InputEnableRefire( inputdata_t &inputdata );  // Private input handler, not in FGD
-	void InputDisable( inputdata_t &inputdata );
-	void InputToggle( inputdata_t &inputdata );
-	void InputTrigger( inputdata_t &inputdata );
-	void InputCancelPending( inputdata_t &inputdata );
+    // Input handlers
+    void InputEnable( inputdata_t &inputdata );
+    void InputEnableRefire( inputdata_t &inputdata );  // Private input handler, not in FGD
+    void InputDisable( inputdata_t &inputdata );
+    void InputToggle( inputdata_t &inputdata );
+    void InputTrigger( inputdata_t &inputdata );
+    void InputCancelPending( inputdata_t &inputdata );
 
-	DECLARE_DATADESC();
+    DECLARE_DATADESC();
 
-	// Outputs
-	COutputEvent m_Output[ NUM_RANDOM_OUTPUTS ];
-	COutputEvent m_OnSpawn;
+    // Outputs
+    COutputEvent m_Output[ NUM_RANDOM_OUTPUTS ];
+    COutputEvent m_OnSpawn;
 
-	float m_flOnTriggerChance[ NUM_RANDOM_OUTPUTS + 1 ];	// DEFINE_AUTO_ARRAY fills this array from index 1
-	
+    float m_flOnTriggerChance[ NUM_RANDOM_OUTPUTS + 1 ];    // DEFINE_AUTO_ARRAY fills this array from index 1
+    
 private:
 
-	bool m_bDisabled;
-	bool m_bWaitForRefire;			// Set to disallow a refire while we are waiting for our outputs to finish firing.
+    bool m_bDisabled;
+    bool m_bWaitForRefire;            // Set to disallow a refire while we are waiting for our outputs to finish firing.
 };
