845a834cfe46ecd0fb95a7134a2778b3982b6849
Gocnak <Gocnak@users.noreply.github.com>
2020-01-19 03:59:58 -0500

    Move TFExplosionCallback logic back in class
    No need for it to be a global/extra func if nothing else calls it
    This also makes it use the new refactor code, particles and sounds from the rocket launcher

--- mp/src/game/client/momentum/fx_mom_explosions.cpp
+++ mp/src/game/client/momentum/fx_mom_explosions.cpp
@@ -1,75 +1,13 @@
 #include "cbase.h"
 #include "c_basetempentity.h"
 #include "engine/IEngineSound.h"
-#include "weapon/weapon_base.h"
+#include "weapon/weapon_def.h"
 #include "mom_shareddefs.h"
 
 #include "tier0/memdbgon.h"
 
-void TFExplosionCallback(const Vector &vecOrigin, const Vector &vecNormal, WeaponID_t iWeaponID)
-{
-    const auto pWeaponInfo = g_pWeaponDef->GetWeaponScript(iWeaponID);
-
-    // Calculate the angles, given the normal.
-    bool bIsWater = (UTIL_PointContents(vecOrigin) & CONTENTS_WATER);
-    bool bInAir = false;
-    QAngle angExplosion(0.0f, 0.0f, 0.0f);
-
-    // Cannot use zeros here because we are sending the normal at a smaller bit size.
-    if (fabs(vecNormal.x) < 0.05f && fabs(vecNormal.y) < 0.05f && fabs(vecNormal.z) < 0.05f)
-    {
-        bInAir = true;
-        angExplosion.Init();
-    }
-    else
-    {
-        VectorAngles(vecNormal, angExplosion);
-        bInAir = false;
-    }
-
-    static ConVarRef mom_rj_particles("mom_rj_particles");
-    static ConVarRef mom_rj_sounds("mom_rj_sounds");
-
-    // Base explosion effect and sound.
-    const char *pszEffect = "ExplosionCore_wall";
-    const char *pszSound = "BaseExplosionEffect.Sound";
-
-    if (pWeaponInfo)
-    {
-        // Explosions.
-        if (bIsWater)
-        {
-            pWeaponInfo->pKVWeaponParticles->GetString("ExplosionWaterEffect");
-        }
-        else
-        {
-            if (bInAir)
-            {
-                pWeaponInfo->pKVWeaponParticles->GetString("ExplosionPlayerEffect");
-            }
-            else
-            {
-                pWeaponInfo->pKVWeaponParticles->GetString("ExplosionEffect");
-            }
-        }
-
-        // Sound.
-        pszSound = pWeaponInfo->pKVWeaponSounds->GetString("explosion");
-    }
-
-    if (mom_rj_sounds.GetInt() > 0)
-    {
-        CLocalPlayerFilter filter;
-        if (mom_rj_sounds.GetInt() == 2)
-            pszSound = "BaseExplosionEffect.SoundTF2";
-        C_BaseEntity::EmitSound(filter, SOUND_FROM_WORLD, pszSound, &vecOrigin);
-    }
-
-    if (mom_rj_particles.GetInt() == 2)
-    {
-        DispatchParticleEffect(pszEffect, vecOrigin, angExplosion);
-    }
-}
+static MAKE_CONVAR(mom_rj_particles, "1", FCVAR_ARCHIVE, "Switches between the particles for rocket explosions.\n"
+    "0 = None\n1 = Use weapon script (momentum)\n2 = Force TF2 particles\n3 through 6 = alternative momentum particles", 0, 6);
 
 class C_TETFExplosion : public C_BaseTempEntity
 {
@@ -97,9 +35,66 @@ C_TETFExplosion::C_TETFExplosion()
 
 void C_TETFExplosion::PostDataUpdate(DataUpdateType_t updateType)
 {
-    VPROF("C_TETFExplosion::PostDataUpdate");
+    if (updateType == DATA_UPDATE_CREATED)
+    {
+        const auto pWeaponInfo = g_pWeaponDef->GetWeaponScript(m_iWeaponID);
+        AssertMsg(pWeaponInfo, "Invalid pWeaponInfo for weaponID %i\n", m_iWeaponID);
+        if (!pWeaponInfo)
+            return;
 
-    TFExplosionCallback(m_vecOrigin, m_vecNormal, m_iWeaponID);
+        const bool bIsWater = (UTIL_PointContents(m_vecOrigin) & CONTENTS_WATER);
+        // Cannot use zeros here because we are sending the normal at a smaller bit size.
+        const bool bInAir = fabs(m_vecNormal.x) < 0.05f && fabs(m_vecNormal.y) < 0.05f && fabs(m_vecNormal.z) < 0.05f;
+        QAngle angExplosion(0.0f, 0.0f, 0.0f);
+
+        if (!bInAir)
+            VectorAngles(m_vecNormal, angExplosion);
+
+        static ConVarRef mom_rj_sounds("mom_rj_sounds");
+
+        if (mom_rj_sounds.GetInt() > 0)
+        {
+            const bool bIsTF2Sound = mom_rj_sounds.GetInt() == 2;
+            const char *pszSound = pWeaponInfo->pKVWeaponSounds->GetString(bIsTF2Sound ? "explosion_TF2" : "explosion");
+
+            CLocalPlayerFilter filter;
+            C_BaseEntity::EmitSound(filter, SOUND_FROM_WORLD, pszSound, &m_vecOrigin);
+        }
+
+        if (mom_rj_particles.GetInt() > 0)
+        {
+            const char *pszEffect;
+            const bool bIsTF2Particle = mom_rj_particles.GetInt() == 2;
+
+            if (bIsWater)
+            {
+                pszEffect = pWeaponInfo->pKVWeaponParticles->GetString(bIsTF2Particle ? "ExplosionWaterEffect_TF2" : "ExplosionWaterEffect");
+            }
+            else
+            {
+                if (bInAir)
+                {
+                    pszEffect = pWeaponInfo->pKVWeaponParticles->GetString(bIsTF2Particle ? "ExplosionMidAirEffect_TF2" : "ExplosionMidAirEffect");
+                }
+                else
+                {
+                    pszEffect = pWeaponInfo->pKVWeaponParticles->GetString(bIsTF2Particle ? "ExplosionEffect_TF2" : "ExplosionEffect");
+                }
+            }
+
+            // MOM_TODO REMOVEME
+            if (mom_rj_particles.GetInt() == 3)
+                pszEffect = "mom_rocket_explosion_b";
+            else if (mom_rj_particles.GetInt() == 4)
+                pszEffect = "mom_rocket_explosion_b_";
+            else if (mom_rj_particles.GetInt() == 5)
+                pszEffect = "mom_rocket_explosion_c";
+            else if (mom_rj_particles.GetInt() == 6)
+                pszEffect = "mom_rocket_explosion_d";
+
+            DispatchParticleEffect(pszEffect, m_vecOrigin, angExplosion);
+        }
+    }
 }
 
 IMPLEMENT_CLIENTCLASS_EVENT_DT(C_TETFExplosion, DT_TETFExplosion, CTETFExplosion)
