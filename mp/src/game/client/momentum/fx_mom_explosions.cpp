78fa18892b4d4b50eebf903a3979a395b9b3a9de
Gocnak <Gocnak@users.noreply.github.com>
2020-01-19 02:20:55 -0500

    Remove entindex from TE_TFExplosion
    You can't hit other players in this game

--- mp/src/game/client/momentum/fx_mom_explosions.cpp
+++ mp/src/game/client/momentum/fx_mom_explosions.cpp
@@ -1,37 +1,14 @@
-//========= Copyright © 1996-2006, Valve Corporation, All rights reserved. ============//
-//
-// Purpose: Game-specific explosion effects
-//
-//=============================================================================//
 #include "cbase.h"
 #include "c_basetempentity.h"
-#include "c_te_effect_dispatch.h"
-#include "c_te_legacytempents.h"
-#include "dlight.h"
 #include "engine/IEngineSound.h"
-#include "iefx.h"
+#include "weapon/weapon_base.h"
 #include "mom_shareddefs.h"
-#include "tempent.h"
-#include "tier0/vprof.h"
-#include "weapon/weapon_def.h"
-#include "weapon/weapon_mom_rocketlauncher.h"
 
 #include "tier0/memdbgon.h"
 
-void TFExplosionCallback(const Vector &vecOrigin, const Vector &vecNormal, WeaponID_t iWeaponID,
-                         ClientEntityHandle_t hEntity)
+void TFExplosionCallback(const Vector &vecOrigin, const Vector &vecNormal, WeaponID_t iWeaponID)
 {
     const auto pWeaponInfo = g_pWeaponDef->GetWeaponScript(iWeaponID);
-
-    bool bIsPlayer = false;
-    if (hEntity.Get())
-    {
-        C_BaseEntity *pEntity = C_BaseEntity::Instance(hEntity);
-        if (pEntity && pEntity->IsPlayer())
-        {
-            bIsPlayer = true;
-        }
-    }
 
     // Calculate the angles, given the normal.
     bool bIsWater = (UTIL_PointContents(vecOrigin) & CONTENTS_WATER);
@@ -66,7 +43,7 @@ void TFExplosionCallback(const Vector &vecOrigin, const Vector &vecNormal, Weapo
         }
         else
         {
-            if (bIsPlayer || bInAir)
+            if (bInAir)
             {
                 pWeaponInfo->pKVWeaponParticles->GetString("ExplosionPlayerEffect");
             }
@@ -87,7 +64,7 @@ void TFExplosionCallback(const Vector &vecOrigin, const Vector &vecNormal, Weapo
             pszSound = "BaseExplosionEffect.SoundTF2";
         C_BaseEntity::EmitSound(filter, SOUND_FROM_WORLD, pszSound, &vecOrigin);
     }
-    
+
     if (mom_rj_particles.GetInt() == 2)
     {
         DispatchParticleEffect(pszEffect, vecOrigin, angExplosion);
@@ -102,11 +79,10 @@ class C_TETFExplosion : public C_BaseTempEntity
 
     C_TETFExplosion();
 
-    ClientEntityHandle_t m_hEntity;
+protected:
+    void PostDataUpdate(DataUpdateType_t updateType) OVERRIDE;
 
-  private:
-    virtual void PostDataUpdate(DataUpdateType_t updateType);
-
+private:
     Vector m_vecOrigin;
     Vector m_vecNormal;
     WeaponID_t m_iWeaponID;
@@ -117,26 +93,17 @@ C_TETFExplosion::C_TETFExplosion()
     m_vecOrigin.Init();
     m_vecNormal.Init();
     m_iWeaponID = WEAPON_NONE;
-    m_hEntity = INVALID_EHANDLE_INDEX;
 }
 
 void C_TETFExplosion::PostDataUpdate(DataUpdateType_t updateType)
 {
     VPROF("C_TETFExplosion::PostDataUpdate");
 
-    TFExplosionCallback(m_vecOrigin, m_vecNormal, m_iWeaponID, m_hEntity);
-}
-
-static void RecvProxy_ExplosionEntIndex(const CRecvProxyData *pData, void *pStruct, void *pOut)
-{
-    int nEntIndex = pData->m_Value.m_Int;
-    ((C_TETFExplosion *)pStruct)->m_hEntity =
-        (nEntIndex < 0) ? INVALID_EHANDLE_INDEX : ClientEntityList().EntIndexToHandle(nEntIndex);
+    TFExplosionCallback(m_vecOrigin, m_vecNormal, m_iWeaponID);
 }
 
 IMPLEMENT_CLIENTCLASS_EVENT_DT(C_TETFExplosion, DT_TETFExplosion, CTETFExplosion)
     RecvPropVector(RECVINFO(m_vecOrigin)),
     RecvPropVector(RECVINFO(m_vecNormal)),
     RecvPropInt(RECVINFO(m_iWeaponID)),
-    RecvPropInt("entindex", 0, SIZEOF_IGNORE, 0, RecvProxy_ExplosionEntIndex),
-END_RECV_TABLE()
+END_RECV_TABLE();
