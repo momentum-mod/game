340a1e19a6c82c71e13d7b802925b3d7c0857b73
Hat Kid <lozanoski.philip779@googlemail.com>
2020-02-27 04:36:51 +0100

    + mom_hud_sj_chargemeter_units
    Toggles between different variants of units for the stickybomb speed while charging

--- mp/src/game/client/momentum/ui/HUD/hud_stickybombcharge.cpp
+++ mp/src/game/client/momentum/ui/HUD/hud_stickybombcharge.cpp
@@ -1,5 +1,8 @@
 #include "cbase.h"
 
+#include <vgui/ILocalize.h>
+#include <vgui/ISurface.h>
+#include <vgui/IVGui.h>
 #include <vgui_controls/EditablePanel.h>
 #include <vgui_controls/ProgressBar.h>
 #include <vgui_controls/Label.h>
@@ -8,6 +11,8 @@
 #include "hud.h"
 #include "hudelement.h"
 #include "iclientmode.h"
+#include "ienginevgui.h"
+
 #include "mom_system_gamemode.h"
 #include "weapon/weapon_mom_stickybomblauncher.h"
 
@@ -16,6 +21,12 @@
 using namespace vgui;
 
 static MAKE_TOGGLE_CONVAR(mom_hud_sj_chargemeter_enable, "1", FCVAR_ARCHIVE, "Toggles the charge meter on or off.\n");
+static MAKE_CONVAR(mom_hud_sj_chargemeter_units, "0", FCVAR_ARCHIVE,
+                   "If above 0, shows the speed at which a sticky will be launched when charged.\n"
+                   " 0 = OFF\n"
+                   " 1 = Speed in Hammer units (900-2400u/s)\n"
+                   " 2 = Speed in percent (0% = 900u/s, 100% = 2400u/s)",
+                   0, 2);
 
 class CHudStickyCharge : public CHudElement, public EditablePanel
 {
@@ -89,11 +100,19 @@ void CHudStickyCharge::OnThink()
     if (!m_pLauncher)
         return;
 
+    // Reset the charge label when player stops charging
+    if (mom_hud_sj_chargemeter_units.GetInt() >= 0 && m_pLauncher->GetChargeBeginTime() == 0)
+    {
+        m_pChargeLabel->SetText("CHARGE");
+    }
+
     // Turn charge meter red while inside start zone to indicate that stickies can't be charged
     if (!m_pLauncher->IsChargeEnabled())
     {
         m_pChargeMeter->SetFgColor(Color(192, 28, 0, 140));
         m_pChargeMeter->SetProgress(1.0f);
+
+        m_pChargeLabel->SetVisible(false);
     }
     else
     {
@@ -110,6 +129,21 @@ void CHudStickyCharge::OnThink()
                 float flPercentCharged = min(1.0, flTimeCharged / flChargeMaxTime);
 
                 m_pChargeMeter->SetProgress(flPercentCharged);
+
+                if (mom_hud_sj_chargemeter_units.GetInt() == 1)
+                {
+                    char buf[64];
+                    Q_snprintf(buf, sizeof(buf), "%du/s", (int) RemapValClamped(flTimeCharged, 0.0f, 4.0f, 900.0f, 2400.0f));
+
+                    m_pChargeLabel->SetText(buf);
+                }
+                else if (mom_hud_sj_chargemeter_units.GetInt() == 2)
+                {
+                    char buf[64];
+                    Q_snprintf(buf, sizeof(buf), "%d%%", (int) (flPercentCharged * 100));
+
+                    m_pChargeLabel->SetText(buf);
+                }
             }
             else
             {
