0ba40a1b3819b10c052ed79de3fbcbc6befe0dd4
Hat Kid <lozanoski.philip779@googlemail.com>
2020-02-29 16:00:30 +0100

    Remove vehicle references from player_command

--- mp/src/game/server/player_command.cpp
+++ mp/src/game/server/player_command.cpp
@@ -12,7 +12,6 @@
 #include "client.h"
 #include "player_command.h"
 #include "movehelper_server.h"
-#include "iservervehicle.h"
 #include "tier0/vprof.h"
 
 #include "in_buttons.h"
@@ -425,19 +424,6 @@ void CPlayerMove::RunCommand ( CBasePlayer *player, CUserCmd *ucmd, IMoveHelper 
 		}
 	}
 
-	IServerVehicle *pVehicle = player->GetVehicle();
-
-	// Latch in impulse.
-	if ( ucmd->impulse )
-	{
-		// Discard impulse commands unless the vehicle allows them.
-		// FIXME: UsingStandardWeapons seems like a bad filter for this. The flashlight is an impulse command, for example.
-		if ( !pVehicle || player->UsingStandardWeaponsInVehicle() )
-		{
-			player->m_nImpulse = ucmd->impulse;
-		}
-	}
-
 	// Update player input button states
 	VPROF_SCOPE_BEGIN( "player->UpdateButtonState" );
 	player->UpdateButtonState( ucmd->buttons );
@@ -484,18 +470,10 @@ void CPlayerMove::RunCommand ( CBasePlayer *player, CUserCmd *ucmd, IMoveHelper 
 	SetupMove( player, ucmd, moveHelper, g_pMoveData );
 
 	// Let the game do the movement.
-	if ( !pVehicle )
-	{
-		VPROF( "g_pGameMovement->ProcessMovement()" );
-		Assert( g_pGameMovement );
-		g_pGameMovement->ProcessMovement( player, g_pMoveData );
-	}
-	else
-	{
-		VPROF( "pVehicle->ProcessMovement()" );
-		pVehicle->ProcessMovement( player, g_pMoveData );
-	}
-			
+	VPROF( "g_pGameMovement->ProcessMovement()" );
+	Assert( g_pGameMovement );
+	g_pGameMovement->ProcessMovement( player, g_pMoveData );
+
 	// Copy output
 	FinishMove( player, ucmd, g_pMoveData );
 
