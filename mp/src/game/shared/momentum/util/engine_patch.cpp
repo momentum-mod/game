0982f5a71d3dce66da7095910434d51e00a87d7d
xen <24222257+xen-000@users.noreply.github.com>
2020-03-05 22:14:28 +0200

    Move engine patching functionality to CEnginePatch
    A more modular system to make additional patches easier to add.
    Also include initial linux patching support, needs testing.

--- mp/src/game/shared/momentum/util/engine_patch.cpp
+++ mp/src/game/shared/momentum/util/engine_patch.cpp
@@ -1,3 +1,6 @@
+//------------------------------------------------------------------------------------
+// Functions used to find patterns of bytes in the engine's memory to hook or patch
+//------------------------------------------------------------------------------------
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "Windows.h"
@@ -5,6 +8,7 @@
 #pragma comment(lib, "psapi.lib")
 #elif defined (POSIX)
 #include "util/os_utils.h"
+#include "sys/mman.h"
 #endif
 
 #include "engine_patch.h"
@@ -41,6 +45,7 @@ bool CEngineBinary::Init()
 
 void CEngineBinary::PostInit()
 {
+    CEnginePatch::ApplyAll();
 }
 
 inline bool CEngineBinary::DataCompare(const unsigned char* data, const unsigned char* pattern, const char* mask)
@@ -71,31 +76,170 @@ void* CEngineBinary::FindPattern(const unsigned char* pattern, const char* mask,
 
 CEngineBinary g_EngineBinary;
 
-#endif //WIN32
-
+// Engine Patch format:
+//
+// Patch name
+// Memory signature
+// Signature mask
+// Patch offset
+// Immediate or referenced variable
+// Patch bytes (int/float/char*)
 #ifdef _WIN32
-	// Prevent the culling of skyboxes at high FOVs
-	// https://github.com/VSES/SourceEngine2007/blob/master/se2007/engine/gl_warp.cpp#L315
-	// TODO: Use a value derived from FOV instead
-	unsigned char pattern[] = { 0xF3, 0x0F, 0x59, 0x15, '?', '?', '?', '?', 0xF3, 0x0F, 0x58, 0xC1, 0xF3, 0x0F, 0x10, 0x0D };
-	auto addr = reinterpret_cast<uintptr_t>(FindPattern(pattern, "xxxx????xxxxxxxx", 16));
-
-	if (addr)
-	{
-		// The value is stored in the data segment so it needs write permission
-		float* fValue = *reinterpret_cast<float**>(addr);
-
-		// 0x40 is read,write,execute
-		unsigned long iOldProtection, iNewProtection = 0x40;
-
-		if (VirtualProtect((void*)fValue, sizeof(float), iNewProtection, &iOldProtection))
-		{
-			*fValue = -1;
-
-			// Restore old protections
-			VirtualProtect((void*)fValue, sizeof(float), iOldProtection, &iNewProtection);
-		}
-	}
-#else //POSIX
+CEnginePatch g_CEnginePatches[] =
+{
+    // Prevent the culling of skyboxes at high FOVs
+    // https://github.com/VSES/SourceEngine2007/blob/master/se2007/engine/gl_warp.cpp#L315
+    // TODO: Use a value derived from FOV instead
+    {
+        "SkyboxCulling",
+        (unsigned char*)"\xF3\x0F\x59\x15\x00\x00\x00\x00\xF3\x0F\x58\xC1\xF3\x0F\x10\x0D",
+        "xxxx????xxxxxxxx",
+        16,
+        false,
+        (float)-1
+    },
+    // Example patch: Trigger "Map has too many brushes" error at 16384 brushes instead of 8192
+    //{
+    //    "BrushLimit",
+    //    (unsigned char*)"\xC2\xEF\x03\x81\xFF\x00\x00\x00\x00",
+    //    "xxxxx????",
+    //    5,
+    //    true,
+    //    16384
+    //},
+    // The same patch as above but using a pure hex value
+    //{
+    //    "BrushLimit2",
+    //    (unsigned char*)"\xC1\xEF\x03\x81\xFF\x00\x00\x00\x00",
+    //    "xxxxx????",
+    //    5,
+    //    true,
+    //    (unsigned char*)"\x00\x40\x00\x00"
+    //}
+};
+#elif __linux__
+CEnginePatch g_CEnginePatches[] =
+{
+    // Prevent the culling of skyboxes at high FOVs
+    // https://github.com/VSES/SourceEngine2007/blob/master/se2007/engine/gl_warp.cpp#L315
+    // TODO: Use a value derived from FOV instead
+    {
+        "SkyboxCulling",
+        (unsigned char*)"\xF3\x0F\x59\x0D\x00\x00\x00\x00\xF3\x0F\x58\xC2\xF3\x0F\x58\xC1\xF3\x0F\x10\x0D",
+        "xxxx????xxxxxxxxxxxx",
+        20,
+        false,
+        (float)-1
+    }
+};
 #endif //WIN32
+
+void CEnginePatch::ApplyAll()
+{
+#if defined (OSX)
+    // No OSX patches yet
+    return;
+#endif
+    int err;
+
+    for (int i = 0; i < sizeof(g_CEnginePatches) / sizeof(*g_CEnginePatches); i++)
+    {
+        err = g_CEnginePatches[i].ApplyPatch();
+        switch (err)
+        {
+        case 1:
+            Warning("Engine patch \"%s\" FAILED: Could not override memory protection\n", g_CEnginePatches[i].getName());
+            break;
+        case 2:
+            Warning("Engine patch \"%s\" FAILED: Could not find signature\n", g_CEnginePatches[i].getName());
+        }
+    }
+}
+
+// Apply a patch to engine memory
+int CEnginePatch::ApplyPatch()
+{
+#ifdef _WIN32
+
+    uintptr_t addr = reinterpret_cast<uintptr_t>(CEngineBinary::FindPattern(m_pPatchSignature, m_pMask, m_iOffset));
+
+    if (addr)
+    {
+        auto pMemory = m_bImmediate ? (uintptr_t*)addr : *reinterpret_cast<uintptr_t**>(addr);
+
+        // Memory is write-protected so it needs to be lifted before the patch is applied
+        // 0x40 is read,write,execute
+        // https://docs.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
+        unsigned long iOldProtection, iNewProtection = 0x40;
+
+        if (VirtualProtect(pMemory, m_iPatchLength, iNewProtection, &iOldProtection))
+        {
+            Q_memcpy(pMemory, m_pPatch, m_iPatchLength);
+
+            // Restore old protections
+            VirtualProtect(pMemory, m_iPatchLength, iOldProtection, &iNewProtection);
+
+            return 0;
+        }
+        return 1;
+    }
+    return 2;
+
+#elif defined __linux__
+
+    void* addr = CEngineBinary::FindPattern(m_pPatchSignature, m_pMask, m_iOffset);
+
+    if (addr)
+    {
+        auto pMemory = m_bImmediate ? (uintptr_t*)addr : *(uintptr_t**)(addr);
+
+        // Memory is write-protected so it needs to be lifted before the patch is applied
+        // http://man7.org/linux/man-pages/man2/mprotect.2.html
+        if (!mprotect(pMemory, m_iPatchLength, PROT_READ | PROT_WRITE | PROT_EXEC))
+        {
+            Q_memcpy(pMemory, m_pPatch, m_iPatchLength);
+
+            // Restore old protections
+            mprotect(pMemory, m_iPatchLength, PROT_READ | PROT_EXEC);
+
+            return 0;
+        }
+        return 1;
+    }
+    return 2;
+
+#endif //WIN32
+}
+
+// Constructors
+CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate)
+{
+    m_sName = name;
+    m_pPatchSignature = signature;
+    m_pMask = mask;
+    m_iOffset = offset;
+    m_iPatchLength = 0;
+    m_bImmediate = immediate;
+    m_pPatch = new unsigned char;
+}
+
+CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, int value)
+    : CEnginePatch(name, signature, mask, offset, immediate)
+{
+    m_iPatchLength = sizeof(value);
+    Q_memcpy(m_pPatch, &value, m_iPatchLength);
+}
+
+CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, float value)
+    : CEnginePatch(name, signature, mask, offset, immediate)
+{
+    m_iPatchLength = sizeof(value);
+    Q_memcpy(m_pPatch, &value, m_iPatchLength);
+}
+
+CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, unsigned char* bytes)
+    : CEnginePatch(name, signature, mask, offset, immediate)
+{
+    m_iPatchLength = sizeof(bytes);
+    m_pPatch = (unsigned char*)bytes;
 }
--- mp/src/game/shared/momentum/util/engine_patch.h
+++ mp/src/game/shared/momentum/util/engine_patch.h
@@ -17,8 +17,30 @@ public:
     static inline bool DataCompare(const unsigned char*, const unsigned char*, const char*);
     static void* FindPattern(const unsigned char*, const char*, size_t);
 
+private:
+    static void* moduleBase;
+    static size_t moduleSize;
+};
+
+class CEnginePatch
+{
+public:
+    CEnginePatch(const char*, unsigned char*, char*, size_t, bool);
+    CEnginePatch(const char*, unsigned char*, char*, size_t, bool, int);
+    CEnginePatch(const char*, unsigned char*, char*, size_t, bool, float);
+    CEnginePatch(const char*, unsigned char*, char*, size_t, bool, unsigned char*);
+
+    int ApplyPatch();
+    static void ApplyAll();
+
+    const char* getName() { return m_sName; }
 
 private:
-	static void* moduleBase;
-	static size_t moduleSize;
+    const char* m_sName;
+    unsigned char *m_pPatchSignature;
+    const char *m_pMask;
+    size_t m_iOffset;
+    size_t m_iPatchLength;
+    unsigned char* m_pPatch;
+    bool m_bImmediate;
 };
