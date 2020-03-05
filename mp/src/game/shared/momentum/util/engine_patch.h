22880c0788613449d143103859fb0fcf689f9686
xen <24222257+xen-000@users.noreply.github.com>
2020-03-05 22:08:09 +0200

    Rename EnginePatch to CEngineBinary
    Now a game system so it's called on init

--- mp/src/game/shared/momentum/util/engine_patch.h
+++ mp/src/game/shared/momentum/util/engine_patch.h
@@ -1,17 +1,22 @@
 #pragma once
-//---------------------------------------------------------------------------------------------
-// Functions used to find patterns of bytes mainly in the engine binary for patching/hooking
-//---------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+// Functions used to find patterns of bytes in the engine's memory to hook or patch
+//------------------------------------------------------------------------------------
 //
 #include "cbase.h"
 #include "tier0/platform.h"
 
-class EnginePatch
+class CEngineBinary : CAutoGameSystem
 {
 public:
-	static void InitPatches();
-	static inline bool DataCompare(const unsigned char*, const unsigned char*, const char*);
-	static void* FindPattern(const unsigned char*, const char*, size_t);
+    CEngineBinary();
+
+    bool Init() OVERRIDE;
+    void PostInit() OVERRIDE;
+
+    static inline bool DataCompare(const unsigned char*, const unsigned char*, const char*);
+    static void* FindPattern(const unsigned char*, const char*, size_t);
+
 
 private:
 	static void* moduleBase;
