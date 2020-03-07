3686d16bf9f82816487044a2bd683459eef5699b
xen <24222257+xen-000@users.noreply.github.com>
2020-03-07 13:52:16 +0200

    Further cleanup and refactor patch application

--- mp/src/game/shared/momentum/util/engine_patch.cpp
+++ mp/src/game/shared/momentum/util/engine_patch.cpp
@@ -12,8 +12,6 @@
 #endif
 
 #include "engine_patch.h"
-#include "mom_shareddefs.h"
-#include "tier0/platform.h"
 
 void* CEngineBinary::moduleBase = nullptr;
 size_t CEngineBinary::moduleSize;
@@ -38,7 +36,7 @@ bool CEngineBinary::Init()
 #else //POSIX
     if (GetModuleInformation(ENGINE_DLL_NAME, &moduleBase, &moduleSize))
         return false;
-#endif //WIN32
+#endif //_WIN32
 
     return true;
 }
@@ -48,7 +46,7 @@ void CEngineBinary::PostInit()
     CEnginePatch::ApplyAll();
 }
 
-inline bool CEngineBinary::DataCompare(const unsigned char* data, const unsigned char* pattern, const char* mask)
+inline bool CEngineBinary::DataCompare(const char* data, const char* pattern, const char* mask)
 {
     for (; *mask != 0; ++data, ++pattern, ++mask)
         if (*mask == 'x' && *data != *pattern)
@@ -61,12 +59,12 @@ inline bool CEngineBinary::DataCompare(const unsigned char* data, const unsigned
 // Finds a pattern of bytes in the engine memory given a signature and a mask
 // Returns the address of the first (and hopefully only) match with an optional offset, otherwise nullptr
 //---------------------------------------------------------------------------------------------------------
-void* CEngineBinary::FindPattern(const unsigned char* pattern, const char* mask, size_t offset)
+void* CEngineBinary::FindPattern(const char* pattern, const char* mask, size_t offset)
 {
     auto maskLength = strlen(mask);
     for (size_t i = 0; i <= moduleSize - maskLength; ++i)
     {
-        auto addr = reinterpret_cast<const unsigned char*>(moduleBase) + i;
+        auto addr = reinterpret_cast<char*>(moduleBase) + i;
         if (DataCompare(addr, pattern, mask))
             return const_cast<void*>(reinterpret_cast<const void*>(addr + offset));
     }
@@ -74,16 +72,25 @@ void* CEngineBinary::FindPattern(const unsigned char* pattern, const char* mask,
     return nullptr;
 }
 
+int CEngineBinary::SetMemoryProtection(void* pAddress, size_t iLength, int iProtection, unsigned long* pOriginalProtection)
+{
+#ifdef _WIN32
+    return VirtualProtect(pAddress, iLength, iProtection, pOriginalProtection);
+#else //POSIX
+#endif //_WIN32
+}
+
 CEngineBinary g_EngineBinary;
 
 // Engine Patch format:
-//
-// Patch name
-// Memory signature
-// Signature mask
-// Patch offset
-// Immediate or referenced variable
-// Patch bytes (int/float/char*)
+//==============================
+// m_sName:         Patch name
+// m_pSignature:    Memory signature
+// m_pMask:         Signature mask
+// m_iOffset:       Patch offset
+// m_bImmediate:    Immediate or referenced variable
+// m_pPatch:        Patch bytes (int/float/char*)
+//==============================
 #ifdef _WIN32
 CEnginePatch g_EnginePatches[] =
 {
@@ -92,29 +99,29 @@ CEnginePatch g_EnginePatches[] =
     // TODO: Use a value derived from FOV instead
     {
         "SkyboxCulling",
-        (unsigned char*)"\xF3\x0F\x59\x15\x00\x00\x00\x00\xF3\x0F\x58\xC1\xF3\x0F\x10\x0D",
+        "\xF3\x0F\x59\x15\x00\x00\x00\x00\xF3\x0F\x58\xC1\xF3\x0F\x10\x0D",
         "xxxx????xxxxxxxx",
         16,
-        false,
-        (float)-1
+        PATCH_REFERENCE,
+        -1.0f
     },
     // Example patch: Trigger "Map has too many brushes" error at 16384 brushes instead of 8192
     //{
     //    "BrushLimit",
-    //    (unsigned char*)"\xC2\xEF\x03\x81\xFF\x00\x00\x00\x00",
+    //    "\xC1\xEF\x03\x81\xFF\x00\x00\x00\x00",
     //    "xxxxx????",
     //    5,
-    //    true,
+    //    PATCH_IMMEDIATE,
     //    16384
     //},
     // The same patch as above but using a pure hex value
     //{
-    //    "BrushLimit2",
-    //    (unsigned char*)"\xC1\xEF\x03\x81\xFF\x00\x00\x00\x00",
+    //    "BrushLimitHex",
+    //    "\xC1\xEF\x03\x81\xFF\x00\x00\x00\x00",
     //    "xxxxx????",
     //    5,
-    //    true,
-    //    (unsigned char*)"\x00\x40\x00\x00"
+    //    PATCH_IMMEDIATE,
+    //    "\x00\x40\x00\x00"
     //}
 };
 #elif __linux__
@@ -132,11 +139,11 @@ CEnginePatch g_EnginePatches[] =
         (float)-1
     }
 };
-#endif //WIN32
+#endif //_WIN32
 
 void CEnginePatch::ApplyAll()
 {
-#if !defined (OSX) // No OSX patches yet
+#if !defined (OSX) // No OSX patches
     int err;
 
     for (int i = 0; i < sizeof(g_EnginePatches) / sizeof(*g_EnginePatches); i++)
@@ -144,11 +151,17 @@ void CEnginePatch::ApplyAll()
         err = g_EnginePatches[i].ApplyPatch();
         switch (err)
         {
-        case 1:
-            Warning("Engine patch \"%s\" FAILED: Could not override memory protection\n", g_EnginePatches[i].getName());
+        case PATCH_SUCCESS:
+            DevLog("Engine patch \"%s\" applied successfully\n", g_EnginePatches[i].GetName());
             break;
-        case 2:
-            Warning("Engine patch \"%s\" FAILED: Could not find signature\n", g_EnginePatches[i].getName());
+        case PATCH_MEMPROTECT_FAIL:
+            Warning("Engine patch \"%s\" FAILED: Could not override memory protection\n", g_EnginePatches[i].GetName());
+            break;
+        case PATCH_INVALID_SIGNATURE:
+            Warning("Engine patch \"%s\" FAILED: Could not find signature\n", g_EnginePatches[i].GetName());
+            break;
+        case PATCH_NO_VALUE:
+            Warning("Engine patch \"%s\" FAILED: No value provided\n", g_EnginePatches[i].GetName());
         }
     }
 #endif // (OSX)
@@ -157,87 +170,70 @@ void CEnginePatch::ApplyAll()
 // Apply a patch to engine memory
 int CEnginePatch::ApplyPatch()
 {
-#ifdef _WIN32
+    if (!m_pPatch)
+        return PATCH_NO_VALUE;
 
-    uintptr_t addr = reinterpret_cast<uintptr_t>(CEngineBinary::FindPattern(m_pPatchSignature, m_pMask, m_iOffset));
+    void* addr = CEngineBinary::FindPattern(m_pSignature, m_pMask, m_iOffset);
 
     if (addr)
     {
         auto pMemory = m_bImmediate ? (uintptr_t*)addr : *reinterpret_cast<uintptr_t**>(addr);
 
         // Memory is write-protected so it needs to be lifted before the patch is applied
-        // 0x40 is read,write,execute
+#ifdef _WIN32
         // https://docs.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
-        unsigned long iOldProtection, iNewProtection = 0x40;
-
-        if (VirtualProtect(pMemory, m_iPatchLength, iNewProtection, &iOldProtection))
-        {
-            Q_memcpy(pMemory, m_pPatch, m_iPatchLength);
-
-            // Restore old protections
-            VirtualProtect(pMemory, m_iPatchLength, iOldProtection, &iNewProtection);
-
-            return 0;
-        }
-        return 1;
-    }
-    return 2;
-
-#elif defined __linux__
-
-    void* addr = CEngineBinary::FindPattern(m_pPatchSignature, m_pMask, m_iOffset);
-
-    if (addr)
-    {
-        auto pMemory = m_bImmediate ? (uintptr_t*)addr : *(uintptr_t**)(addr);
-
-        // Memory is write-protected so it needs to be lifted before the patch is applied
+        unsigned long iOldProtection;
+        unsigned long iNewProtection = PAGE_EXECUTE_READWRITE;
+#else //POSIX
         // http://man7.org/linux/man-pages/man2/mprotect.2.html
-        if (!mprotect(pMemory, m_iPatchLength, PROT_READ | PROT_WRITE | PROT_EXEC))
+#endif //_WIN32
+
+        if (CEngineBinary::SetMemoryProtection(pMemory, m_iLength, iNewProtection, &iOldProtection))
         {
-            Q_memcpy(pMemory, m_pPatch, m_iPatchLength);
+            Q_memcpy(pMemory, m_pPatch, m_iLength);
 
             // Restore old protections
-            mprotect(pMemory, m_iPatchLength, PROT_READ | PROT_EXEC);
+            CEngineBinary::SetMemoryProtection(pMemory, m_iLength, iOldProtection, &iNewProtection);
 
-            return 0;
+            return PATCH_SUCCESS;
         }
-        return 1;
+        return PATCH_MEMPROTECT_FAIL;
     }
-    return 2;
-
-#endif //WIN32
+    return PATCH_INVALID_SIGNATURE;
 }
 
 // Constructors
-CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate)
+CEnginePatch::CEnginePatch(const char* name, char* signature, char* mask, size_t offset, bool immediate)
 {
     m_sName = name;
-    m_pPatchSignature = signature;
+    m_pSignature = signature;
     m_pMask = mask;
     m_iOffset = offset;
-    m_iPatchLength = 0;
+    m_iLength = 0;
     m_bImmediate = immediate;
-    m_pPatch = new unsigned char;
+    m_pPatch = nullptr;
 }
 
-CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, int value)
+// Converting numeric types into bytes
+CEnginePatch::CEnginePatch(const char* name, char* signature, char* mask, size_t offset, bool immediate, int value)
     : CEnginePatch(name, signature, mask, offset, immediate)
 {
-    m_iPatchLength = sizeof(value);
-    Q_memcpy(m_pPatch, &value, m_iPatchLength);
+    m_iLength = sizeof(value);
+    m_pPatch = new char[m_iLength];
+    Q_memcpy(m_pPatch, &value, m_iLength);
 }
 
-CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, float value)
+CEnginePatch::CEnginePatch(const char* name, char* signature, char* mask, size_t offset, bool immediate, float value)
     : CEnginePatch(name, signature, mask, offset, immediate)
 {
-    m_iPatchLength = sizeof(value);
-    Q_memcpy(m_pPatch, &value, m_iPatchLength);
+    m_iLength = sizeof(value);
+    m_pPatch = new char[m_iLength];
+    Q_memcpy(m_pPatch, &value, m_iLength);
 }
 
-CEnginePatch::CEnginePatch(const char* name, unsigned char* signature, char* mask, size_t offset, bool immediate, unsigned char* bytes)
+CEnginePatch::CEnginePatch(const char* name, char* signature, char* mask, size_t offset, bool immediate, char* bytes)
     : CEnginePatch(name, signature, mask, offset, immediate)
 {
-    m_iPatchLength = sizeof(bytes);
-    m_pPatch = (unsigned char*)bytes;
+    m_iLength = sizeof(bytes);
+    m_pPatch = bytes;
 }
