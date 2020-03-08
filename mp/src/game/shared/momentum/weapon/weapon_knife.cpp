44d8d6f2e6e3e55634267232639fd417446db180
Gocnak <Gocnak@users.noreply.github.com>
2020-03-08 00:19:33 -0500

    Cleanup weapon_knife class
    Removed unnecessary duplicates and unused/old code

--- mp/src/game/shared/momentum/weapon/weapon_knife.cpp
+++ mp/src/game/shared/momentum/weapon/weapon_knife.cpp
@@ -12,55 +12,13 @@
 #define KNIFE_BODYHIT_VOLUME 128
 #define KNIFE_WALLHIT_VOLUME 512
 
-#ifndef CLIENT_DLL
-//-----------------------------------------------------------------------------
-// Purpose: Only send to local player if this weapon is the active weapon
-// Input  : *pStruct -
-//			*pVarData -
-//			*pRecipients -
-//			objectID -
-// Output : void*
-//-----------------------------------------------------------------------------
-void *SendProxy_SendActiveLocalKnifeDataTable(const SendProp *pProp, const void *pStruct, const void *pVarData,
-                                              CSendProxyRecipients *pRecipients, int objectID)
-{
-    // Get the weapon entity
-    CBaseCombatWeapon *pWeapon = (CBaseCombatWeapon *)pVarData;
-    if (pWeapon)
-    {
-        // Only send this chunk of data to the player carrying this weapon
-        CBasePlayer *pPlayer = ToBasePlayer(pWeapon->GetOwner());
-        if (pPlayer /*&& pPlayer->GetActiveWeapon() == pWeapon*/)
-        {
-            pRecipients->SetOnly(pPlayer->GetClientIndex());
-            return (void *)pVarData;
-        }
-    }
+IMPLEMENT_NETWORKCLASS_ALIASED(Knife, DT_WeaponKnife);
 
-    return nullptr;
-}
-REGISTER_SEND_PROXY_NON_MODIFIED_POINTER(SendProxy_SendActiveLocalKnifeDataTable);
-#endif
-
-// ----------------------------------------------------------------------------- //
-// CKnife tables.
-// ----------------------------------------------------------------------------- //
-
-IMPLEMENT_NETWORKCLASS_ALIASED(Knife, DT_WeaponKnife)
-
-BEGIN_NETWORK_TABLE_NOBASE(CKnife, DT_LocalActiveWeaponKnifeData)
+BEGIN_NETWORK_TABLE(CKnife, DT_WeaponKnife)
 #ifdef GAME_DLL
     SendPropTime(SENDINFO(m_flSmackTime)),
 #else
     RecvPropTime(RECVINFO(m_flSmackTime)),
-#endif
-END_NETWORK_TABLE();
-
-BEGIN_NETWORK_TABLE(CKnife, DT_WeaponKnife)
-#ifdef GAME_DLL
-    SendPropDataTable("LocalActiveWeaponKnifeData", 0, &REFERENCE_SEND_TABLE(DT_LocalActiveWeaponKnifeData), SendProxy_SendActiveLocalKnifeDataTable),
-#else
-    RecvPropDataTable("LocalActiveWeaponKnifeData", 0, 0, &REFERENCE_RECV_TABLE(DT_LocalActiveWeaponKnifeData)),
 #endif
 END_NETWORK_TABLE();
 
@@ -73,61 +31,10 @@ END_PREDICTION_DATA();
 LINK_ENTITY_TO_CLASS(weapon_knife, CKnife);
 PRECACHE_WEAPON_REGISTER(weapon_knife);
 
-#ifndef CLIENT_DLL
-
-BEGIN_DATADESC(CKnife)
-DEFINE_FUNCTION(Smack)
-END_DATADESC()
-
-#endif
-
-// ----------------------------------------------------------------------------- //
-// CKnife implementation.
-// ----------------------------------------------------------------------------- //
-
 CKnife::CKnife()
 {
     m_bStab = false;
-}
-
-bool CKnife::HasPrimaryAmmo() { return true; }
-
-bool CKnife::CanBeSelected() { return true; }
-
-void CKnife::Precache()
-{
-    BaseClass::Precache();
-
-    PrecacheScriptSound("Weapon_Knife.Deploy");
-    PrecacheScriptSound("Weapon_Knife.Slash");
-    PrecacheScriptSound("Weapon_Knife.Stab");
-    PrecacheScriptSound("Weapon_Knife.Hit");
-    PrecacheScriptSound("Weapon_Knife.HitWall");
-}
-
-void CKnife::Spawn()
-{
-    Precache();
-
     m_iClip1 = -1;
-    BaseClass::Spawn();
-}
-
-bool CKnife::Deploy()
-{
-    CPASAttenuationFilter filter(GetOwner()->EarPosition());
-    filter.UsePredictionRules();
-    EmitSound(filter, GetOwner()->entindex(), "Weapon_Knife.Deploy");
-
-    return BaseClass::Deploy();
-}
-
-void CKnife::Holster(int skiplocal)
-{
-    if (GetPlayerOwner())
-    {
-        GetPlayerOwner()->m_flNextAttack = gpGlobals->curtime + 0.5f;
-    }
 }
 
 void CKnife::PrimaryAttack() { DoAttack(false); }
@@ -139,18 +46,20 @@ void CKnife::DoAttack(bool bIsSecondary)
     CMomentumPlayer *pPlayer = GetPlayerOwner();
     if (pPlayer)
     {
-#if !defined(CLIENT_DLL)
+#ifndef CLIENT_DLL
         // Move other players back to history positions based on local player's lag
         lagcompensation->StartLagCompensation(pPlayer, pPlayer->GetCurrentCommand());
 #endif
+
         SwingOrStab(bIsSecondary);
-#if !defined(CLIENT_DLL)
+
+#ifndef CLIENT_DLL
         lagcompensation->FinishLagCompensation(pPlayer);
 #endif
     }
 }
 
-void CKnife::Smack(void)
+void CKnife::Smack()
 {
     if (!GetPlayerOwner())
         return;
@@ -169,7 +78,7 @@ void CKnife::WeaponIdle()
     if (!pPlayer)
         return;
 
-    SetWeaponIdleTime(gpGlobals->curtime + 20);
+    SetWeaponIdleTime(gpGlobals->curtime + 20.0f);
 
     // only idle if the slid isn't back
     SendWeaponAnim(ACT_VM_IDLE);
@@ -213,7 +122,7 @@ bool CKnife::SwingOrStab(bool bStab)
 
     m_flNextPrimaryAttack = gpGlobals->curtime + fPrimDelay;
     m_flNextSecondaryAttack = gpGlobals->curtime + fSecDelay;
-    SetWeaponIdleTime(gpGlobals->curtime + 2);
+    SetWeaponIdleTime(gpGlobals->curtime + 2.0f);
 
     if (bDidHit)
     {
@@ -236,7 +145,7 @@ bool CKnife::SwingOrStab(bool bStab)
     return bDidHit;
 }
 
-void CKnife::ItemPostFrame(void)
+void CKnife::ItemPostFrame()
 {
     if (m_flSmackTime > 0 && gpGlobals->curtime > m_flSmackTime)
     {
--- mp/src/game/shared/momentum/weapon/weapon_knife.h
+++ mp/src/game/shared/momentum/weapon/weapon_knife.h
@@ -17,41 +17,28 @@ class CKnife : public CWeaponBase
     DECLARE_NETWORKCLASS();
     DECLARE_PREDICTABLE();
 
-#ifndef CLIENT_DLL
-    DECLARE_DATADESC();
-#endif
-
     CKnife();
 
     // We say yes to this so the weapon system lets us switch to it.
-    bool HasPrimaryAmmo() OVERRIDE;
-    bool CanBeSelected() OVERRIDE;
+    bool HasPrimaryAmmo() OVERRIDE { return true; }
+    bool CanBeSelected() OVERRIDE { return true; }
 
-    void Precache() OVERRIDE;
-
-    void Spawn() OVERRIDE;
     void Smack();
     bool SwingOrStab(bool bStab);
     void DoAttack(bool bIsSecondary);
     void PrimaryAttack() OVERRIDE;
     void SecondaryAttack() OVERRIDE;
 
-    void ItemPostFrame(void) OVERRIDE;
-
-    bool Deploy() OVERRIDE;
-    void Holster(int skiplocal = 0);
+    void ItemPostFrame() OVERRIDE;
 
     void WeaponIdle() OVERRIDE;
 
-    WeaponID_t GetWeaponID(void) const OVERRIDE { return WEAPON_KNIFE; }
+    WeaponID_t GetWeaponID() const OVERRIDE { return WEAPON_KNIFE; }
 
-  public:
+private:
     trace_t m_trHit;
     EHANDLE m_pTraceHitEnt;
 
     CNetworkVar(float, m_flSmackTime);
     bool m_bStab;
-
-  private:
-    CKnife(const CKnife &) {}
 };
