eb17a76ddc0884e02f946f4e5c81a38ad718ba20
braem <brae@braebie.com>
2020-03-26 09:29:22 -0700

    Added missing checkbox declarations in code

--- mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.cpp
+++ mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.cpp
@@ -11,6 +11,14 @@ using namespace vgui;
 
 OnlineSettingsPage::OnlineSettingsPage(Panel* pParent) : BaseClass(pParent, "OnlineSettings")
 {
+    m_pEnableGhostRotations = new CvarToggleCheckButton(this, "ToggleRotations", "#MOM_Settings_Online_Rotations", "mom_ghost_online_rotations");
+    m_pEnableGhostRotations->AddActionSignalTarget(this);
+    m_pEnableGhostSounds = new CvarToggleCheckButton(this, "ToggleSounds", "#MOM_Settings_Online_Sounds", "mom_ghost_online_sounds");
+    m_pEnableGhostSounds->AddActionSignalTarget(this);
+    m_pEnableEntityPanels = new CvarToggleCheckButton(this, "ToggleEntityPanels", "#MOM_Settings_Entity_Panels", "mom_entpanels_enable");
+    m_pEnableEntityPanels->AddActionSignalTarget(this);
+    m_pEnableGhostTrails = new CvarToggleCheckButton(this, "ToggleTrails", "#MOM_Settings_Online_Trails", "mom_ghost_online_trail_enable");
+    m_pEnableGhostTrails->AddActionSignalTarget(this);
     m_pEnableColorAlphaOverride = new CvarToggleCheckButton(this, "EnableAlphaOverride", "#MOM_Settings_Override_Alpha_Enable", "mom_ghost_online_alpha_override_enable");
     m_pEnableColorAlphaOverride->AddActionSignalTarget(this);
     m_pAlphaOverrideSlider = new CvarSlider(this, "AlphaOverrideSlider", nullptr, 0.0f, 255.0f, "mom_ghost_online_alpha_override");
