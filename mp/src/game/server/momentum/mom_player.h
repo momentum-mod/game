2387072da3e5cad93046da3445f00b1f326bd9db
braem <brae@braebie.com>
2020-03-31 12:02:17 -0700

    avoided ConVarRef calls every tick

--- mp/src/game/server/momentum/mom_player.h
+++ mp/src/game/server/momentum/mom_player.h
@@ -327,4 +327,6 @@ class CMomentumPlayer : public CBasePlayer, public CGameEventListener, public CM
 
     SavedState_t m_SavedRunState; // Used when either entering practice mode or spectating while in a run
     SavedState_t m_PracticeModeState; // Only used when the path is (in a run) -> (enters Practice) -> (spectates)
+
+    ConVarRef m_cvarMapFinMoveEnable;
 };
