68130df26b43b4503e8ca3ecc4461a2df09cb4ad
brae <brae@braebie.com>
2020-04-18 01:39:23 -0700

    Removed unneed method overrides
    These overrides are not needed since the slider and textentry update the cvar without needing the apply.

--- mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.cpp
+++ mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.cpp
@@ -30,53 +30,7 @@ OnlineSettingsPage::OnlineSettingsPage(Panel* pParent) : BaseClass(pParent, "Onl
     LoadControlSettings("resource/ui/SettingsPanel_OnlineSettings.res");
 }
 
-void OnlineSettingsPage::OnApplyChanges()
-{
-    BaseClass::OnApplyChanges();
-
-    
-}
-
 void OnlineSettingsPage::LoadSettings()
 {
-
-    UpdateSliderSettings();
-}
-
-void OnlineSettingsPage::OnCheckboxChecked(Panel* p)
-{
-}
-
-void OnlineSettingsPage::OnTextChanged(vgui::Panel* panel)
-{
-    BaseClass::OnTextChanged(panel);
-
-    if (panel == m_pAlphaOverrideInput)
-    {
-        char buf[64];
-        m_pAlphaOverrideInput->GetText(buf, 64);
-
-        float fValue = static_cast<float>(Q_atof(buf));
-        if (fValue >= 0.0f && fValue <= 255.0f)
-        {
-            m_pAlphaOverrideSlider->SetSliderValue(fValue);
-        }
-    }
-}
-
-void OnlineSettingsPage::OnControlModified(vgui::Panel* p)
-{
-    BaseClass::OnControlModified(p);
-
-    if (p == m_pAlphaOverrideSlider)
-    {
-        UpdateSliderSettings();
-    }
-}
-
-void OnlineSettingsPage::UpdateSliderSettings()
-{
-    char buf[64];
-    Q_snprintf(buf, sizeof(buf), "%i", static_cast<int>(m_pAlphaOverrideSlider->GetSliderValue()));
-    m_pAlphaOverrideInput->SetText(buf);
+    BaseClass::LoadSettings();
 }
--- mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.h
+++ mp/src/game/client/momentum/ui/SettingsPanel/OnlineSettingsPage.h
@@ -10,21 +10,9 @@ class OnlineSettingsPage : public SettingsPage
 
     ~OnlineSettingsPage() {}
 
-    void OnApplyChanges() OVERRIDE;
-
     void LoadSettings() OVERRIDE;
 
-    //This uses OnCheckbox and not OnModified because we want to be able to enable
-    // the other checkboxes regardless of whether the player clicks Apply/OK
-    void OnCheckboxChecked(Panel *p) OVERRIDE;
-
-    void OnTextChanged(vgui::Panel* panel) OVERRIDE;
-
-    void OnControlModified(vgui::Panel* panel) OVERRIDE;
-
 private:
-    void UpdateSliderSettings();
-
     vgui::CvarSlider *m_pAlphaOverrideSlider;
     vgui::CvarTextEntry *m_pAlphaOverrideInput;
     vgui::CvarToggleCheckButton *m_pEnableGhostRotations, *m_pEnableGhostSounds, *m_pEnableEntityPanels,
