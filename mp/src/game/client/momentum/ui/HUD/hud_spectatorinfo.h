d23bfab4a30d69b1682a61d10bf04062ee877f8b
Gocnak <Gocnak@users.noreply.github.com>
2020-04-21 00:57:45 -0400

    Show spectator lists for other people in your lobby

--- mp/src/game/client/momentum/ui/HUD/hud_spectatorinfo.h
+++ mp/src/game/client/momentum/ui/HUD/hud_spectatorinfo.h
@@ -3,18 +3,21 @@
 #include "vgui_controls/Panel.h"
 #include "hudelement.h"
 
+struct SpecList
+{
+    CUtlVector<uint64> m_vecSpectators;
+};
+
 class CHudSpectatorInfo : public CHudElement, public vgui::Panel
 {
-    
     DECLARE_CLASS_SIMPLE(CHudSpectatorInfo, vgui::Panel);
 
     CHudSpectatorInfo(const char *pName);
     ~CHudSpectatorInfo();
 
     bool ShouldDraw() OVERRIDE;
-
     void Paint() OVERRIDE;
-    void LevelShutdown() OVERRIDE;
+    void LevelShutdown() override;
 
     void SpectatorUpdate(const CSteamID &person, const CSteamID &target);
 
@@ -22,8 +25,9 @@ protected:
     CPanelAnimationVar(vgui::HFont, m_hTextFont, "TextFont", "Default");
 
 private:
-    CUtlMap<uint64, wchar_t *> m_mapNameMap;
+    CUtlMap<uint64, SpecList*> m_mapTargetToSpectateList;
+    CUtlMap<uint64, uint64> m_mapSpectatorToTargetMap;
 
-    uint64 m_idLocal;
+    uint64 m_CurrentSpecTargetSteamID;
     int m_iSpecCount;
 };
