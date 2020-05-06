58afee5a47265ece1dfed62e492732f8a6ccfe2b
brae <brae@braebie.com>
2020-05-06 09:57:28 -0700

    Cleared up logic of when to apply changes
    Logic of when to actually update the cvar & reset the text to it is better defined. Fixed bug of not being able to update the text entry without wiping it first.

--- mp/src/vgui2/vgui_controls/CvarTextEntry.cpp
+++ mp/src/vgui2/vgui_controls/CvarTextEntry.cpp
@@ -27,6 +27,8 @@ CvarTextEntry::CvarTextEntry(Panel *parent, const char *panelName, char const *c
 
     if (m_cvarRef.IsValid())
     {
+        float min;
+        m_bCvarMinAboveZero = m_cvarRef.GetMin(min) ? min >= 0.0f : true;
         Reset();
     }
     
@@ -40,6 +42,7 @@ void CvarTextEntry::SetPrecision(int precision)
     {
         Q_snprintf(m_szNumberFormat, sizeof(m_szNumberFormat), "%%.%if", m_iPrecision);
     }
+    m_fClosestToZeroPossible = 1 / powf(10, m_iPrecision);
 }
 
 void CvarTextEntry::ApplySchemeSettings(IScheme *pScheme)
@@ -79,26 +82,16 @@ void CvarTextEntry::SetText(const char* text)
 
     if (GetAllowNumericInputOnly())
     {
+        char newText[MAX_CVAR_TEXT];
         if (m_iPrecision)
         {
-            if (text[strnlen(text, MAX_CVAR_TEXT) - 1] == '.')
-            {
-                // trying to enter a decimal number so set the text normally
-                BaseClass::SetText(text);
-            }
-            else // not in the middle of setting a decimal number
-            {
-                char newText[MAX_CVAR_TEXT];
-                Q_snprintf(newText, MAX_CVAR_TEXT, m_szNumberFormat, atof(text));
-                BaseClass::SetText(newText);
-            }
+            Q_snprintf(newText, MAX_CVAR_TEXT, m_szNumberFormat, atof(text));
         }
         else
         {
-            char newText[MAX_CVAR_TEXT];
             Q_snprintf(newText, MAX_CVAR_TEXT, "%i", atoi(text));
-            BaseClass::SetText(newText);
         }
+        BaseClass::SetText(newText);
     }
     else
     {
@@ -129,9 +122,7 @@ void CvarTextEntry::ApplyChanges()
     if (!szText[0])
         return;
 
-    m_cvarRef.SetValue(szText);
-
-    Q_strncpy(m_pszStartValue, szText, sizeof(m_pszStartValue));
+    SetCvarVal(szText);
 }
 
 void CvarTextEntry::Reset()
@@ -178,6 +169,104 @@ bool CvarTextEntry::HasBeenModified()
 bool CvarTextEntry::HasBeenModifiedExternally() const
 {
     return m_cvarRef.IsValid() && stricmp(m_cvarRef.GetString(), m_pszStartValue) != 0;
+}
+
+bool CvarTextEntry::ShouldUpdate(const char *szText)
+{
+    if (m_iPrecision == 0) // always update if just integers
+        return true;
+
+    int strLength = Q_strlen(szText);
+
+    if (strLength == 1 && szText[0] == '-')
+        return false;
+
+    bool isJustZero = szText[strLength - 1] == '0' && 
+        (strLength == 1 || (strLength == 2 && szText[0] == '-')); // 0 or -0
+    if (isJustZero)
+        return true;
+
+    if (szText[strLength - 1] == '.') // dont reset if end is a dot (inputing decimal)
+        return false;
+
+    bool isEqToZero = CloseEnough(Q_atof(szText), 0.0f, FLT_EPSILON), // current text value is 0, 0.0, 0.00, etc
+         lastNumIsZero = szText[strLength - 1] == '0'; // last number is zero -> hasn't finished inputting
+    if (lastNumIsZero && isEqToZero)
+        return false;
+
+    return true;
+}
+
+void CvarTextEntry::SetCvarVal(const char* szText)
+{
+    if (!GetAllowNumericInputOnly())
+    {
+        m_cvarRef.SetValue(szText);
+        return;
+    }
+
+    char szCorrectedText[MAX_CVAR_TEXT];
+    Q_strncpy(szCorrectedText, szText, MAX_CVAR_TEXT);
+    if (m_bCvarMinAboveZero && szCorrectedText[0] == '-') // prevent negative if cvar's min is >= 0
+    {
+        Q_StrSubst(szCorrectedText, "-", "", szCorrectedText, MAX_CVAR_TEXT);
+        Q_strncpy(m_pszStartValue, szCorrectedText, sizeof(m_pszStartValue));
+    }
+
+    if (!ShouldUpdate(szCorrectedText))
+        return;
+
+    if (m_iPrecision == 0) // integer
+    {
+        m_cvarRef.SetValue(Q_atoi(szCorrectedText));
+        Q_strncpy(m_pszStartValue, szCorrectedText, sizeof(m_pszStartValue));
+        return;
+    }
+
+    // now just floating point numbers which need precision checks
+    float fVal = Q_atof(szCorrectedText);
+    float fAbsVal = fabsf(fVal);
+    // if value is lower than the smallest (in magnitude) allowed value, change it to that & reset
+    if (fAbsVal < 1.0f && fAbsVal < m_fClosestToZeroPossible && !CloseEnough(fVal, 0.0f, FLT_EPSILON))
+    {
+        m_cvarRef.SetValue(fVal > 0.0f ? m_fClosestToZeroPossible : m_fClosestToZeroPossible * -1.0f);
+        Reset();
+    }
+    else
+    {
+        if (GetPrecisionOfText(szCorrectedText) > m_iPrecision) // too precise; change & reset
+        {
+            Q_snprintf(szCorrectedText, MAX_CVAR_TEXT, m_szNumberFormat, fVal);
+            m_cvarRef.SetValue(szCorrectedText);
+            Reset();
+        }
+        else // good to set
+        {
+            m_cvarRef.SetValue(szCorrectedText);
+            Q_strncpy(m_pszStartValue, szCorrectedText, sizeof(m_pszStartValue));
+        }
+    }
+}
+
+int CvarTextEntry::GetPrecisionOfText(const char *szText)
+{
+    int precision = 0;
+    for (int i = Q_strlen(szText) - 1; i >= 0; i--)
+    {
+        if (szText[i] != '.')
+        {
+            precision++;
+            if (i == 0) // none found
+            {
+                precision = 0;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    return precision;
 }
 
 void CvarTextEntry::OnTextChanged()
