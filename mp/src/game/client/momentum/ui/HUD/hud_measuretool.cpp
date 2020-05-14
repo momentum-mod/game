7bf25c971553a51d66db06e237c8ebdb2f955240
brae <brae@braebie.com>
2020-05-13 22:33:19 -0700

    Made rulermenu configurable using VDF file
    Done in the same fashion as the saveloc menu

--- mp/src/game/client/momentum/ui/HUD/hud_measuretool.cpp
+++ mp/src/game/client/momentum/ui/HUD/hud_measuretool.cpp
@@ -1,34 +1,39 @@
 #include "cbase.h"
 #include "hud_menu_static.h"
 #include "mom_player_shared.h"
+#include "filesystem.h"
 
 #include "tier0/memdbgon.h"
 
+static KeyValues *pRulerKV = nullptr;
+
 static void SelectMenuItem(int menu_item)
 {
-    switch (menu_item)
+    if (!pRulerKV)
+        return;
+
+    if (menu_item == 0) // close button
     {
-    case 1:  //create a checkpoint
-        engine->ExecuteClientCmd("mom_ruler_first");
-        break;
-    case 2:  // Select second point position
-        engine->ExecuteClientCmd("mom_ruler_second");
-        break;
-    case 3:  // Measure distance between points
-        engine->ExecuteClientCmd("mom_ruler_measure");
-        break;
-    case 4:  // Close the ruler
-    case 0:
-        engine->ExecuteClientCmd("mom_ruler_close");
-        break;
-    default:
-        const auto pPlayer = C_MomentumPlayer::GetLocalMomPlayer();
-        if (pPlayer != nullptr)
+        engine->ExecuteClientCmd("mom_saveloc_close");
+        return;
+    }
+
+    if (menu_item < 0 || menu_item > 9) // unknown
+    {
+        C_BasePlayer *cPlayer = C_BasePlayer::GetLocalPlayer();
+        if (cPlayer)
         {
-            // How...
-            pPlayer->EmitSound("Momentum.UIMissingMenuSelection");
+            cPlayer->EmitSound("Momentum.UIMissingMenuSelection");
         }
-        break;
+        return;
+    }
+
+    char strMenuItem[4];
+    Q_snprintf(strMenuItem, sizeof(strMenuItem), "%i", menu_item);
+    KeyValues *pButtonKV = pRulerKV->FindKey(strMenuItem);
+    if (pButtonKV) // menu item is defined in KVs
+    {
+        engine->ExecuteClientCmd(pButtonKV->FindKey("command")->GetString());
     }
 }
 
@@ -48,10 +53,28 @@ CON_COMMAND(showRuler, "Opens the ruler tool.\n")
         }
         else
         {
-            KeyValues* pKv = new KeyValues("Ruler Menu");
-            pKv->AddSubKey(new KeyValues("#MOM_Ruler_FirstPoint"));
-            pKv->AddSubKey(new KeyValues("#MOM_Ruler_SecondPoint"));
-            pKv->AddSubKey(new KeyValues("#MOM_Ruler_Measure"));
+            if (pRulerKV)
+                pRulerKV->deleteThis();
+
+            pRulerKV = new KeyValues("Ruler Menu");
+            pRulerKV->LoadFromFile(g_pFullFileSystem, "cfg/rulermenu.vdf", "MOD");
+
+            KeyValues *pKv = new KeyValues(pRulerKV->GetString());
+            char strMenuItem[4];
+
+            // keep 9 max (0 for cancel & >10 isn't reachable)
+            // also retrieves by number, not by order in file, & ignores the rest
+            for (int iCtr = 1; iCtr <= 9; iCtr++)
+            {
+                Q_snprintf(strMenuItem, sizeof(strMenuItem), "%i", iCtr);
+                KeyValues *subKV = pRulerKV->FindKey(strMenuItem);
+
+                if (!subKV) // no more defined
+                    break;
+
+                pKv->AddSubKey(new KeyValues(subKV->FindKey("label")->GetString()));
+            }
+
             rulerMenu->ShowMenu(pKv, SelectMenuItem, OnClose);
             pKv->deleteThis();
         }
