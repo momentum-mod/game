292c9a715318aa82ef662a00762e2ff62fa1466c
Gocnak <Gocnak@users.noreply.github.com>
2020-05-14 00:51:34 -0400

    + FileImageCache class
    Caches images so that we don't hit the disk/network repeatedly for already-loaded images. Stays in memory for the lifetime of the game.

--- mp/src/game/client/momentum/ui/controls/FileImage.cpp
+++ mp/src/game/client/momentum/ui/controls/FileImage.cpp
@@ -18,6 +18,52 @@
 
 using namespace vgui;
 
+struct ImageCacheEntry
+{
+    CUtlBuffer m_bufOriginalImage;
+};
+
+class CFileImageCache
+{
+public:
+    CFileImageCache() {}
+
+    void AddImageToCache(const char *pPath, const CUtlBuffer &pBuf)
+    {
+        const auto pEntry = new ImageCacheEntry;
+        pEntry->m_bufOriginalImage.CopyBuffer(pBuf);
+
+        const auto index = m_vecImages.AddToTail(pEntry);
+        m_dictImages.Insert(pPath, index);
+    }
+
+    void RemoveImageFromCache(const char *pImagePath)
+    {
+        const auto index = m_dictImages.Find(pImagePath);
+        if (!m_dictImages.IsValidIndex(index))
+            return;
+
+        m_vecImages.PurgeAndDeleteElement(m_dictImages[index]);
+        m_dictImages.RemoveAt(index);
+    }
+
+    ImageCacheEntry *FindImageByPath(const char *pPath)
+    {
+        const auto index = m_dictImages.Find(pPath);
+        if (m_dictImages.IsValidIndex(index))
+        {
+            return m_vecImages[m_dictImages[index]];
+        }
+
+        return nullptr;
+    }
+
+    CUtlVector<ImageCacheEntry *> m_vecImages;
+    CUtlDict<int, uint16> m_dictImages;
+};
+
+CFileImageCache g_FileImageCache;
+
 FileImage::FileImage(IImage *pDefaultImage /* = nullptr*/): m_iX(0), m_iY(0), m_iImageWide(0), 
     m_iDesiredWide(0), m_iImageTall(0), m_iDesiredTall(0), m_iRotation(0), m_iTextureID(-1), m_pDefaultImage(pDefaultImage)
 {
