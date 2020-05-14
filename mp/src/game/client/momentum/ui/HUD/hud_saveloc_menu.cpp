21ea63c52c7e106cea38901cd07622e3728d104b
brae <brae@braebie.com>
2020-05-13 22:18:37 -0700

    Made saveloc menu configurable using VDF file
    VDF file describes the text of the buttons and the command they execute. Ordering is done with "1", "2", etc., and can be unordered (unsorted).

--- mp/src/game/client/momentum/ui/HUD/hud_saveloc_menu.cpp
+++ mp/src/game/client/momentum/ui/HUD/hud_saveloc_menu.cpp
@@ -1,46 +1,38 @@
 #include "cbase.h"
 #include "hud_menu_static.h"
+#include "filesystem.h"
 
 #include "tier0/memdbgon.h"
 
+static KeyValues *pSavelocKV = nullptr;
+
 static void SelectMenuItem(int menu_item)
 {
-    switch (menu_item)
+    if (!pSavelocKV)
+        return;
+
+    if (menu_item == 0) // close button
     {
-    case 1://create a saveloc
-        engine->ExecuteClientCmd("mom_saveloc_create");
-        break;
-    case 2://load previous saveloc
-        engine->ExecuteClientCmd("mom_saveloc_current");
-        break;
-    case 3://cycle through savelocs forwards (+1 % length)
-        engine->ExecuteClientCmd("mom_saveloc_nav_next");
-        break;
-    case 4://cycle backwards through savelocs
-        engine->ExecuteClientCmd("mom_saveloc_nav_prev");
-        break;
-    case 5:// Go to the first saveloc
-        engine->ExecuteClientCmd("mom_saveloc_nav_first");
-        break;
-    case 6:// Go to the last saveloc
-        engine->ExecuteClientCmd("mom_saveloc_nav_last");
-        break;
-    case 7://remove current saveloc
-        engine->ExecuteClientCmd("mom_saveloc_remove_current");
-        break;
-    case 8://remove every saveloc
-        engine->ExecuteClientCmd("mom_saveloc_remove_all");
-        break;
-    case 0://They closed the menu
         engine->ExecuteClientCmd("mom_saveloc_close");
-        break;
-    default:
-        C_BasePlayer* cPlayer = C_BasePlayer::GetLocalPlayer();
+        return;
+    }
+
+    if (menu_item < 0 || menu_item > 9) // unknown
+    {
+        C_BasePlayer *cPlayer = C_BasePlayer::GetLocalPlayer();
         if (cPlayer)
         {
             cPlayer->EmitSound("Momentum.UIMissingMenuSelection");
         }
-        break;
+        return;
+    }
+
+    char strMenuItem[4];
+    Q_snprintf(strMenuItem, sizeof(strMenuItem), "%i", menu_item);
+    KeyValues *pButtonKV = pSavelocKV->FindKey(strMenuItem);
+    if (pButtonKV) // menu item is defined in KVs
+    {
+        engine->ExecuteClientCmd(pButtonKV->FindKey("command")->GetString());
     }
 }
 
@@ -60,15 +52,28 @@ CON_COMMAND(mom_saveloc_show, "Opens the Saved Locations Menu.\n")
         }
         else
         {
-            KeyValues* pKv = new KeyValues("Saveloc Menu");
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_SaveCurLoc"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_ToCurrentSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_ToNextSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_ToPrevSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_ToFirstSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_ToLastSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_RemoveCurrentSL"));
-            pKv->AddSubKey(new KeyValues("#MOM_Menu_RemoveEverySL"));
+            if (pSavelocKV)
+                pSavelocKV->deleteThis();
+
+            pSavelocKV = new KeyValues("Saveloc Menu");
+            pSavelocKV->LoadFromFile(g_pFullFileSystem, "cfg/savelocmenu.vdf", "MOD");
+
+            KeyValues *pKv = new KeyValues(pSavelocKV->GetString());
+            char strMenuItem[4];
+
+            // keep 9 max (0 for cancel & >10 isn't reachable)
+            // also retrieves by number, not by order in file, & ignores the rest
+            for (int iCtr = 1; iCtr <= 9; iCtr++)
+            {
+                Q_snprintf(strMenuItem, sizeof(strMenuItem), "%i", iCtr);
+                KeyValues *subKV = pSavelocKV->FindKey(strMenuItem);
+
+                if (!subKV) // no more defined
+                    break;
+
+                pKv->AddSubKey(new KeyValues(subKV->FindKey("label")->GetString()));
+            }
+
             savelocMenu->ShowMenu(pKv, SelectMenuItem, OnClose);
             pKv->deleteThis();
         }
