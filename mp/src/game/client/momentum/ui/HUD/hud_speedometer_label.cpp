6d6c725df287e8f598f6f2493ba0bd1b8aa614f6
brae <brae@braebie.com>
2020-06-01 09:21:37 -0700

    + unit conversions

--- mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.cpp
+++ mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.cpp
@@ -8,9 +8,17 @@
 
 #include "tier0/memdbgon.h"
 
+// 1 unit = 19.05mm -> 0.01905m -> 0.00001905Km(/s) -> 0.06858Km(/h)
+#define UPS_TO_KMH_FACTOR 0.06858f
+// 1 unit = 0.75", 1 mile = 63360. 0.75 / 63360 ~~> 0.00001184"(/s) ~~> 0.04262MPH
+#define UPS_TO_MPH_FACTOR 0.04262f
+
 using namespace vgui;
 
-SpeedometerLabel::SpeedometerLabel(Panel* parent, const char* panelName): Label(parent, panelName, "")
+extern ConVar sv_gravity;
+
+SpeedometerLabel::SpeedometerLabel(Panel *parent, const char *panelName)
+    : Label(parent, panelName, ""), m_bSupportsEnergyUnits(false)
 {
     Reset();
 }
@@ -41,7 +49,51 @@ void SpeedometerLabel::SetText(int value)
 void SpeedometerLabel::Update(float value)
 {
     m_flCurrentValue = value;
+
+    ConvertUnits();
     SetText(m_flCurrentValue);
 
     m_flPastValue = m_flCurrentValue;
+}
+
+void SpeedometerLabel::SetUnitType(int type) 
+{ 
+    if (type >= SPEEDOMETER_UNITS_FIRST && type <= SPEEDOMETER_UNITS_LAST)
+        SetUnitType(SpeedometerUnits_t(type));
+}
+
+void SpeedometerLabel::ConvertUnits()
+{
+    if (m_eUnitType == SPEEDOMETER_UNITS_UPS)
+        return;
+
+    switch (m_eUnitType)
+    {
+    case SPEEDOMETER_UNITS_KMH:
+        m_flCurrentValue *= UPS_TO_KMH_FACTOR;
+        m_flDiff *= UPS_TO_KMH_FACTOR;
+        break;
+    case SPEEDOMETER_UNITS_MPH:
+        m_flCurrentValue *= UPS_TO_MPH_FACTOR;
+        m_flDiff *= UPS_TO_MPH_FACTOR;
+        break;
+    case SPEEDOMETER_UNITS_ENERGY:
+        // outlier as it requires more than just 1 velocity
+        if (m_bSupportsEnergyUnits)
+        {
+            const auto gravity = sv_gravity.GetFloat();
+            const auto pPlayer = C_MomentumPlayer::GetLocalMomPlayer();
+            Vector velocity = pPlayer->GetAbsVelocity();
+            const auto pEntData = pPlayer->GetCurrentUIEntData();
+            if (!pEntData)
+                break;
+
+            m_flCurrentValue =
+                (velocity.LengthSqr() / 2.0f + gravity * (pPlayer->GetLocalOrigin().z - pEntData->m_flLastJumpZPos)) /
+                gravity;
+        }
+        break;
+    default:
+        break;
+    }
 }
--- mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.h
+++ mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vgui_controls/Label.h>
+#include "mom_shareddefs.h"
 
 class SpeedometerLabel : public vgui::Label
 {
@@ -16,9 +17,23 @@ class SpeedometerLabel : public vgui::Label
     void SetText(int value);
     void SetText(float value) { SetText(RoundFloatToInt(value)); }
 
+
+    // getter/setters
+    SpeedometerUnits_t GetUnitType() { return m_eUnitType; }
+    void SetUnitType(SpeedometerUnits_t type) { m_eUnitType = type; }
+    void SetUnitType(int type);
+
+    bool GetSupportsEnergyUnits() { return m_bSupportsEnergyUnits; }
+    void SetSupportsEnergyUnits(bool bSupportsEnergyUnits) { m_bSupportsEnergyUnits = bSupportsEnergyUnits; }
+
   private:
+    void ConvertUnits();
 
     float m_flCurrentValue;
     float m_flPastValue;
     float m_flDiff;
+
+    bool m_bSupportsEnergyUnits;
+
+    SpeedometerUnits_t m_eUnitType;
 };
