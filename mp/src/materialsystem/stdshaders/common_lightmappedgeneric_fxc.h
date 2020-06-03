f55de2aa6374218bd0b66183aabdfbf635f2064e
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2020-06-03 15:18:21 +1000

    Shader .fxc compatibility fixes
     - Removed references to D3DVERTEXTEXTURESAMPLER0
     - Removed references to X360
     - Replace CalcFixedFunctionFog with CalcFog
     - Fix calls to functions that slightly differ from ASW
     - Add missing utility functions from ASW

--- mp/src/materialsystem/stdshaders/common_lightmappedgeneric_fxc.h
+++ mp/src/materialsystem/stdshaders/common_lightmappedgeneric_fxc.h
@@ -147,6 +147,66 @@ void GetBaseTextureAndNormal( sampler base, sampler base2, sampler bump, bool bB
 
 }
 
+void GetBaseTextureAndNormal( sampler base, sampler base2, sampler bump, bool bBase2, bool bBump,
+							  float3 coords, float2 bumpcoords, float3 vWeights,
+							 out float4 vResultBase, out float4 vResultBase2, out float4 vResultBump )
+{
+	vResultBase = 0;
+	vResultBase2 = 0;
+	vResultBump = 0;
+
+	if ( !bBump )
+	{
+		vResultBump = float4(0, 0, 1, 1);
+	}
+
+#if SEAMLESS
+
+	vResultBase  += vWeights.x * tex2D( base, coords.zy );
+	if ( bBase2 )
+	{
+		vResultBase2 += vWeights.x * tex2D( base2, coords.zy );
+	}
+	if ( bBump )
+	{
+		vResultBump  += vWeights.x * tex2D( bump, coords.zy );
+	}
+
+	vResultBase  += vWeights.y * tex2D( base, coords.xz );
+	if ( bBase2 )
+	{
+		vResultBase2 += vWeights.y * tex2D( base2, coords.xz );
+	}
+	if ( bBump )
+	{
+		vResultBump  += vWeights.y * tex2D( bump, coords.xz );
+	}
+
+	vResultBase  += vWeights.z * tex2D( base, coords.xy );
+	if ( bBase2 )
+	{
+		vResultBase2 += vWeights.z * tex2D( base2, coords.xy );
+	}
+	if ( bBump )
+	{
+		vResultBump  += vWeights.z * tex2D( bump, coords.xy );
+	}
+
+#else  // not seamless
+
+	vResultBase  = tex2D( base, coords.xy );
+	if ( bBase2 )
+	{
+		vResultBase2 = tex2D( base2, coords.xy );
+	}
+	if ( bBump )
+	{
+		vResultBump  = tex2D( bump, bumpcoords.xy );
+	}
+#endif
+
+}
+
 #endif
 
 
