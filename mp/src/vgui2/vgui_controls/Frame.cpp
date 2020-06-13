ebf5b6fed914fc80c5cdf2d78fd21ef75b54abac
Gocnak <Gocnak@users.noreply.github.com>
2020-06-12 21:45:47 -0400

    Move visibility override to EditablePanel
    This makes it so that reloading controls won't make random panels pop open. Note that this only applies to editable panels and derivitives (Frame, Viewport), and not individual controls

--- mp/src/vgui2/vgui_controls/Frame.cpp
+++ mp/src/vgui2/vgui_controls/Frame.cpp
@@ -1662,10 +1662,6 @@ void Frame::SetFadeEffectDisableOverride( bool disabled )
 //-----------------------------------------------------------------------------
 void Frame::ApplySettings(KeyValues *inResourceData)
 {
-	// Don't change the frame's visibility if there is no specific override for it
-	if (!inResourceData->FindKey("visible"))
-	    inResourceData->SetInt("visible", -1);
-
 	BaseClass::ApplySettings(inResourceData);
 
 	// if "title" is "0" then don't draw the title bar
