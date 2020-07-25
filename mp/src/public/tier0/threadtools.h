7877c9d8b3f100175fd30d3c407270891c37d953
Travis Baylor <tbaylor00@gmail.com>
2020-07-25 02:32:22 -0500

    Spelling fixes (threadtools.h)

--- mp/src/public/tier0/threadtools.h
+++ mp/src/public/tier0/threadtools.h
@@ -1377,7 +1377,7 @@ public:
 	// Inter-thread communication
 	//
 	// Calls in either direction take place on the same "channel."
-	// Seperate functions are specified to make identities obvious
+	// Separate functions are specified to make identities obvious
 	//
 	//-----------------------------------------------------
 
