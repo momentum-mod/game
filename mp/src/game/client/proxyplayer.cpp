22136cb5965196b5311e53d4c41273de84c2e919
Travis Baylor <tbaylor00@gmail.com>
2020-08-01 21:49:57 -0500

    Spelling fixes (proxyplayer.cpp)

--- mp/src/game/client/proxyplayer.cpp
+++ mp/src/game/client/proxyplayer.cpp
@@ -409,7 +409,7 @@ bool CPlayerLogoProxy::Init( IMaterial *pMaterial, KeyValues *pKeyValues )
 
 void CPlayerLogoProxy::OnBind( void *pC_BaseEntity )
 {
-	// Decal's are bound with the player index as the passed in paramter
+	// Decal's are bound with the player index as the passed in parameter
 	int playerindex = (int)pC_BaseEntity;
 
 	if ( playerindex <= 0 )
