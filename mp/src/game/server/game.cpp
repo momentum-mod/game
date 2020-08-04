5c21099bb172a3a4cbae04420a78b8600a54bcfe
Matthew Davis <45373823+davis-matthew@users.noreply.github.com>
2020-08-04 01:48:29 -0500

    Spelling fixes (game.cpp)

--- mp/src/game/server/game.cpp
+++ mp/src/game/server/game.cpp
@@ -83,7 +83,7 @@ public:
 			// NOTE:  If not overridden at the command line, then if it's a replicated cvar, make sure that it's
 			//  value is the server's value.  This solves a problem where think_limit is defined in shared
 			//  code but the value is inside and #if defined( _DEBUG ) block and if you have a debug game .dll
-			//  and a release client, then the limiit was coming from the client even though the server value 
+			//  and a release client, then the limit was coming from the client even though the server value 
 			//  was the one that was important during debugging.  Now the server trumps the client value for
 			//  replicated ConVars by setting the value here after the ConVar has been linked.
 			if ( replicated && defvalue && !pCommand->IsCommand() )
@@ -100,7 +100,7 @@ public:
 static CGameDLL_ConVarAccessor g_ConVarAccessor;
 
 // Register your console variables here
-// This gets called one time when the game is initialied
+// This gets called one time when the game is initialized
 void InitializeCvars( void )
 {
 	// Register cvars here:
