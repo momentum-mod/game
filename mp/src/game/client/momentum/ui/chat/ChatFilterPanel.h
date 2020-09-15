c551be9a1dfaeb22771cd3d05f918e1b6819bad1
Gocnak <Gocnak@users.noreply.github.com>
2020-09-15 01:46:57 -0400

    + New base chat files
    Ripped apart the old chat panel into these. There really was a ton of classes inside the basechat file, but now they have room to breathe. All chat related stuff is handled through these files instead of being all over the codebase

---
+++ mp/src/game/client/momentum/ui/chat/ChatFilterPanel.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "vgui_controls/CheckButton.h"
+#include "vgui_controls/EditablePanel.h"
+
+class ChatPanel;
+
+enum ChatFilters
+{
+    CHAT_FILTER_NONE = 0,
+    CHAT_FILTER_JOINLEAVE = 0x000001,
+    CHAT_FILTER_NAMECHANGE = 0x000002,
+    CHAT_FILTER_PUBLICCHAT = 0x000004,
+    CHAT_FILTER_SERVERMSG = 0x000008,
+    CHAT_FILTER_TEAMCHANGE = 0x000010,
+    CHAT_FILTER_ACHIEVEMENT = 0x000020,
+};
+
+class ChatFilterCheckButton : public vgui::CheckButton
+{
+public:
+    DECLARE_CLASS_SIMPLE(ChatFilterCheckButton, vgui::CheckButton);
+
+    ChatFilterCheckButton(Panel *pParent, const char *pName, int iFlag);
+
+    int GetFilterFlag() { return m_iFlag; }
+
+private:
+    int m_iFlag;
+};
+
+class ChatFilterPanel : public vgui::EditablePanel
+{
+public:
+    DECLARE_CLASS_SIMPLE(ChatFilterPanel, vgui::EditablePanel);
+
+    ChatFilterPanel(ChatPanel *pParent);
+
+    MESSAGE_FUNC_PTR(OnFilterButtonChecked, "CheckButtonChecked", panel);
+
+    void SetVisible(bool state) override;
+
+private:
+    ChatPanel *m_pChatParent;
+};
