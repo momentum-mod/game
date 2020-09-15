3215a26037f7429f408b65abf71a98f036919575
Gocnak <Gocnak@users.noreply.github.com>
2020-09-15 01:34:24 -0400

    + UserComponent VGUI control
    This allows us to display a user's avatar, name, current level and maybe some more info down the line. It'll be used to show the local player's data on the menu for now

---
+++ mp/src/game/client/momentum/ui/controls/UserComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "vgui_controls/EditablePanel.h"
+
+class CAvatarImagePanel;
+struct UserData;
+
+class UserComponent : public vgui::EditablePanel
+{
+public:
+    DECLARE_CLASS_SIMPLE(UserComponent, EditablePanel);
+    UserComponent(Panel *pParent, const char *pName = "UserComponent");
+    ~UserComponent();
+
+    void SetClickable(bool bClickable);
+
+    void SetUser(uint64 uID);
+
+    void FillControlsWithUserData(const UserData &data);
+
+    MESSAGE_FUNC(OnUserDataUpdate, "UserDataUpdate");
+
+protected:
+    void OnReloadControls() override;
+    void PerformLayout() override;
+    void ApplySchemeSettings(vgui::IScheme* pScheme) override;
+    void OnMouseReleased(vgui::MouseCode code) override;
+
+private:
+    CAvatarImagePanel *m_pUserImage;
+    vgui::Label *m_pUserName, *m_pUserRank;
+    vgui::ProgressBar *m_pXPProgressBar;
+
+    bool m_bClickable;
+};
