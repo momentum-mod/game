9263da481ed3a12592f48b23f83ad6fe42312e94
Gocnak <Gocnak@users.noreply.github.com>
2020-10-03 00:19:03 -0400

    Use same SetLocalAngles normalization logic on C_BaseEntity
    Same one from Server, tested on some maps that rotate stuff and it seemed fine

--- mp/src/game/client/c_baseentity.cpp
+++ mp/src/game/client/c_baseentity.cpp
@@ -4202,13 +4202,14 @@ vec_t C_BaseEntity::GetLocalAnglesDim( int iDim ) const
 // Prevent these for now until hierarchy is properly networked
 void C_BaseEntity::SetLocalAngles( const QAngle& angles )
 {
-	// NOTE: The angle normalize is a little expensive, but we can save
-	// a bunch of time in interpolation if we don't have to invalidate everything
-	// and sometimes it's off by a normalization amount
-
-	// FIXME: The normalize caused problems in server code like momentary_rot_button that isn't
-	//        handling things like +/-180 degrees properly. This should be revisited.
-	//QAngle angleNormalize( AngleNormalize( angles.x ), AngleNormalize( angles.y ), AngleNormalize( angles.z ) );
+	// Safety check against NaN's or really huge numbers
+	// And actually normalize the angles rather than freeze the entity and spam warnings
+	if (!IsEntityQAngleReasonable(angles))
+	{
+		QAngle angleNormalize(AngleNormalize(angles.x), AngleNormalize(angles.y), AngleNormalize(angles.z));
+		SetLocalAngles(angleNormalize);
+		return;
+	}
 
 	if (m_angRotation != angles)
 	{
