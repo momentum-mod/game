5ef78a6cda9ac4563a642d841f7d78b5952a8e8a
brae <brae@braebie.com>
2020-10-19 00:38:16 -0700

    + autocomplete for mom_hud_menu_show
    Also a command for reloading the menu listing, `mom_hud_menu_reload`.

--- mp/src/game/client/momentum/ui/menus/hud_menu_static_system.h
+++ mp/src/game/client/momentum/ui/menus/hud_menu_static_system.h
@@ -8,6 +8,17 @@ class CHudMenuStaticSystem : public CAutoGameSystem
     void ShowMenu(const char *pszFileName);
     void HideMenu();
     bool IsMenuDisplayed(const char *pszMenuName);
+
+    void ReloadMenuList();
+
+    const char *GetMenuName(int index) { return m_vecMenuNames[index]; }
+    int GetNumMenus() { return m_vecMenuNames.Size(); }
+
+  protected:
+    void PostInit() override;
+
+  private:
+    CUtlStringList m_vecMenuNames;
 };
 
 extern CHudMenuStaticSystem *g_pHudMenuStaticSystem;
