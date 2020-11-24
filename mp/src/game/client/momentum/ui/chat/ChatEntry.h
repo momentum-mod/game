1e30345cb337c0ab63e2c5f87bb91964b1f6cf15
Gocnak <Gocnak@users.noreply.github.com>
2020-11-24 02:33:06 -0500

    Add history tracking to chat entry input
    Saves the last 20 unique messages you input
    Use up arrow when empty text to start from the most recent message you entered, and to iterate further into history
    Use down arrow when empty text to start from the least recent message you entered, and to iterate towards the most recent message
    You can do partial deletes and iteration will still work

--- mp/src/game/client/momentum/ui/chat/ChatEntry.h
+++ mp/src/game/client/momentum/ui/chat/ChatEntry.h
@@ -17,5 +17,12 @@ public:
     CPanelAnimationStringVar(32, m_FontName, "font", "Default");
     CPanelAnimationVar(Color, m_cTypingColor, "TypingText", "White");
 private:
+    void AddInputToHistory();
+    // Pressing "up" should go to previously sent message (end of history list and then up, decreasing the index to 0)
+    // Pressing "down" should go to first sent message, going oldest -> newest in history
+    void TraverseHistory(bool bUpKeyPressed);
+
+    QueueIter_t m_iHistoryIndex; // Currently at this point in history, partial backspaces will still allow traversal
+    CUtlQueue<CUtlString> m_queueChatHistory;
     ChatPanel *m_pChatParent;
 };
