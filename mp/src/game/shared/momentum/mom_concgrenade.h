825417f2e9cdcf189cdc852dd71d812e5a356274
Gocnak <Gocnak@users.noreply.github.com>
2020-11-27 20:01:40 -0500

    Convert Conc grenade to CMomExplosive
    Adds:
    mom_conc_particle_trail_enable
    mom_conc_sound_bounce_enable
    mom_conc_sound_fizzle_enable
    "ConcDetProg" proxy for progress of the grenade to detonation, 0 is just primed and thrown, 1 is detonating
    Removed:
    Old entity-based particle and trail

--- mp/src/game/shared/momentum/mom_concgrenade.h
+++ mp/src/game/shared/momentum/mom_concgrenade.h
@@ -1,113 +1,75 @@
 #pragma once
 
-#include "Sprite.h"
-#include "SpriteTrail.h"
-#include "basegrenade_shared.h"
+#include "mom_explosive.h"
 
 #ifdef CLIENT_DLL
 class CHudConcEntPanel;
 #define CMomConcProjectile C_MomConcProjectile
-#define CMomConcGlow C_MomConcGlow
 #endif
 
-class CMomConcGlow : public CSprite
+#define CONC_MAX_TIME 3.81f
+
+class CMomConcProjectile : public CMomExplosive
 {
   public:
-    DECLARE_CLASS(CMomConcGlow, CSprite);
-
-    DECLARE_NETWORKCLASS();
-    DECLARE_DATADESC();
-
-    int ObjectCaps() override { return BaseClass::ObjectCaps() & (~FCAP_ACROSS_TRANSITION | FCAP_DONT_SAVE); }
-
-#ifdef CLIENT_DLL
-    bool IsTransparent() override { return true; }
-    RenderGroup_t GetRenderGroup() override { return RENDER_GROUP_TRANSLUCENT_ENTITY; }
-    int DrawModel(int flags) override;
-    void OnDataChanged(DataUpdateType_t updateType) override;
-    bool ShouldDraw() { return (IsEffectActive(EF_NODRAW) == false); }
-#else
-    static CMomConcGlow *Create(const Vector &origin, CBaseEntity *pOwner = nullptr);
-#endif
-};
-
-class CMomConcProjectile : public CBaseGrenade
-{
-  public:
-    DECLARE_CLASS(CMomConcProjectile, CBaseGrenade);
+    DECLARE_CLASS(CMomConcProjectile, CMomExplosive);
     DECLARE_NETWORKCLASS();
 
     ~CMomConcProjectile();
 
-    bool m_bIsHandheld;
-    CNetworkVar(bool, m_bIsOn);
-    CNetworkVector(m_vInitialVelocity);
-
     void Spawn() override;
     void Precache() override;
 
-    float GetDamage() override { return 0.0f; }
-    float GetMaxTimer() { return 3.81f; }
+    void BounceSound();
 
-    bool SetHandheld(bool state) { return m_bIsHandheld = state; }
-    bool IsHandheld() { return m_bIsHandheld; }
-
-    void BounceSound() override;
-
-    CNetworkVarForDerived(float, m_flSpawnTime);
     CNetworkVar(float, m_flDetonateTime);
     float m_flNextBounceSoundTime;
 
 #ifdef GAME_DLL
     DECLARE_DATADESC();
 
-    void DrawRadius(float flRadius);
-    void Explode(trace_t *pTrace, int bitsDamageType) override;
+    static CMomConcProjectile *Create(float fTimer, const Vector &position, const Vector &velocity, CBaseEntity *pOwner);
 
-    // Delete entity without explosion
-    void Remove();
+    void PlayFizzleSound() override;
 
-    float GetShakeAmplitude() override { return 0.0f; }
+    // Need *some* damage even though we don't actually do any, in order to distinguish
+    // the difference between a "dud" conc and an active one. Stickybombs and rockets use the value of
+    // "0" to be a dud, conc by default does 0 damage.
+    float GetDamageAmount() override { return 1.0f; }
+
+    void DrawRadius();
+    void Explode();
+    void AffectEntitiesInRadius();
+
+    void Destroy() override;
+
     float GetGrenadeGravity() { return 0.8f; }
     float GetGrenadeFriction() { return 0.3f; }
     float GetGrenadeElasticity() { return 0.6f; }
     float GetGrenadeRadius() { return 280.0f; }
-    float GetGrenadeDamage() { return 0.0f; }
 
     void WaterCheck();
     void GrenadeThink();
 
-    void CreateTrail();
-    void CreateGlowSprite();
-
-    // Specify what velocity we want to have on the client immediately.
-    // Without this, the entity wouldn't have an interpolation history initially, so it would
-    // sit still until it had gotten a few updates from the server.
-    void SetupInitialTransmittedVelocity(const Vector &velocity);
     void SetDetonateTimerLength(float timer);
     bool IsInWorld() const;
 
-    bool m_bHitwater;
-    float m_flHitwaterTimer;
+    bool SetHandheld(bool state) { return m_bIsHandheld = state; }
+    bool IsHandheld() const { return m_bIsHandheld; }
 
   private:
     void ResolveFlyCollisionCustom(trace_t &trace, Vector &vecVelocity) override;
+
+    bool m_bIsHandheld;
+    bool m_bHitwater;
+    float m_flHitwaterTimer;
 #else
-    CMomConcProjectile() {}
-    CMomConcProjectile(const CMomConcProjectile &) {}
-
-    // Add initial velocity into the interpolation history so that interp works okay
-    void OnDataChanged(DataUpdateType_t type) override;
+    void CreateTrailParticles() override;
     void ClientThink() override;
-
-    void DoIdleEffect();
 
     virtual ShadowType_t ShadowCastType() { return SHADOWS_NONE; }
 
   private:
     CHudConcEntPanel *m_pEntPanel;
 #endif
-  private:
-    CHandle<CSpriteTrail> m_pTrail;
-    CHandle<CMomConcGlow> m_hGlowSprite;
 };
