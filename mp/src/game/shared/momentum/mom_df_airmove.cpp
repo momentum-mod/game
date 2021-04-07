a8e4d975c8ad93d53758db06794b6e948c9c7b94
Lumia <66639143+chovelyoukai@users.noreply.github.com>
2021-04-06 21:40:51 -0400

    added missing includes

--- mp/src/game/shared/momentum/mom_df_airmove.cpp
+++ mp/src/game/shared/momentum/mom_df_airmove.cpp
@@ -1,3 +1,25 @@
+#include "cbase.h"
+
+#include "in_buttons.h"
+#include "mom_gamemovement.h"
+#include "mom_player_shared.h"
+#include "mom_shareddefs.h"
+#include "movevars_shared.h"
+#include "coordsize.h"
+#include "mom_system_gamemode.h"
+
+#ifdef CLIENT_DLL
+#include "c_mom_triggers.h"
+#else
+#include "env_player_surface_trigger.h"
+#include "momentum/mom_triggers.h"
+#include "momentum/mom_system_saveloc.h"
+#include "momentum/mom_timer.h"
+#endif
+
+#include "tier0/memdbgon.h"
+
+
 void CMomentumGameMovement::DFAirAccelerate(Vector wishdir, float wishspeed, float accel, float maxspeed)
 {
     int i;
